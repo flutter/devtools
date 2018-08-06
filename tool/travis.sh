c91f3d15ab649a3eba8b97c9f6ccd43c3395360c
Devon Carew <devoncarew@google.com>
2018-08-06 16:24:19 -0700

    add enough files to set up travis support

---
+++ tool/travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Fast fail the script on failures.
+set -e
+
+# Print out the Dart version in use.
+dart --version
+
+# Add globally activated packages to the path.
+export PATH="$PATH":"~/.pub-cache/bin"
+
+# Analyze the source.
+pub global activate tuneup
+tuneup check --ignore-infos
+
+# Ensure we can build the app.
+pub global activate webdev
+webdev build
