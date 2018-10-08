e224c4ef6492ba70e1306f218adb11ba907b5add
Danny Tuppeny <dantup@google.com>
2018-10-08 12:38:08 +0100

    Fix compile error from rebase

--- lib/tables.dart
+++ lib/tables.dart
@@ -229,7 +229,7 @@ class Table<T> extends Object with SetStateMixin, OnAddedToDomMixin {
   void _rebuildTable() {
     // If we've never had any data set, we don't need to (and can't - since
     // all the elements aren't created) rebuild.
-    if (rows == null) {
+    if (data == null) {
       return;
     }
     if (_isVirtual)
