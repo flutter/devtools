81eb68bb9bdc2628eeb3dcb75015c23f9aaddd51
kenzieschmoll <kenzieschmoll@google.com>
2018-12-17 09:41:39 -0800

    make class members private

--- lib/vm_service_wrapper.dart
+++ lib/vm_service_wrapper.dart
@@ -2,105 +2,105 @@ import 'dart:async';
 import 'package:vm_service_lib/vm_service_lib.dart';
 
 class VmServiceWrapper implements VmService {
-  VmServiceWrapper(this.vmService);
+  VmServiceWrapper(this._vmService);
 
   VmServiceWrapper.fromNewVmService(
       Stream<dynamic> /*String|List<int>*/ inStream,
       void writeMessage(String message),
       {Log log,
       DisposeHandler disposeHandler}) {
-    vmService = new VmService(inStream, writeMessage,
+    _vmService = new VmService(inStream, writeMessage,
         log: log, disposeHandler: disposeHandler);
   }
 
-  VmService vmService;
-  Set<String> activeStreams = Set();
-  Set<Future<Object>> activeFutures = Set();
+  VmService _vmService;
+  final Set<String> _activeStreams = Set();
+  final Set<Future<Object>> _activeFutures = Set();
   Completer<bool> noPendingFutures = new Completer<bool>();
 
   @override
   Future<Breakpoint> addBreakpoint(String isolateId, String scriptId, int line,
           {int column}) =>
       _trackFuture(
-          vmService.addBreakpoint(isolateId, scriptId, line, column: column));
+          _vmService.addBreakpoint(isolateId, scriptId, line, column: column));
 
   @override
   Future<Breakpoint> addBreakpointAtEntry(
           String isolateId, String functionId) =>
-      _trackFuture(vmService.addBreakpointAtEntry(isolateId, functionId));
+      _trackFuture(_vmService.addBreakpointAtEntry(isolateId, functionId));
 
   @override
   Future<Breakpoint> addBreakpointWithScriptUri(
           String isolateId, String scriptUri, int line, {int column}) =>
-      _trackFuture(vmService.addBreakpointWithScriptUri(
+      _trackFuture(_vmService.addBreakpointWithScriptUri(
           isolateId, scriptUri, line,
           column: column));
 
   @override
   Future<Response> callMethod(String method, {String isolateId, Map args}) =>
       _trackFuture(
-          vmService.callMethod(method, isolateId: isolateId, args: args));
+          _vmService.callMethod(method, isolateId: isolateId, args: args));
 
   @override
   Future<Response> callServiceExtension(String method,
           {String isolateId, Map args}) =>
-      _trackFuture(vmService.callServiceExtension(method,
+      _trackFuture(_vmService.callServiceExtension(method,
           isolateId: isolateId, args: args));
 
   @override
   Future<Success> clearCpuProfile(String isolateId) =>
-      _trackFuture(vmService.clearCpuProfile(isolateId));
+      _trackFuture(_vmService.clearCpuProfile(isolateId));
 
   @override
-  Future<Success> clearVMTimeline() => _trackFuture(vmService.clearVMTimeline());
+  Future<Success> clearVMTimeline() => _trackFuture(_vmService.clearVMTimeline());
 
   @override
   Future<Success> collectAllGarbage(String isolateId) =>
-      _trackFuture(vmService.collectAllGarbage(isolateId));
+      _trackFuture(_vmService.collectAllGarbage(isolateId));
 
   @override
-  void dispose() => vmService.dispose();
+  void dispose() => _vmService.dispose();
 
   @override
   Future evaluate(String isolateId, String targetId, String expression,
           {Map<String, String> scope}) =>
       _trackFuture(
-          vmService.evaluate(isolateId, targetId, expression, scope: scope));
+          _vmService.evaluate(isolateId, targetId, expression, scope: scope));
 
   @override
   Future evaluateInFrame(String isolateId, int frameIndex, String expression,
           {Map<String, String> scope}) =>
-      _trackFuture(vmService.evaluateInFrame(isolateId, frameIndex, expression,
+      _trackFuture(_vmService.evaluateInFrame(isolateId, frameIndex, expression,
           scope: scope));
 
   @override
   Future<AllocationProfile> getAllocationProfile(String isolateId,
           {String gc, bool reset}) =>
-      _trackFuture(vmService.getAllocationProfile(isolateId));
+      _trackFuture(_vmService.getAllocationProfile(isolateId));
 
   @override
   Future<CpuProfile> getCpuProfile(String isolateId, String tags) =>
-      _trackFuture(vmService.getCpuProfile(isolateId, tags));
+      _trackFuture(_vmService.getCpuProfile(isolateId, tags));
 
   @override
-  Future<FlagList> getFlagList() => _trackFuture(vmService.getFlagList());
+  Future<FlagList> getFlagList() => _trackFuture(_vmService.getFlagList());
 
   @override
   Future<ObjRef> getInstances(String isolateId, String classId, int limit) =>
-      _trackFuture(vmService.getInstances(isolateId, classId, limit));
+      _trackFuture(_vmService.getInstances(isolateId, classId, limit));
 
   @override
   Future getIsolate(String isolateId) =>
-      _trackFuture(vmService.getIsolate(isolateId));
+      _trackFuture(_vmService.getIsolate(isolateId));
 
   @override
   Future getObject(String isolateId, String objectId,
           {int offset, int count}) =>
-      _trackFuture(vmService.getObject(isolateId, objectId));
+      _trackFuture(_vmService.getObject(isolateId, objectId));
 
   @override
   Future<ScriptList> getScripts(String isolateId) =>
-      _trackFuture(vmService.getScripts(isolateId));
+      _trackFuture(_vmService.getScripts(isolateId));
 
   @override
   Future<SourceReport> getSourceReport(
@@ -109,7 +109,7 @@ class VmServiceWrapper implements VmService {
           int tokenPos,
           int endTokenPos,
           bool forceCompile}) =>
-      _trackFuture(vmService.getSourceReport(isolateId, reports,
+      _trackFuture(_vmService.getSourceReport(isolateId, reports,
           scriptId: scriptId,
           tokenPos: tokenPos,
           endTokenPos: endTokenPos,
@@ -117,77 +117,77 @@ class VmServiceWrapper implements VmService {
 
   @override
   Future<Stack> getStack(String isolateId) =>
-      _trackFuture(vmService.getStack(isolateId));
+      _trackFuture(_vmService.getStack(isolateId));
 
   @override
-  Future<VM> getVM() => _trackFuture(vmService.getVM());
+  Future<VM> getVM() => _trackFuture(_vmService.getVM());
 
   @override
-  Future<Response> getVMTimeline() => _trackFuture(vmService.getVMTimeline());
+  Future<Response> getVMTimeline() => _trackFuture(_vmService.getVMTimeline());
 
   @override
-  Future<Version> getVersion() => _trackFuture(vmService.getVersion());
+  Future<Version> getVersion() => _trackFuture(_vmService.getVersion());
 
   @override
   Future invoke(String isolateId, String targetId, String selector,
           List<String> argumentIds) =>
-      _trackFuture(vmService.invoke(isolateId, targetId, selector, argumentIds));
+      _trackFuture(_vmService.invoke(isolateId, targetId, selector, argumentIds));
 
   @override
   Future<Success> kill(String isolateId) =>
-      _trackFuture(vmService.kill(isolateId));
+      _trackFuture(_vmService.kill(isolateId));
 
   @override
-  Stream<Event> get onDebugEvent => vmService.onDebugEvent;
+  Stream<Event> get onDebugEvent => _vmService.onDebugEvent;
 
   @override
-  Stream<Event> onEvent(String streamName) => vmService.onEvent(streamName);
+  Stream<Event> onEvent(String streamName) => _vmService.onEvent(streamName);
 
   @override
-  Stream<Event> get onExtensionEvent => vmService.onExtensionEvent;
+  Stream<Event> get onExtensionEvent => _vmService.onExtensionEvent;
 
   @override
-  Stream<Event> get onGCEvent => vmService.onGCEvent;
+  Stream<Event> get onGCEvent => _vmService.onGCEvent;
 
   @override
-  Stream<Event> get onGraphEvent => vmService.onGraphEvent;
+  Stream<Event> get onGraphEvent => _vmService.onGraphEvent;
 
   @override
-  Stream<Event> get onIsolateEvent => vmService.onIsolateEvent;
+  Stream<Event> get onIsolateEvent => _vmService.onIsolateEvent;
 
   @override
-  Stream<String> get onReceive => vmService.onReceive;
+  Stream<String> get onReceive => _vmService.onReceive;
 
   @override
-  Stream<String> get onSend => vmService.onSend;
+  Stream<String> get onSend => _vmService.onSend;
 
   @override
-  Stream<Event> get onServiceEvent => vmService.onServiceEvent;
+  Stream<Event> get onServiceEvent => _vmService.onServiceEvent;
 
   @override
-  Stream<Event> get onStderrEvent => vmService.onStderrEvent;
+  Stream<Event> get onStderrEvent => _vmService.onStderrEvent;
 
   @override
-  Stream<Event> get onStdoutEvent => vmService.onStdoutEvent;
+  Stream<Event> get onStdoutEvent => _vmService.onStdoutEvent;
 
   @override
-  Stream<Event> get onVMEvent => vmService.onVMEvent;
+  Stream<Event> get onVMEvent => _vmService.onVMEvent;
 
   @override
-  Future<Success> pause(String isolateId) => _trackFuture(vmService.pause(isolateId));
+  Future<Success> pause(String isolateId) => _trackFuture(_vmService.pause(isolateId));
 
   @override
   Future<Success> registerService(String service, String alias) =>
-      _trackFuture(vmService.registerService(service, alias));
+      _trackFuture(_vmService.registerService(service, alias));
 
   @override
   void registerServiceCallback(String service, ServiceCallback cb) =>
-      vmService.registerServiceCallback(service, cb);
+      _vmService.registerServiceCallback(service, cb);
 
   @override
   Future<ReloadReport> reloadSources(String isolateId,
           {bool force, bool pause, String rootLibUri, String packagesUri}) =>
-      _trackFuture(vmService.reloadSources(isolateId,
+      _trackFuture(_vmService.reloadSources(isolateId,
           force: force,
           pause: pause,
           rootLibUri: rootLibUri,
@@ -195,66 +195,66 @@ class VmServiceWrapper implements VmService {
 
   @override
   Future<Success> removeBreakpoint(String isolateId, String breakpointId) =>
-      _trackFuture(vmService.removeBreakpoint(isolateId, breakpointId));
+      _trackFuture(_vmService.removeBreakpoint(isolateId, breakpointId));
 
   @override
   Future<Success> requestHeapSnapshot(
           String isolateId, String roots, bool collectGarbage) =>
       _trackFuture(
-          vmService.requestHeapSnapshot(isolateId, roots, collectGarbage));
+          _vmService.requestHeapSnapshot(isolateId, roots, collectGarbage));
 
   @override
   Future<Success> resume(String isolateId, {String step, int frameIndex}) =>
       _trackFuture(
-          vmService.resume(isolateId, step: step, frameIndex: frameIndex));
+          _vmService.resume(isolateId, step: step, frameIndex: frameIndex));
 
   @override
   Future<Success> setExceptionPauseMode(String isolateId, String mode) =>
-      _trackFuture(vmService.setExceptionPauseMode(isolateId, mode));
+      _trackFuture(_vmService.setExceptionPauseMode(isolateId, mode));
 
   @override
   Future<Success> setFlag(String name, String value) =>
-      _trackFuture(vmService.setFlag(name, value));
+      _trackFuture(_vmService.setFlag(name, value));
 
   @override
   Future<Success> setLibraryDebuggable(
           String isolateId, String libraryId, bool isDebuggable) =>
       _trackFuture(
-          vmService.setLibraryDebuggable(isolateId, libraryId, isDebuggable));
+          _vmService.setLibraryDebuggable(isolateId, libraryId, isDebuggable));
 
   @override
   Future<Success> setName(String isolateId, String name) =>
-      _trackFuture(vmService.setName(isolateId, name));
+      _trackFuture(_vmService.setName(isolateId, name));
 
   @override
   Future<Success> setVMName(String name) =>
-      _trackFuture(vmService.setVMName(name));
+      _trackFuture(_vmService.setVMName(name));
 
   @override
   Future<Success> setVMTimelineFlags(List<String> recordedStreams) =>
-      _trackFuture(vmService.setVMTimelineFlags(recordedStreams));
+      _trackFuture(_vmService.setVMTimelineFlags(recordedStreams));
 
   @override
   Future<Success> streamCancel(String streamId) {
-    activeStreams.remove(streamId);
-    return _trackFuture(vmService.streamCancel(streamId));
+    _activeStreams.remove(streamId);
+    return _trackFuture(_vmService.streamCancel(streamId));
   }
 
   @override
   Future<Success> streamListen(String streamId) {
     Future<Success> future;
-    if (!activeStreams.contains(streamId)) {
-      future = _trackFuture(vmService.streamListen(streamId));
+    if (!_activeStreams.contains(streamId)) {
+      future = _trackFuture(_vmService.streamListen(streamId));
     }
-    activeStreams.add(streamId);
+    _activeStreams.add(streamId);
     return future;
   }
 
   Future<T> _trackFuture<T>(Future<T> future) {
-    activeFutures.add(future);
+    _activeFutures.add(future);
     future.whenComplete(() {
-      activeFutures.remove(future);
-      if (activeFutures.isEmpty && !noPendingFutures.isCompleted) {
+      _activeFutures.remove(future);
+      if (_activeFutures.isEmpty && !noPendingFutures.isCompleted) {
         noPendingFutures.complete(true);
       } else {
         noPendingFutures = new Completer<bool>();
