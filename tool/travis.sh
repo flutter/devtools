4648acc5c311391a8f9fb6beddd6cdff7ab21a66
kenzieschmoll <kenzieschmoll@google.com>
2018-12-20 08:41:03 -0800

    debugging - add ls command

--- tool/travis.sh
+++ tool/travis.sh
@@ -12,7 +12,8 @@ dart --version
 
 # Get Flutter.
 curl https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.0.0-stable.tar.xz -o ../flutter.tar.xz
-tar -xf flutter.tar.xz
+ls -la | grep flutter.tar.xz
+tar -xzf flutter.tar.xz
 export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
 flutter config --no-analytics
 flutter doctor
