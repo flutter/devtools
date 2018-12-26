37dcb8e181d4dcb5cdfc32f0c8b514fe19ee5029
Jacob Richman <jacobr@google.com>
2018-12-26 10:11:59 -0800

    Add comment referencing bug to remove this.

--- lib/ui/fake_flutter/fake_flutter.dart
+++ lib/ui/fake_flutter/fake_flutter.dart
@@ -5,16 +5,13 @@
 /// Collection of utility classes forked from package:flutter and dart:ui
 /// to make it easier to write UI code that can later be ported to flutter.
 ///
-/// At some point you should be able to switch out imports to fake_flutter
-/// with imports to package:flutter.
+/// Once Hummingbird launches, this library should be removed completely.
+/// https://github.com/flutter/devtools/issues/69
+///
 /// Functionality such as I18N and shadows have been arbitrarily ripped out of
 /// these classes as it would be hard to implement writing directly to
 /// dart:html.
 library fake_flutter;
-
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 import 'package:meta/meta.dart';
 
