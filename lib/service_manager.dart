14bdbcb678ce106b22fee22f139cb2a38dcf1272
Kenzie Schmoll <43759233+kenzieschmoll@users.noreply.github.com>
2019-01-10 12:06:48 -0800

    Small adjustment to service registration stream controller. (#110)

--- lib/service_manager.dart
+++ lib/service_manager.dart
@@ -35,7 +35,10 @@ class ServiceConnectionManager {
 
   final Map<String, StreamController<bool>> _serviceRegistrationController =
       <String, StreamController<bool>>{};
-  final Map<String, List<String>> registeredMethodsForService = {};
+  final Map<String, List<String>> _registeredMethodsForService = {};
+
+  Map<String, List<String>> get registeredMethodsForService =>
+      _registeredMethodsForService;
 
   IsolateManager _isolateManager;
   ServiceExtensionManager _serviceExtensionManager;
@@ -64,7 +67,7 @@ class ServiceConnectionManager {
     String isolateId,
     Map args,
   }) async {
-    final registered = registeredMethodsForService[name] ?? const [];
+    final registered = _registeredMethodsForService[name] ?? const [];
     if (registered.length != 1) {
       throw Exception('Expected one registered service for "$name" but found '
           '${registered.length}');
@@ -82,7 +85,7 @@ class ServiceConnectionManager {
     String isolateId,
     Map args,
   }) async {
-    final registered = registeredMethodsForService[name] ?? const [];
+    final registered = _registeredMethodsForService[name] ?? const [];
     if (registered.isNotEmpty) {
       return Future.wait(registered.map((String method) {
         return service.callMethod(method, isolateId: isolateId, args: args);
@@ -96,7 +99,7 @@ class ServiceConnectionManager {
     String name,
     void onData(bool value),
   ) {
-    if (registeredMethodsForService.containsKey(name) && onData != null) {
+    if (_registeredMethodsForService.containsKey(name) && onData != null) {
       onData(true);
     }
     final StreamController<bool> streamController =
@@ -105,12 +108,12 @@ class ServiceConnectionManager {
   }
 
   StreamController<bool> _getServiceRegistrationController(String name) {
-    return _getStream(
+    return _getStreamController(
       name,
       _serviceRegistrationController,
       onFirstListenerSubscribed: () {
         _serviceRegistrationController[name]
-            .add(registeredMethodsForService.containsKey(name));
+            .add(_registeredMethodsForService.containsKey(name));
       },
     );
   }
@@ -130,12 +133,14 @@ class ServiceConnectionManager {
 
       service.onServiceEvent.listen((e) {
         if (e.kind == EventKind.kServiceRegistered) {
-          registeredMethodsForService
-              .putIfAbsent(e.service, () => [])
-              .add(e.method);
-          final StreamController<bool> streamController =
-              _getServiceRegistrationController(e.service);
-          streamController.add(true);
+          if (!_registeredMethodsForService.containsKey(e.service)) {
+            _registeredMethodsForService[e.service] = [e.method];
+            final StreamController<bool> streamController =
+                _getServiceRegistrationController(e.service);
+            streamController.add(true);
+          } else {
+            _registeredMethodsForService[e.service].add(e.method);
+          }
         }
       });
 
@@ -564,7 +569,7 @@ class ServiceExtensionManager {
   }
 
   StreamController<bool> _getServiceExtensionController(String name) {
-    return _getStream(
+    return _getStreamController(
       name,
       _serviceExtensionController,
       onFirstListenerSubscribed: () {
@@ -579,7 +584,7 @@ class ServiceExtensionManager {
 
   StreamController<ServiceExtensionState> _getServiceExtensionStateController(
       String name) {
-    return _getStream(
+    return _getStreamController(
       name,
       _serviceExtensionStateController,
       onFirstListenerSubscribed: () {
@@ -598,17 +603,17 @@ class ServiceExtensionManager {
   }
 }
 
-/// Given a map of Strings to StreamControllers [streams], get the stream for
-/// the given name. If it does not exist, initialize a generic stream and map it
-/// to the name.
-StreamController<T> _getStream<T>(
-    String name, Map<String, StreamController<T>> streams,
+/// Given a map of Strings to StreamControllers [streamControllers], get the
+/// stream controller for the given name. If it does not exist, initialize a
+/// generic stream controller and map it to the name.
+StreamController<T> _getStreamController<T>(
+    String name, Map<String, StreamController<T>> streamControllers,
     {@required void onFirstListenerSubscribed()}) {
-  streams.putIfAbsent(
+  streamControllers.putIfAbsent(
     name,
     () => StreamController<T>.broadcast(onListen: onFirstListenerSubscribed),
   );
-  return streams[name];
+  return streamControllers[name];
 }
 
 class ServiceExtensionState {
