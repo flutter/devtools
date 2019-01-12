71349b1f4d19219c3924f87c8defce595bde18c6
Devon Carew <devoncarew@google.com>
2019-01-12 12:20:15 -0800

    use a non-animating icon for 'track widget rebuilds'

--- lib/ui/icons.dart
+++ lib/ui/icons.dart
@@ -114,6 +114,8 @@ class FlutterIcons {
   static const Icon extractMethod =
       UrlIcon('/icons/preview/extract_method.png');
 
+
+  static const Icon greyProgr = UrlIcon('/icons/perf/GreyProgr@2x.png');
   static const Icon greyProgress = UrlIcon('/icons/perf/grey_progress.gif');
   static const Icon redProgress = UrlIcon('/icons/perf/red_progress.gif');
   static const Icon yellowProgress = UrlIcon('/icons/perf/yellow_progress.gif');
