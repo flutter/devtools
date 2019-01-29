497ad14b99a6ced2bd1d7a537ca9a7e7ce753697
Devon Carew <devoncarew@google.com>
2019-01-29 12:33:01 -0800

    remove older assumpion of a default port

--- lib/framework/framework_core.dart
+++ lib/framework/framework_core.dart
@@ -20,9 +20,11 @@ class FrameworkCore {
   }
 
   static void initVmService(
-      void errorReporter(String title, dynamic error)) async {
-    // Identify port so that we can connect the VmService.
+    void errorReporter(String title, dynamic error),
+  ) async {
     int port;
+
+    // Identify the port so that we can connect to the VM service.
     if (window.location.search.isNotEmpty) {
       final Uri uri = Uri.parse(window.location.toString());
       final String portStr = uri.queryParameters['port'];
@@ -30,18 +32,22 @@ class FrameworkCore {
         port = int.tryParse(portStr);
       }
     }
-    port ??= 8100;
 
-    final Completer<Null> finishedCompleter = Completer<Null>();
+    if (port != null) {
+      final Completer<Null> finishedCompleter = Completer<Null>();
 
-    try {
-      final VmServiceWrapper service =
-          await connect('localhost', port, finishedCompleter);
-      if (serviceManager != null) {
-        await serviceManager.vmServiceOpened(service, finishedCompleter.future);
+      try {
+        final VmServiceWrapper service =
+            await connect('localhost', port, finishedCompleter);
+        if (serviceManager != null) {
+          await serviceManager.vmServiceOpened(
+            service,
+            onClosed: finishedCompleter.future,
+          );
+        }
+      } catch (e) {
+        errorReporter('Unable to connect to service on port $port', e);
       }
-    } catch (e) {
-      errorReporter('Unable to connect to service on port $port', e);
     }
   }
 }
