00b42ba5c0ea421cb7cbe2b4d6905460de143943
Danny Tuppeny <danny@tuppeny.com>
2019-01-29 21:04:55 +0000

    Add a script to serve pre-built app, and a build task to build it (#178)
    * Add a script to serve pre-built app, and a build task to build it
    
    * Print output as JSON
    
    * Add copyright
    
    * Add --port and --machine options
    
    * Rename serve.dart -> devools.dart
    
    * Add devtools to executables list
    
    * Remove unused variable

---
+++ bin/devtools.dart
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http_server/http_server.dart' show VirtualDirectory;
+import 'package:path/path.dart';
+import 'package:args/args.dart';
+
+const argHelp = 'help';
+const argMachine = 'machine';
+const argPort = 'port';
+
+final argParser = new ArgParser()
+  ..addFlag(argHelp, hide: true)
+  ..addFlag(argMachine,
+      negatable: false,
+      abbr: 'm',
+      help: 'Sets output format to JSON for consumption in tools')
+  ..addOption(argPort, abbr: 'p', help: 'Port to serve the web application on');
+
+var machineMode = false;
+final webroot = join(dirname(dirname(Platform.script.toFilePath())), 'build');
+
+Future<void> main(List<String> arguments) async {
+  final args = argParser.parse(arguments);
+  if (args[argHelp]) {
+    print(argParser.usage);
+    return;
+  }
+
+  machineMode = args[argMachine];
+  final virDir = new VirtualDirectory(webroot);
+
+  // Set up a directory handler to serve index.html files.
+  virDir.allowDirectoryListing = true;
+  virDir.directoryHandler = (dir, request) {
+    final indexUri = new Uri.file(dir.path).resolve('index.html');
+    virDir.serveFile(new File(indexUri.toFilePath()), request);
+  };
+
+  final port = args[argPort] != null ? int.tryParse(args[argPort]) ?? 0 : 0;
+  final server = await HttpServer.bind('127.0.0.1', port);
+
+  virDir.serve(server);
+  printOutput(
+    'Listening at http://${server.address.host}:${server.port}',
+    {
+      'method': 'server.started',
+      'params': {'host': server.address.host, 'port': server.port}
+    },
+  );
+}
+
+void printOutput(String message, Object json) {
+  print(machineMode ? jsonEncode(json) : message);
+}
