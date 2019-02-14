b5679850e8a40f255c07913446ca99832eafc2f9
Danny Tuppeny <danny@tuppeny.com>
2019-02-14 17:30:59 +0000

    Handle missing getScripts (#287)
    * When connected VM service doesn't support getScripts, show "unavailable" in badge
    
    ... and don't throw an error.
    
    Fixes #276.
    
    * Temporarily skip tests when getScripts isn't available
    
    * Don't fetch versions if not required
    
    * Use '-' instead of 'Unavailable'

--- packages/devtools/lib/src/service_manager.dart
+++ packages/devtools/lib/src/service_manager.dart
@@ -33,6 +33,16 @@ class ServiceConnectionManager {
 
   final Completer<Null> serviceAvailable = Completer();
 
+  VmServiceCapabilities _serviceCapabilities;
+  Future<VmServiceCapabilities> get serviceCapabilities async {
+    if (_serviceCapabilities == null) {
+      await serviceAvailable.future;
+      final version = await service.getVersion();
+      _serviceCapabilities = new VmServiceCapabilities(version);
+    }
+    return _serviceCapabilities;
+  }
+
   final Map<String, StreamController<bool>> _serviceRegistrationController =
       <String, StreamController<bool>>{};
   final Map<String, List<String>> _registeredMethodsForService = {};
@@ -660,4 +670,12 @@ class ServiceExtensionState {
   // For boolean service extensions, [enabled] should equal [value].
   final bool enabled;
   final dynamic value;
+}
+
+class VmServiceCapabilities {
+  VmServiceCapabilities(this.version);
+  final Version version;
+
+  bool get supportsGetScripts =>
+      version.major > 3 || (version.major == 3 && version.minor >= 12);
 }
