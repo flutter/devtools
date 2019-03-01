839df04326a8e1614513402e5c7d44f73f046e45
Terry Lucas <terry@google.com>
2019-03-01 06:50:13 -0800

    Prepare for v0.012.release

--- packages/devtools/lib/devtools.dart
+++ packages/devtools/lib/devtools.dart
@@ -5,4 +5,4 @@
 /// The DevTools application version.
 // Note: when updating this, please update the corresponding version in the
 // pubspec.
-const String version = '0.0.11';
+const String version = '0.0.12';
