bbdb1bc38442c93aba8f1b7cf3e7e1f8eedcbd57
Kenzie Schmoll <kenzieschmoll@google.com>
2019-04-29 14:09:23 -0700

    Add link to dart:html bug.

--- packages/devtools/lib/src/timeline/event_details.dart
+++ packages/devtools/lib/src/timeline/event_details.dart
@@ -41,6 +41,8 @@ class EventDetails extends CoreElement {
     // The size of the event details section will change as the splitter is
     // is moved. Observe resizing so that we can rebuild the flame chart canvas
     // as necessary.
+    // TODO(kenzie): clean this code up when
+    // https://github.com/dart-lang/html/issues/102 is fixed.
     final observer =
         html.ResizeObserver(allowInterop((List<dynamic> entries, _) {
       _details.uiEventDetails.flameChart.updateForContainerResize();
