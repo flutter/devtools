fdd0315d03da2359d0a1b95f0dd570deb8cf405b
Kenzie Schmoll <kenzieschmoll@google.com>
2019-04-30 13:37:13 -0700

    fix indenting.

--- packages/devtools/lib/src/inspector/inspector.dart
+++ packages/devtools/lib/src/inspector/inspector.dart
@@ -199,18 +199,14 @@ class InspectorScreen extends Screen {
                 target: '_blank;'))
             ..add(span(text: ' is not enabled. '))
             ..add(span(
-                text:
-                    '''This feature allows the Flutter inspector to present the 
-                    widget tree in a manner similar to how the UI was defined in
-                    your source code. Without it, the tree of nodes in the 
-                    widget tree are much deeper, and it can be more difficult to
-                    understand how the runtime widget hierarchy corresponds to 
-                    your application\’s UI.''')),
-          div(
-              text:
-                  '''To fix this, relaunch your application by running 'flutter 
-                    run --track-widget-creation' (or run your application from 
-                    VS Code or IntelliJ).'''),
+                text: '''This feature allows the Flutter inspector to present 
+the widget tree in a manner similar to how the UI was defined in your source
+code. Without it, the tree of nodes in the widget tree are much deeper, and it
+can be more difficult to understand how the runtime widget hierarchy corresponds
+to your application\’s UI.''')),
+          div(text: '''To fix this, relaunch your application by running 
+'flutter run --track-widget-creation' (or run your application from VS Code or
+IntelliJ).'''),
         ]);
       });
     }
