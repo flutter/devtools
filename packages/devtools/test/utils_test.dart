160e146a64daa45dd732fa463ca789adba577ecc
Kenzie Schmoll <43759233+kenzieschmoll@users.noreply.github.com>
2019-04-30 09:19:54 -0700

    Trim trailing characters from Vm Service uris (#608)
    * Trim trailing characters from Vm Service uris

--- packages/devtools/test/utils_test.dart
+++ packages/devtools/test/utils_test.dart
@@ -215,6 +215,29 @@ void main() {
         equals('ԪElement.updateChildԪ'),
       );
     });
+
+    test('getTrimmedUri', () {
+      expect(
+        getTrimmedUri('http://127.0.0.1:60667/72K34Xmq0X0=/#/vm').toString(),
+        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+      );
+      expect(
+        getTrimmedUri('http://127.0.0.1:60667/72K34Xmq0X0=').toString(),
+        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+      );
+      expect(
+        getTrimmedUri('http://127.0.0.1:60667/72K34Xmq0X0=/   ').toString(),
+        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+      );
+      expect(
+        getTrimmedUri('http://127.0.0.1:60667').toString(),
+        equals('http://127.0.0.1:60667'),
+      );
+      expect(
+        getTrimmedUri('http://127.0.0.1:60667/').toString(),
+        equals('http://127.0.0.1:60667/'),
+      );
+    });
   });
 }
 
