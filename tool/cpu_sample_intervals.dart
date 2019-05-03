53a6d35554ab45241399be2e865d10e13e37470c
Kenzie Schmoll <kenzieschmoll@google.com>
2019-05-03 14:32:15 -0700

    review comments

--- tool/cpu_sample_intervals.dart
+++ tool/cpu_sample_intervals.dart
@@ -2,20 +2,19 @@ import 'dart:convert';
 import 'dart:io';
 
 /// Outputs the time intervals between adjacent cpu samples.
-/// 
+///
 /// A json file path is required as a command line argument.
 /// Ex: dart cpu_sample_intervals.dart ~/Downloads/example_json.dart
 void main(List<String> arguments) async {
   if (arguments.isEmpty) {
-    print('''
-    You must specify a json input file path.
-    Ex: dart cpu_sample_intervals.dart ~/Downloads/example_json.dart
-    ''');
+    print('You must specify a json input file path.\n'
+        'Ex: dart cpu_sample_intervals.dart ~/Downloads/example.json');
     return;
   }
 
   final File file = File(arguments.first);
-  final Map<String, dynamic> timelineDump = jsonDecode(await file.readAsString());
+  final Map<String, dynamic> timelineDump =
+      jsonDecode(await file.readAsString());
   final cpuSampleTraceEvents = timelineDump['cpuProfile']['traceEvents'];
 
   final List<int> deltas = [];
