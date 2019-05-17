00185684085100f82b75037a17d24a708cf5884e
Kenzie Schmoll <kenzieschmoll@google.com>
2019-05-17 13:27:56 -0700

    Store resolved_urls for each stack frame.

--- packages/devtools/test/cpu_profile_protocol_test.dart
+++ packages/devtools/test/cpu_profile_protocol_test.dart
@@ -63,11 +63,13 @@ void main() {
         id: 'id_0',
         name: 'parent',
         category: 'Dart',
+        url: 'url',
       );
       final child = CpuStackFrame(
         id: 'id_1',
         name: 'child',
         category: 'Dart',
+        url: 'url',
       );
       expect(parent.children, isEmpty);
       expect(child.parent, isNull);
@@ -90,31 +92,37 @@ final CpuStackFrame stackFrame_0 = CpuStackFrame(
   id: 'id_0',
   name: '0',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 0;
 final CpuStackFrame stackFrame_1 = CpuStackFrame(
   id: 'id_1',
   name: '1',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 0;
 final CpuStackFrame stackFrame_2 = CpuStackFrame(
   id: 'id_2',
   name: '2',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 1;
 final CpuStackFrame stackFrame_3 = CpuStackFrame(
   id: 'id_3',
   name: '3',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 0;
 final CpuStackFrame stackFrame_4 = CpuStackFrame(
   id: 'id_4',
   name: '4',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 1;
 final CpuStackFrame stackFrame_5 = CpuStackFrame(
   id: 'id_5',
   name: '5',
   category: 'Dart',
+  url: 'url',
 )..exclusiveSampleCount = 1;
 
 final testStackFrame = stackFrame_0
@@ -166,88 +174,105 @@ final sampleResponse = Response.parse({
     '140357727781376-1': {
       'category': 'Dart',
       'name': 'thread_start',
+      'resolvedUrl': '',
     },
     '140357727781376-2': {
       'category': 'Dart',
       'name': '_pthread_start',
       'parent': '140357727781376-1',
+      'resolvedUrl': '',
     },
     '140357727781376-3': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase',
+      'name': '_drawFrame',
       'parent': '140357727781376-2',
+      'resolvedUrl': 'org-dartlang-sdk:///flutter/lib/ui/hooks.dart',
     },
     '140357727781376-4': {
       'category': 'Dart',
-      'name': 'BuildOwner.buildScope',
+      'name': '_WidgetsFlutterBinding.draw',
       'parent': '140357727781376-3',
+      'resolvedUrl': 'file:///path/to/flutter/packages/flutter/lib/src/scheduler/binding.dart',
     },
     '140357727781376-5': {
       'category': 'Dart',
-      'name': 'Element.rebuild',
+      'name': 'RendererBinding.drawFrame',
       'parent': '140357727781376-4',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/widgets/binding.dart',
     },
     // Branches off to different sample.
     '140357727781376-6': {
       'category': 'Dart',
-      'name': 'BuildOwner.finalizeTree',
+      'name': '_RenderProxyBox.paint',
       'parent': '140357727781376-3',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/proxy_box.dart',
     },
     '140357727781376-7': {
       'category': 'Dart',
-      'name': 'Timeline.finishSync',
+      'name': 'PaintingContext.paintChild',
       'parent': '140357727781376-6',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/object.dart',
     },
     '140357727781376-8': {
       'category': 'Dart',
       'name': '_SyncBlock.finish',
       'parent': '140357727781376-7',
+      'resolvedUrl': '',
     },
     // Root of new sample.
     '140357727781376-9': {
       'category': 'Dart',
       'name': '[Truncated]',
+      'resolvedUrl': '',
     },
     '140357727781376-10': {
       'category': 'Dart',
       'name': 'RenderObject._getSemanticsForParent.<anonymous closure>',
       'parent': '140357727781376-9',
+      'resolvedUrl': 'file:///path/to/flutter/packages/flutter/lib/src/rendering/object.dart',
     },
     '140357727781376-11': {
       'category': 'Dart',
       'name': 'RenderObject._getSemanticsForParent',
       'parent': '140357727781376-10',
+      'resolvedUrl': 'file:///path/to/flutter/packages/flutter/lib/src/rendering/object.dart',
     },
     // Branches off to different sample.
     '140357727781376-12': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase_handleDrawFrame',
+      'name': 'RenderPhysicalModel.paint',
       'parent': '140357727781376-9',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/proxy_box.dart',
     },
     '140357727781376-13': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase&Gesture.handleDrawFrame',
+      'name': 'RenderCustomMultiChildLayoutBox.paint',
       'parent': '140357727781376-12',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/custom_layout.dart',
     },
     '140357727781376-14': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase&Gesture._invokeFrameCallback',
+      'name': '_RenderCustomMultiChildLayoutBox.defaultPaint',
       'parent': '140357727781376-13',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/box.dart',
     },
     '140357727781376-15': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase&Gesture._invokeFrameCallback',
+      'name': 'RenderObject._paintWithContext',
       'parent': '140357727781376-14',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/object.dart',
     },
     '140357727781376-16': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase&Gesture._invokeFrameCallback',
+      'name': 'RenderStack.paintStack',
       'parent': '140357727781376-14',
+      'resolvedUrl': 'path/to/flutter/packages/flutter/lib/src/rendering/stack.dart',
     },
     '140357727781376-17': {
-      'category': 'Dart',
+      'category': '[Stub] OneArgCheckInlineCache',
       'name': '_WidgetsFlutterBinding&BindingBase&Gesture._invokeFrameCallback',
       'parent': '140357727781376-16',
+      'resolvedUrl': '',
     },
   },
   'traceEvents': [
@@ -347,20 +372,25 @@ final sampleResponseWithMissingLeafFrame = Response.parse({
     '140357727781376-1': {
       'category': 'Dart',
       'name': 'thread_start',
+      'resolvedUrl': '',
     },
     '140357727781376-2': {
       'category': 'Dart',
       'name': '_pthread_start',
       'parent': '140357727781376-1',
+      'resolvedUrl': '',
     },
     '140357727781376-3': {
       'category': 'Dart',
-      'name': '_WidgetsFlutterBinding&BindingBase',
+      'name': '_drawFrame',
+      'parent': '140357727781376-2',
+      'resolvedUrl': 'org-dartlang-sdk:///flutter/lib/ui/hooks.dart',
     },
     '140357727781376-4': {
       'category': 'Dart',
-      'name': 'BuildOwner.buildScope',
+      'name': '_WidgetsFlutterBinding&BindingBase',
       'parent': '140357727781376-3',
+      'resolvedUrl': 'file:///path/to/flutter/packages/flutter/lib/src/scheduler/binding.dart',
     },
   },
   'traceEvents': [
