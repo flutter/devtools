6aa4ab2ee7580155b1f4d5bc8352b9ab053480de
Kenzie Schmoll <kenzieschmoll@google.com>
2019-06-24 17:30:18 -0700

    Add missing import.

--- packages/devtools/lib/src/timeline/cpu_profile_protocol.dart
+++ packages/devtools/lib/src/timeline/cpu_profile_protocol.dart
@@ -1,6 +1,8 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'package:meta/meta.dart';
+
 import '../utils.dart';
 import 'cpu_profile_model.dart';
 
