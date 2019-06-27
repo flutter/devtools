69749d113fc7bdeabec46b4b8310b4a103db5765
Kenzie Schmoll <kenzieschmoll@google.com>
2019-06-27 09:53:01 -0700

    fix import

--- packages/devtools/lib/src/performance/performance_screen.dart
+++ packages/devtools/lib/src/performance/performance_screen.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:devtools/src/ui/html_icon_renderer.dart';
 import 'package:meta/meta.dart';
 
 import '../framework/framework.dart';
