cc52f6351d5670505f3021076b1d877e66c44c9e
Danny Tuppeny <dantup@google.com>
2019-09-02 13:38:12 +0100

    Skip checksum check for Chocolatey Chrome package

--- tool/travis.sh
+++ tool/travis.sh
@@ -21,7 +21,7 @@ if [[ $TRAVIS_OS_NAME == "windows" ]]; then
     echo Installing Google Chrome Stable...
     # Install Chrome via Chocolatey while `addons: chrome` doesn't seem to work on Windows yet
     # https://travis-ci.community/t/installing-google-chrome-stable-but-i-cant-find-it-anywhere/2118
-    choco install googlechrome --acceptlicense --yes --no-progress
+    choco install googlechrome --acceptlicense --yes --no-progress --ignore-checksums
 fi
 
 # In GitBash on Windows, we have to call pub.bat so we alias `pub` in this script to call the
