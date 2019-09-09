1a395b0469fdac34806790fe89f831aa9d48b30a
Kenzie Schmoll <kenzieschmoll@google.com>
2019-09-09 07:30:17 -0700

    update comment

--- packages/devtools/lib/src/logger.dart
+++ packages/devtools/lib/src/logger.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// Logger for DevTools.
+/// Handles console level logging for DevTools.
 class Logger {
   static void log(String message, {LogLevel level = LogLevel.debug}) {
     switch (level) {
