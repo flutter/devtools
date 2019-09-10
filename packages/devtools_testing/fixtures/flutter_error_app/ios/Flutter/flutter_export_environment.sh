bb231de35530c110a3df59d2d8c1570457132884
Jacob MacDonald <jakemac@google.com>
2019-09-10 09:04:46 -0700

    revert flutter_export_environment.sh changes

--- packages/devtools_testing/fixtures/flutter_error_app/ios/Flutter/flutter_export_environment.sh
+++ packages/devtools_testing/fixtures/flutter_error_app/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/google/home/jakemac/flutter"
-export "FLUTTER_APPLICATION_PATH=/usr/local/google/home/jakemac/devtools/packages/devtools_testing/fixtures/flutter_error_app"
+export "FLUTTER_ROOT=/Users/jacobr/git/flutter/2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jacobr/git/devtools/fixtures/flutter_error_app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/google/home/jakemac/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jacobr/git/flutter/2/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
