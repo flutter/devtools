0824aa70b5377a0816549869c2f84d176b8d924d
Dave Shuckerow <djshuckerow@google.com>
2019-09-11 13:19:42 -0700

    Rename routers -> handlers

--- packages/devtools_server/lib/devtools_server.dart
+++ packages/devtools_server/lib/devtools_server.dart
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/routing.dart';
+export 'src/handlers.dart';
 export 'src/server.dart';
