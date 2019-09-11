57ec8add94c22082e2ce2a32a1bd526e26eb4abd
Dave Shuckerow <djshuckerow@google.com>
2019-09-11 14:34:29 -0700

    Clean up handlers.dart

--- packages/devtools_server/lib/src/handlers.dart
+++ packages/devtools_server/lib/src/handlers.dart
@@ -11,12 +11,12 @@ import 'package:shelf_static/shelf_static.dart';
 /// This serves files out from the build results of running a pub build of the
 /// DevTools project.
 Future<shelf.Handler> defaultHandler() async {
-  final Uri resourceUri = await Isolate.resolvePackageUri(
+  final resourceUri = await Isolate.resolvePackageUri(
       Uri(scheme: 'package', path: 'devtools/devtools.dart'));
   final packageDir = path.dirname(path.dirname(resourceUri.toFilePath()));
 
   // Default static handler for all non-package requests.
-  final String buildDir = path.join(packageDir, 'build');
+  final buildDir = path.join(packageDir, 'build');
   final buildHandler = createStaticHandler(
     buildDir,
     defaultDocument: 'index.html',
@@ -24,20 +24,18 @@ Future<shelf.Handler> defaultHandler() async {
 
   // The packages folder is renamed in the pub package so this handler serves
   // out of the `pack` folder.
-  final String packagesDir = path.join(packageDir, 'build', 'pack');
+  final packagesDir = path.join(packageDir, 'build', 'pack');
   final packHandler = createStaticHandler(
     packagesDir,
     defaultDocument: 'index.html',
   );
 
   // Make a handler that delegates based on path.
-  FutureOr<shelf.Response> handler(shelf.Request request) {
+  return (shelf.Request request) {
     return request.url.path.startsWith('packages/')
         // request.change here will strip the `packages` prefix from the path
         // so it's relative to packHandler's root.
         ? packHandler(request.change(path: 'packages'))
         : buildHandler(request);
-  }
-
-  return handler;
+  };
 }
