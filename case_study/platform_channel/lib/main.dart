6cb13a91f247fa66883f47624097590a10ee1964
Kenzie Schmoll <kenzieschmoll@google.com>
2019-09-17 14:30:11 -0700

    Add platform channel example to devtools/case_study

---
+++ case_study/platform_channel/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:platform_channel/channel_demo.dart';
+
+void main() => runApp(MyApp());
+
+const platformChannelTitle = 'Platform Channel Demo';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: _HomePage(),
+    );
+  }
+}
+
+class _HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(platformChannelTitle),
+      ),
+      body: Center(
+        child: FlatButton(
+          color: Colors.green,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ChannelDemo()),
+            );
+          },
+          child: const Text(
+            platformChannelTitle,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
