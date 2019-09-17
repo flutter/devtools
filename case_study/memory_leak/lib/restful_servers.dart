6273a9133450c2eabbb52d5b1b79fc652de95675
Terry Lucas <terry@google.com>
2019-09-17 12:41:46 -0700

    Initial memory leak case study.

---
+++ case_study/memory_leak/lib/restful_servers.dart
@@ -0,0 +1,187 @@
+/// All Restful Servers are defined here.
+
+import 'tabs/settings.dart';
+
+/// All servers with RestfulAPI implement this base.
+abstract class RestfulAPI {
+  RestfulAPI(this.previous);
+
+  RestfulAPI previous;
+  RestfulAPI next;
+
+  String uri();
+
+  dynamic findData(dynamic data);
+
+  String display(dynamic data, int index);
+}
+
+/// StarWars information server.
+class StarWars extends RestfulAPI {
+  StarWars([int index = 1]) : super(currentRestfulAPI) {
+    switch (index) {
+      case 0:
+        _defaultUri = StarWars.filmsUri;
+        break;
+      case 1:
+        _defaultUri = StarWars.peopleUri;
+        break;
+      case 2:
+        _defaultUri = StarWars.planetsUri;
+        break;
+      case 3:
+        _defaultUri = StarWars.speciesUri;
+        break;
+      case 4:
+        _defaultUri = StarWars.starshipsUri;
+        break;
+      case 5:
+        _defaultUri = StarWars.vehiclesUri;
+        break;
+      default:
+        _defaultUri = StarWars.peopleUri;
+    }
+  }
+
+  static const peopleUri = 'https://swapi.co/api/people';
+  static const vehiclesUri = 'https://swapi.co/api/vehicles';
+  static const starshipsUri = 'https://swapi.co/api/starships';
+  static const planetsUri = 'https://swapi.co/api/planets';
+  static const filmsUri = 'https://swapi.co/api/films';
+  static const speciesUri = 'https://swapi.co/api/species';
+
+  String _defaultUri;
+
+  @override
+  String uri() => _defaultUri;
+
+  @override
+  dynamic findData(dynamic data) => data['results'];
+
+  @override
+  String display(dynamic data, int index) {
+    final isFilm = _defaultUri == StarWars.filmsUri;
+    return data == null ? '' : data[index][isFilm ? 'title' : 'name'];
+  }
+}
+
+/// CitiBike NYCA single public API that shows location, status and current
+/// availability for all stations in the New York City bike sharing imitative.
+class CitiBikesNYC extends RestfulAPI {
+  CitiBikesNYC() : super(currentRestfulAPI);
+
+  static const citiBikesUri =
+      'https://feeds.citibikenyc.com/stations/stations.json';
+
+  @override
+  String uri() => citiBikesUri;
+
+  @override
+  dynamic findData(dynamic data) => data['stationBeanList'];
+
+  @override
+  String display(dynamic data, int index) =>
+      data == null ? '' : data[index]['stationName'];
+}
+
+class CityInformation {
+  CityInformation(this.name);
+
+  String name;
+  int size = -1;
+  String state = '???';
+}
+
+/// openewathermap APIs
+///
+///   Docs on Restful APIs https://openweathermap.org/current#data
+///
+///   City IDs are found in these files: http://bulk.openweathermap.org/sample/
+///
+///   Find a particular city use grep e.g.,
+///       > grep -A 10 -B 10 -i "Kansas City" city.list.json
+///
+///   APPID is  ca0dbbe6d72c4d9e8c829abb4a534c16     DevTools_memoryLeaks
+///
+///   Subscribe for free OpenWeatherMap then create an appid using:
+///       https://home.openweathermap.org/api_keys
+class OpenWeatherMapAPI extends RestfulAPI {
+  OpenWeatherMapAPI() : super(currentRestfulAPI);
+
+  static const _baseUrl = 'http://api.openweathermap.org/data/2.5/group?id=';
+  static const _unitsOption = '&units=imperial';
+  static const _appidOption = '&appid=ca0dbbe6d72c4d9e8c829abb4a534c16';
+
+  static const _cityIds = {
+    'Seattle': 5809844,
+    'Atlanta': 4180439,
+    'Portland': 4720131,
+    'Chicago': 4887398,
+    'Orlando': 4167147,
+    'San Francisco': 5391959,
+    'San Jose': 5392171,
+    'Phoenix': 5131135,
+    'Denver': 4853799,
+    'St. Louis': 6157004,
+    'Houston': 4430529,
+    'Dallas': 5722064,
+    'Mobile': 4076598,
+    'Richmond': 5780388,
+    'Hartford': 5255628,
+    'Detroit': 4990729,
+    'Minneapolis': 4275586,
+    'Cleveland': 5248933,
+    'Harrisburg': 5228340,
+    'Bangor': 5244626,
+    'Nashville': 4245376,
+    'Boston': 4183849,
+    'Las Vegas': 5475433,
+    'Kansas City': 4273837,
+  };
+
+  CityInformation firstCity;
+  CityInformation secondCity;
+
+  String _cityIdsList({int initialStart = -1, int count = 20}) {
+    final StringBuffer buff = StringBuffer();
+
+    final int start = initialStart == -1 ? randomSeed() : initialStart;
+    firstCity = CityInformation(_cityIds.keys.toList()[start]);
+
+    secondCity = firstCity;
+
+    final ids = _cityIds.values.toList();
+    for (var index = start; index < count; index++) {
+      if (index != start) buff.write(',');
+      buff.write('${ids[index]}');
+    }
+
+    return buff.toString();
+  }
+
+  // Index starting from 0..3
+  static int randomSeed() => DateTime.now().second % 4;
+
+  String get uri20Cities =>
+      '$_baseUrl${_cityIdsList()}$_unitsOption$_appidOption';
+
+  @override
+  String uri() => uri20Cities;
+
+  @override
+  dynamic findData(dynamic data) => data['list']; // weather group
+
+  static String cityName(dynamic data, int index) => '${data[index]['name']}';
+
+  static String temperature(dynamic data, int index) =>
+      '${data[index]['main']['temp']}';
+
+  static String weather(dynamic data, int index) =>
+      '${data[index]['weather'][0]['main']}';
+
+  @override
+  String display(dynamic data, int index) =>
+      '${OpenWeatherMapAPI.cityName(data, index)} '
+          '${OpenWeatherMapAPI.temperature(data, index)} '
+          '${OpenWeatherMapAPI.weather(data, index)}';
+}
---
+++ case_study/memory_leak/lib/tabs/http_data.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../logging.dart';
+import '../restful_servers.dart';
+import 'settings.dart';
+
+
+/// Create a stateful widget
+class MyGetHttpData extends StatefulWidget {
+  @override
+  MyGetHttpDataState createState() => MyGetHttpDataState();
+}
+
+// Create the state for our stateful widget
+class MyGetHttpDataState extends State<MyGetHttpData> {
+  MyGetHttpDataState() {
+    api = computeUri();
+    currentRestfulAPI?.next = api;
+    currentRestfulAPI = api;
+  }
+
+  final Logging logs = Logging.logging;
+
+  RestfulAPI api;
+  List data;
+
+  RestfulAPI computeUri() {
+    switch (restfulApi) {
+      case 'Weather':
+        return OpenWeatherMapAPI();
+      case 'NYC Bike Sharing':
+        return CitiBikesNYC();
+      case 'StarWars Films':
+        return StarWars(0);
+      case 'StarWars People':
+        return StarWars(1);
+      case 'StarWars Planets':
+        return StarWars(2);
+      case 'StarWars Species':
+        return StarWars(3);
+      case 'StarWars Starships':
+        return StarWars(4);
+      case 'StarWars Vehicles':
+        return StarWars(5);
+      default:
+        return StarWars();
+    }
+  }
+
+  // Function to get the JSON data
+  Future<String> getJSONData() async {
+    logs.add(api.uri());
+
+    final startTime = DateTime.now();
+
+    final response = await http.get(
+        // Encode the url
+        Uri.encodeFull(api.uri()),
+        // Only accept JSON response
+        headers: {'Accept': 'application/json'});
+
+    logs.add('Packet received on ${response.headers['date']} '
+        'content-size ${response.contentLength} bytes '
+        'elapsed time ${DateTime.now().difference(startTime)}');
+
+    // To modify the state of the app, use this method
+    setState(() {
+      // Get the JSON data
+      final dataConvertedToJSON = json.decode(response.body);
+      // Extract the required part and assign it to the global variable named data
+      data = api.findData(dataConvertedToJSON);
+    });
+
+    return 'Successful';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Retrieve JSON Data via HTTP GET'),
+      ),
+      // Create a Listview and load the data when available
+      body: ListView.builder(
+          itemCount: data == null ? 0 : data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Center(
+                  child: Column(
+                // Stretch the cards in horizontal axis
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                      child: Text(
+                        // Read the name field value and set it in the Text widget
+                        api?.display(data, index),
+
+                        // set some style to text
+                        style: TextStyle(
+                            fontSize: 20.0, color: Colors.lightBlueAccent),
+                      ),
+                      // added padding
+                      padding: const EdgeInsets.all(15.0),
+                    ),
+                  )
+                ],
+              )),
+            );
+          }),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Call the getJSONData() method when the app initializes
+    getJSONData();
+  }
+}
