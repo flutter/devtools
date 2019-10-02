c500b9796c1b070804e5a369dae8023c8ad97f5d
Dave Shuckerow <djshuckerow@google.com>
2019-10-02 10:50:42 -0700

    Name the app properly

--- packages/devtools_app/linux/main.cc
+++ packages/devtools_app/linux/main.cc
@@ -60,7 +60,7 @@ int main(int argc, char **argv) {
 
   flutter::FlutterWindowController flutter_controller(icu_data_path);
   flutter::WindowProperties window_properties = {};
-  window_properties.title = "Flutter Desktop Example";
+  window_properties.title = "Dart DevTools";
   window_properties.width = 800;
   window_properties.height = 600;
 
