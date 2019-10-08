9dbed1f6ce36bf6a04afb04ffc0cbdfe0a9ba552
Dave Shuckerow <djshuckerow@google.com>
2019-10-08 10:22:26 -0700

    Fix lint warnings

--- packages/devtools_app/lib/flutter_main.dart
+++ packages/devtools_app/lib/flutter_main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
