ea8e59a3266c27d55fe4e059363d0202e6efe71d
Jacob Richman <jacobr@google.com>
2019-10-11 10:47:53 -0700

    Switch fake_flutter implementation to use package:flutter except for the legacy dart:html app. (#1166)

--- packages/devtools_app/lib/src/ui/fake_flutter/assertions.dart
+++ packages/devtools_app/lib/src/ui/fake_flutter/assertions.dart
@@ -1,7 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-part of 'fake_flutter.dart';
+part of '_fake_flutter.dart';
 
 /// Error class used to report Flutter-specific assertion failures and
 /// contract violations.
--- packages/devtools_app/lib/src/ui/fake_flutter/foundation.dart
+++ packages/devtools_app/lib/src/ui/fake_flutter/foundation.dart
@@ -1,7 +1,7 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-part of 'fake_flutter.dart';
+part of '_fake_flutter.dart';
 
 /// Signature of callbacks that have no arguments and return no data.
 typedef VoidCallback = void Function();
