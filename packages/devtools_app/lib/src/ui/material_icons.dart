f392b88fbe54c315d091616e97c2f37cded5ccaf
Jacob Richman <jacob314@gmail.com>
2019-10-20 18:49:54 -0700

    Add codePoint to MaterialIcon.

--- packages/devtools_app/lib/src/ui/material_icons.dart
+++ packages/devtools_app/lib/src/ui/material_icons.dart
@@ -23,18 +23,25 @@ const DevToolsIcon record =
 
 const DevToolsIcon stop = MaterialIcon('stop', defaultButtonIconColor);
 
+// TODO(jacobr): remove this class completely once the migration to Flutter
+// desktop is complete and just use Flutter native support for Material icons.
 /// Class for icons consistent with
 /// https://docs.flutter.io/flutter/material/Icons-class.html
 class MaterialIcon extends DevToolsIcon {
   const MaterialIcon(
     this.text,
     this.color, {
+    this.codePoint,
     this.fontSize = 18,
     this.iconWidth = 18,
     this.angle = 0.0,
   });
 
   final String text;
+
+  /// If the codePoint is specified, use it directly on platforms that can run
+  /// Flutter natively rather than depending on the text.
+  final int codePoint;
   final Color color;
   final int fontSize;
   final double angle;
@@ -50,6 +57,6 @@ class FlutterMaterialIcons {
   static DevToolsIcon getIconForCodePoint(int charCode) {
     final String code = String.fromCharCode(charCode);
     return _iconCache.putIfAbsent(
-        code, () => MaterialIcon(code, defaultForeground));
+        code, () => MaterialIcon(code, defaultForeground, codePoint: charCode));
   }
 }
