712bf843e13835bc1790f2f066a4dd0ecc7fe0b0
Dave Shuckerow <djshuckerow@google.com>
2019-10-23 11:57:52 -0700

    Add failure test cases

--- packages/devtools_app/test/flutter/service_extension_widgets_test.dart
+++ packages/devtools_app/test/flutter/service_extension_widgets_test.dart
@@ -9,15 +9,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import '../support/mocks.dart';
-import 'initializer_test.dart';
 import 'wrappers.dart';
 
 void main() {
   MockServiceManager mockServiceManager;
   setUp(() {
     mockServiceManager = MockServiceManager();
-    registerServiceExtension(mockServiceManager, hotReload);
-    registerServiceExtension(mockServiceManager, hotRestart);
 
     setGlobal(
       ServiceConnectionManager,
@@ -25,13 +22,19 @@ void main() {
     );
   });
   group('Hot Reload Button', () {
-    testWidgets('performs a hot reload when pressed',
-        (WidgetTester tester) async {
-      int reloads = 0;
+    int reloads = 0;
+
+    setUp(() {
+      reloads = 0;
       when(mockServiceManager.performHotReload()).thenAnswer((invocation) {
         reloads++;
         return Future<void>.value();
       });
+    });
+
+    testWidgets('performs a hot reload when pressed',
+        (WidgetTester tester) async {
+      registerServiceExtension(mockServiceManager, hotReload);
       final button = HotReloadButton();
       await tester.pumpWidget(wrap(Scaffold(body: Center(child: button))));
       expect(find.byWidget(button), findsOneWidget);
@@ -41,16 +44,40 @@ void main() {
       await tester.pumpAndSettle();
       expect(reloads, 1);
     });
+
+    testWidgets(
+        'does not perform a hot reload when the extension is not registered.',
+        (WidgetTester tester) async {
+      registerServiceExtension(
+        mockServiceManager,
+        hotReload,
+        serviceAvailable: false,
+      );
+      final button = HotReloadButton();
+      await tester.pumpWidget(wrap(Scaffold(body: Center(child: button))));
+      expect(find.byWidget(button), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(reloads, 0);
+      await tester.tap(find.byWidget(button));
+      await tester.pumpAndSettle();
+      expect(reloads, 0);
+    });
   });
 
   group('Hot Restart Button', () {
-    testWidgets('performs a hot restart when pressed',
-        (WidgetTester tester) async {
-      int restarts = 0;
+    int restarts = 0;
+
+    setUp(() {
+      restarts = 0;
       when(mockServiceManager.performHotRestart()).thenAnswer((invocation) {
         restarts++;
         return Future<void>.value();
       });
+    });
+
+    testWidgets('performs a hot restart when pressed',
+        (WidgetTester tester) async {
+      registerServiceExtension(mockServiceManager, hotRestart);
       final button = HotRestartButton();
       await tester.pumpWidget(wrap(Scaffold(body: Center(child: button))));
       expect(find.byWidget(button), findsOneWidget);
@@ -60,14 +87,35 @@ void main() {
       await tester.pumpAndSettle();
       expect(restarts, 1);
     });
+
+    testWidgets(
+        'does not perform a hot restart when the service is not available',
+        (WidgetTester tester) async {
+      registerServiceExtension(
+        mockServiceManager,
+        hotRestart,
+        serviceAvailable: false,
+      );
+      final button = HotRestartButton();
+      await tester.pumpWidget(wrap(Scaffold(body: Center(child: button))));
+      expect(find.byWidget(button), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(restarts, 0);
+      await tester.tap(find.byWidget(button));
+      await tester.pumpAndSettle();
+      expect(restarts, 0);
+    });
   });
 }
 
-void registerServiceExtension(MockServiceManager mockServiceManager,
-    RegisteredServiceDescription description) {
+void registerServiceExtension(
+  MockServiceManager mockServiceManager,
+  RegisteredServiceDescription description, {
+  bool serviceAvailable = true,
+}) {
   when(mockServiceManager.hasRegisteredService(description.service, any))
       .thenAnswer((invocation) {
     final onData = invocation.positionalArguments[1];
-    return Stream<bool>.value(true).listen(onData);
+    return Stream<bool>.value(serviceAvailable).listen(onData);
   });
 }
