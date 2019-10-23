a793c20d271b24f6bf624443b2843c4db09f1070
Dave Shuckerow <djshuckerow@google.com>
2019-10-23 16:27:28 -0700

    Try loading special content for the debugger screen

--- packages/devtools_app/lib/debugger_html_plugin.dart
+++ packages/devtools_app/lib/debugger_html_plugin.dart
@@ -4,27 +4,52 @@
 
 // We actually use dart:html here, no shim.
 // This code may only be compiled into the web app.
-import 'dart:html' as html;
-
 import 'dart:ui' as web_ui;
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:html_shim/html.dart' as html;
+
+import 'src/debugger/html_debugger_screen.dart';
+import 'src/ui/html_elements.dart';
 
 /// Debugger HTML view.
 ///
 /// This is code that will run as a web-only Flutter plugin.
 class DebuggerHtmlPlugin {
+  DebuggerHtmlPlugin();
+
   static void registerWith(Registrar registrar) {
+    final instance = DebuggerHtmlPlugin();
     // ignore:undefined_prefixed_name
     web_ui.platformViewRegistry
-        .registerViewFactory('DebuggerFlutterPlugin', build);
+        .registerViewFactory('DebuggerFlutterPlugin', instance.build);
   }
 
   /// Builds the html content of the debugger plugin.
   ///
   /// [viewId] is used to distinguish between multiple instances of the same
   /// view, such as video players.  We can ignore it on DevTools.
-  static html.Element build(int viewId) {
-    return html.DivElement()..text = 'Hello world!';
+  html.Element build(int viewId) {
+    return html.IFrameElement()
+      ..src = 'debugger_screen.html'
+      ..style.border = '0';
+    print('Building html framework');
+    // final framework = HtmlFramework();
+    print('Built framework, building screen.');
+    final screen = HtmlDebuggerScreen();
+    print('Building html contents');
+    final element = screen.createContent(null);
+    print('Element: ${element.element}');
+    element.attribute('full');
+    final div = html.DivElement();
+    html.HttpRequest.getString('debugger_screen.html').then((response) {
+      final fullContent = html.Element.html(response);
+      final content = fullContent.querySelector('#content')
+        ..children.clear()
+        ..children.add(element.element);
+
+      div.replaceWith(content);
+    });
+    return div;
   }
 }
