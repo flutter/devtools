9895b7069d557dae4cfb8c0e40b94687e128aee6
Dave Shuckerow <djshuckerow@google.com>
2019-10-25 13:10:02 -0700

    Travis needs the generated plugin registrant to run tests

---
+++ packages/devtools_app/lib/generated_plugin_registrant.dart
@@ -0,0 +1,13 @@
+//
+// Generated file. Do not edit.
+//
+import 'dart:ui';
+
+import 'package:devtools_app/debugger_html_plugin.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins(PluginRegistry registry) {
+  DebuggerHtmlPlugin.registerWith(registry.registrarFor(DebuggerHtmlPlugin));
+  registry.registerMessageHandler();
+}
