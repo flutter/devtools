f21e14250ab3579c09c6ea67f4bd5285d785c1f5
David Shuckerow <DaveShuckerow@users.noreply.github.com>
2019-10-25 16:37:05 -0700

    Move flutter/inspector/diagnostics to inspector/flutter/diagnostics to go with the other inspector code (#1260)

--- packages/devtools_app/lib/src/inspector/flutter/inspector_tree_flutter.dart
+++ packages/devtools_app/lib/src/inspector/flutter/inspector_tree_flutter.dart
@@ -9,9 +9,9 @@ import 'package:flutter/material.dart';
 import 'package:pedantic/pedantic.dart';
 
 import '../../flutter/collapsible_mixin.dart';
-import '../../flutter/inspector/diagnostics.dart';
 import '../diagnostics_node.dart';
 import '../inspector_tree.dart';
+import 'diagnostics.dart';
 
 /// Presents a [TreeNode].
 class _InspectorTreeRowWidget extends StatefulWidget {
