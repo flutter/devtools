65618389ba5ebd7650d9016376f7adad1a7498af
Dave Shuckerow <djshuckerow@google.com>
2019-10-29 12:10:24 -0700

    Move split into its own file

--- packages/devtools_app/test/flutter/split_test.dart
+++ packages/devtools_app/test/flutter/split_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:devtools_app/src/flutter/common_widgets.dart';
+import 'package:devtools_app/src/flutter/split.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
