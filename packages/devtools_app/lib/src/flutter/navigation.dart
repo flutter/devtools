2132e8e5df3b073a7dfa9d4969f206e42abf0fde
Dave Shuckerow <djshuckerow@google.com>
2019-10-30 14:38:04 -0700

    Add tests to the navigation utility

--- packages/devtools_app/lib/src/flutter/navigation.dart
+++ packages/devtools_app/lib/src/flutter/navigation.dart
@@ -4,17 +4,34 @@
 
 import 'package:flutter/material.dart';
 
+import '../ui/theme.dart' as devtools_theme;
+
 /// Returns [routeName] with [queryParameters] appended as [Uri] query
 /// parameters.
 ///
 /// If not overridden, this will preserve the theme parameter across
 /// navigations.
+///
+///
+/// This will fail because we can't determine what the theme value for the app
+/// is.
 String routeNameWithQueryParams(BuildContext context, String routeName,
     [Map<String, String> queryParameters]) {
   final newQueryParams =
       queryParameters == null ? null : Map.of(queryParameters);
-  final previousQueryParams =
-      Uri.parse(ModalRoute.of(context).settings.name ?? '').queryParameters;
+
+  String previousQuery;
+  if (context == null) {
+    // We allow null context values to make easy pure-VM tests.
+    previousQuery = '';
+  } else {
+    previousQuery = ModalRoute.of(context).settings.name;
+    // When this function is invoked from an unnamed context,
+    // infer from the global theme configuration.
+    previousQuery ??= _inferThemeParameter;
+  }
+
+  final previousQueryParams = Uri.parse(previousQuery).queryParameters;
   // Preserve the theme across app-triggered navigations.
   if (newQueryParams != null &&
       !newQueryParams.containsKey('theme') &&
@@ -25,3 +42,18 @@ String routeNameWithQueryParams(BuildContext context, String routeName,
       .replace(queryParameters: newQueryParams)
       .toString();
 }
+
+/// Infers the app's theme from a global constant.
+///
+/// When calling from an unnamed route, we can't infer the theme
+/// value from the Uri. For example,
+///
+/// ```dart
+/// Navigator.of(context).push(MaterialPageRoute(builder: (innerContext) {
+///   routeNameWithQueryParams(innerContext, '/foo');
+/// }));
+/// ```
+///
+/// ModalRoute.of`(innerContext)` returns the unnamed page route.
+String get _inferThemeParameter =>
+    devtools_theme.isDarkTheme ? '/unnamedRoute?theme=dark' : '/unnamedRoute';
