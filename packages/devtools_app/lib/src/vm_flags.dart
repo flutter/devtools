048b93280c40179a63b4351e0f2d35346e4d0005
Kenzie Schmoll <kenzieschmoll@google.com>
2019-11-05 15:22:41 -0800

    Add link to VM flags in SDK.

--- packages/devtools_app/lib/src/vm_flags.dart
+++ packages/devtools_app/lib/src/vm_flags.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Defined in SDK: https://github.com/dart-lang/sdk/blob/master/runtime/vm/flag_list.h.
 const asyncDebugger = 'async_debugger';
 const causalAsyncStacks = 'causal_async_stacks';
+
+// Defined in SDK: https://github.com/dart-lang/sdk/blob/master/runtime/vm/profiler.cc#L36
 const profilePeriod = 'profile_period';
