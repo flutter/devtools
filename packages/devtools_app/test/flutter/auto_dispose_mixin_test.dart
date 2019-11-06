e8b2958e0f053c41ad97ebb5843a4094c0c07e4c
Dave Shuckerow <djshuckerow@google.com>
2019-11-05 17:34:37 -0800

    Tests

--- packages/devtools_app/test/flutter/auto_dispose_mixin_test.dart
+++ packages/devtools_app/test/flutter/auto_dispose_mixin_test.dart
@@ -42,6 +42,81 @@ class _AutoDisposedWidgetState extends State<AutoDisposedWidget>
 }
 
 void main() {
+  group('Disposer', () {
+    test('disposes streams', () {
+      final disposer = Disposer();
+      final controller1 = StreamController(sync: true);
+      final controller2 = StreamController(sync: true);
+      var c1Events = 0;
+      var c2Events = 0;
+      disposer.autoDispose(controller1.stream.listen((data) {
+        c1Events++;
+      }));
+      disposer.autoDispose(controller2.stream.listen((data) {
+        c2Events++;
+      }));
+      expect(c1Events, 0);
+      expect(c2Events, 0);
+      controller1.add(null);
+      expect(c1Events, 1);
+      expect(c2Events, 0);
+      controller1.add(null);
+      controller2.add(null);
+      expect(c1Events, 2);
+      expect(c2Events, 1);
+      disposer.cancel();
+
+      // Make sure stream subscriptions are cancelled.
+      controller1.add(null);
+      controller2.add(null);
+      expect(c1Events, 2);
+      expect(c2Events, 1);
+    });
+
+    test('disposes listeners', () {
+      final disposer = Disposer();
+      final notifier = ValueNotifier<int>(42);
+      final values = <int>[];
+      disposer.addAutoDisposeListener(notifier, () {
+        values.add(notifier.value);
+      });
+      // ignore: invalid_use_of_protected_member
+      expect(notifier.hasListeners, isTrue);
+      notifier.value = 13;
+      expect(values.length, equals(1));
+      expect(values.last, equals(13));
+      notifier.value = 15;
+      expect(values.length, equals(2));
+      expect(values.last, equals(15));
+      // ignore: invalid_use_of_protected_member
+      expect(notifier.hasListeners, isTrue);
+      disposer.cancel();
+      // ignore: invalid_use_of_protected_member
+      expect(notifier.hasListeners, isFalse);
+      notifier.value = 17;
+      // Verify listener not fired.
+      expect(values.length, equals(2));
+      expect(values.last, equals(15));
+
+      // Add a new listener:
+      disposer.addAutoDisposeListener(notifier, () {
+        values.add(notifier.value);
+      });
+      // ignore: invalid_use_of_protected_member
+      expect(notifier.hasListeners, isTrue);
+      notifier.value = 19;
+      expect(values.length, equals(3));
+      expect(values.last, equals(19));
+      disposer.cancel();
+
+      // ignore: invalid_use_of_protected_member
+      expect(notifier.hasListeners, isFalse);
+      notifier.value = 21;
+      expect(values.length, equals(3));
+      expect(values.last, equals(19));
+    });
+  });
+
   testWidgets('Test stream auto dispose', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     final key = GlobalKey();
@@ -91,19 +166,5 @@ void main() {
     // Verify listener not fired.
     expect(values.length, equals(2));
     expect(values.last, equals(15));
-    controller.addAutoDisposeListener(notifier, () {
-      values.add(notifier.value);
-    });
-    // ignore: invalid_use_of_protected_member
-    expect(notifier.hasListeners, isTrue);
-    notifier.value = 19;
-    expect(values.length, equals(3));
-    expect(values.last, equals(19));
-    controller.dispose();
-    // ignore: invalid_use_of_protected_member
-    expect(notifier.hasListeners, isFalse);
-    notifier.value = 21;
-    expect(values.length, equals(3));
-    expect(values.last, equals(19));
   });
 }
