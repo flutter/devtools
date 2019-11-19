aff9dada5e3dbcfb0d1988e4f5151d51beffa7b3
Dave Shuckerow <djshuckerow@google.com>
2019-11-19 13:13:09 -0800

    Add json prettyprinting detail view to the log details page.
    Cross-fade between log details views

--- packages/devtools_app/test/flutter/logging_screen_test.dart
+++ packages/devtools_app/test/flutter/logging_screen_test.dart
@@ -51,6 +51,8 @@ void main() {
     testWidgets('builds with no data', (WidgetTester tester) async {
       await tester.pumpWidget(wrap(Builder(builder: screen.build)));
       expect(find.byType(LoggingScreenBody), findsOneWidget);
+      expect(find.byType(LogsTable), findsOneWidget);
+      expect(find.byType(LogDetails), findsOneWidget);
       expect(find.text('Clear logs'), findsOneWidget);
       expect(find.byType(StructuredErrorsToggle), findsOneWidget);
     });
@@ -81,26 +83,129 @@ void main() {
       // TODO(djshuckerow): Hook up fake extension state querying.
     });
 
-    testWidgets('shows most recent logs first', (WidgetTester tester) async {
-      when(mockLoggingController.data).thenReturn(fakeLogData);
-      await tester.pumpWidget(wrap(Builder(builder: screen.build)));
-      await tester.pumpAndSettle();
-      expect(find.byType(LogsTable), findsOneWidget);
-      expect(
-        find.byKey(ValueKey(fakeLogData.last)),
-        findsOneWidget,
-        reason: 'the most recently added logdata should show in the list by '
-            'default.',
-      );
-      expect(
-        find.byKey(ValueKey(fakeLogData.first)),
-        findsNothing,
-        reason: 'the least recently added logdata should be at the top of the '
-            'list, hidden from view.',
-      );
+    group('with data', () {
+      setUp(() {
+        when(mockLoggingController.data).thenReturn(fakeLogData);
+      });
+
+      testWidgets('shows most recent logs first', (WidgetTester tester) async {
+        await tester.pumpWidget(wrap(Builder(builder: screen.build)));
+        await tester.pumpAndSettle();
+        expect(find.byType(LogsTable), findsOneWidget);
+        expect(
+          find.byKey(ValueKey(fakeLogData.last)),
+          findsOneWidget,
+          reason: 'the most recently added logdata should show in the list by '
+              'default.',
+        );
+        expect(
+          find.byKey(ValueKey(fakeLogData.first)),
+          findsNothing,
+          reason:
+              'the least recently added logdata should be at the top of the '
+              'list, hidden from view.',
+        );
+      });
+
+      testWidgets('can show non-computing log data',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(wrap(Builder(builder: screen.build)));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[996])));
+        await tester.pumpAndSettle();
+        expect(
+          find.text('log event 996'),
+          findsNWidgets(2),
+          reason: 'The log details should be visible both in the table and '
+              'the details section.',
+        );
+      });
+
+      testWidgets('can show null log data', (WidgetTester tester) async {
+        await tester.pumpWidget(wrap(Builder(builder: screen.build)));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[997])));
+        await tester.pumpAndSettle();
+      });
+
+      testWidgets('can compute details of non-json log data',
+          (WidgetTester tester) async {
+        const index = 998;
+        final log = fakeLogData[index];
+
+        await tester.pumpWidget(wrap(Builder(builder: screen.build)));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(log)));
+        await tester.pump();
+        expect(
+          find.text(nonJsonOutput),
+          findsNothing,
+          reason:
+              "The details of the log haven't computed yet, so they shouldn't "
+              'be available.',
+        );
+
+        await tester.pumpAndSettle();
+        expect(find.text(nonJsonOutput), findsOneWidget);
+      });
+
+      testWidgets('can show details of json log data',
+          (WidgetTester tester) async {
+        const index = 999;
+        bool containsJson(Widget widget) {
+          if (widget is! Text) return false;
+          final text = widget as Text;
+          return text.data.contains('{') && text.data.contains('}');
+        }
+
+        final findJson = find.descendant(
+          of: find.byType(LogDetails),
+          matching: find.byWidgetPredicate(containsJson),
+        );
+
+        await tester.pumpWidget(wrap(Builder(builder: screen.build)));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[index])));
+        await tester.pump();
+
+        expect(
+          findJson,
+          findsNothing,
+          reason:
+              "The details of the log haven't computed yet, so they shouldn't be available.",
+        );
+
+        await tester.pumpAndSettle();
+        expect(findJson, findsOneWidget);
+      });
     });
   });
 }
 
-final fakeLogData =
-    List<LogData>.generate(1000, (i) => LogData('kind $i', 'log event $i', i));
+const totalLogs = 1000;
+final fakeLogData = List<LogData>.generate(totalLogs, _generate);
+
+LogData _generate(int i) {
+  String details = 'log event $i';
+  String computedDetails;
+  switch (i) {
+    case 999:
+      computedDetails = jsonOutput;
+      break;
+    case 998:
+      computedDetails = nonJsonOutput;
+      break;
+    case 997:
+      details = null;
+      break;
+    default:
+      break;
+  }
+  final detailsComputer = computedDetails == null
+      ? null
+      : () => Future.delayed(const Duration(seconds: 1), () => computedDetails);
+  return LogData('kind $i', details, i, detailsComputer: detailsComputer);
+}
+
+const nonJsonOutput = 'Non-json details for log number 998';
+const jsonOutput = '{\n"Details": "of log event 999",\n"logEvent": "999"\n}\n';
