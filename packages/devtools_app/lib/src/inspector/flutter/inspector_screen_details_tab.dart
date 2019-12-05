0274b45a94467628e9b63be4903e59ebf577985a
Albertus Angga Raharja <albertusangga@google.com>
2019-12-05 14:31:12 -0800

    Remove unnecessary import

--- packages/devtools_app/lib/src/inspector/flutter/inspector_screen_details_tab.dart
+++ packages/devtools_app/lib/src/inspector/flutter/inspector_screen_details_tab.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:devtools_app/src/ui/fake_flutter/_real_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
