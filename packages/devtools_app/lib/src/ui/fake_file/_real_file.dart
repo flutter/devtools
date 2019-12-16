2ae5b62e58302f47d7a3e193cd95c26fa6d5616a
Terry Lucas <terry@google.com>
2019-12-16 14:04:00 -0800

    dartfmt'd

--- packages/devtools_app/lib/src/ui/fake_file/_real_file.dart
+++ packages/devtools_app/lib/src/ui/fake_file/_real_file.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 /// Abstracted local file system access for Flutter Desktop.
 
 import 'package:file/file.dart';
