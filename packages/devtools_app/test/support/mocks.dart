8328a5e09affad641c90cb1a6b5f6c78d9ca5477
Terry Lucas <terry@google.com>
2019-12-17 10:45:11 -0800

    Fix analysis error.

--- packages/devtools_app/test/support/mocks.dart
+++ packages/devtools_app/test/support/mocks.dart
@@ -14,7 +14,7 @@ import 'package:devtools_app/src/service_manager.dart';
 import 'package:devtools_app/src/stream_value_listenable.dart';
 import 'package:devtools_app/src/memory/memory_controller.dart';
 import 'package:devtools_app/src/memory/flutter/memory_controller.dart'
-    as flutterMemory;
+    as flutter_memory;
 import 'package:devtools_app/src/timeline/timeline_controller.dart';
 import 'package:devtools_app/src/timeline/timeline_model.dart';
 import 'package:devtools_app/src/ui/fake_flutter/fake_flutter.dart';
@@ -158,7 +158,7 @@ class MockLoggingController extends Mock implements LoggingController {}
 class MockMemoryController extends Mock implements MemoryController {}
 
 class MockFlutterMemoryController extends Mock
-    implements flutterMemory.MemoryController {}
+    implements flutter_memory.MemoryController {}
 
 class MockTimelineController extends Mock implements TimelineController {}
 
