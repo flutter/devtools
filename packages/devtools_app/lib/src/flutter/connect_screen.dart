96e11f18554099f96a05715ad67ff16bfa243283
Dave Shuckerow <djshuckerow@google.com>
2020-01-07 11:03:23 -0800

    Fix import

--- packages/devtools_app/lib/src/flutter/connect_screen.dart
+++ packages/devtools_app/lib/src/flutter/connect_screen.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pedantic/pedantic.dart';
 
 import '../../src/framework/framework_core.dart';
