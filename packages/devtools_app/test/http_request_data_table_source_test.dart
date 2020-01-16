45c6cfbb9310190ef3f9bba459e4b2c7df4a0891
Ben Konyi <bkonyi@google.com>
2020-01-16 14:11:32 -0800

    Addressed more comments

--- packages/devtools_app/test/http_request_data_table_source_test.dart
+++ packages/devtools_app/test/http_request_data_table_source_test.dart
@@ -22,7 +22,9 @@ void main() {
     setUpAll(() async {
       const testDataPath =
           '../devtools_testing/lib/support/http_request_timeline_test_data.json';
-      httpTestData = jsonDecode(await File(testDataPath).readAsString());
+      httpTestData = jsonDecode(
+        await File(testDataPath).readAsString(),
+      );
       final timeline = Timeline.parse(httpTestData);
       final httpRequests = NetworkController.processHttpTimelineEventsHelper(
           timeline, 0, [], {});
@@ -34,7 +36,10 @@ void main() {
       dataTable = HttpRequestDataTableSource();
     });
 
-    void dataTableListenerScope(Function listener, Function callback) {
+    void dataTableListenerScope(
+      Function listener,
+      Function callback,
+    ) {
       dataTable.addListener(listener);
       callback();
       dataTable.removeListener(listener);
@@ -48,19 +53,41 @@ void main() {
     });
 
     test('time display', () {
-      expect(dataTable.formatDuration(null), 'In Progress');
-      expect(dataTable.formatDuration(const Duration(milliseconds: 1234)),
+      expect(
+        dataTable.formatDuration(null),
+        'In Progress',
+      );
+      expect(
+          dataTable.formatDuration(
+            const Duration(milliseconds: 1234),
+          ),
           '1,234');
 
-      expect(dataTable.formatRequestTime(DateTime(2020, 1, 16, 13, 0, 0)),
+      expect(
+          dataTable.formatRequestTime(
+            DateTime(
+              2020,
+              1,
+              16,
+              13,
+              0,
+              0,
+            ),
+          ),
           '13:00:00 1/16/2020');
     });
 
     test('verify status colors', () {
       const standard = TextStyle();
-      const green = TextStyle(color: Colors.greenAccent);
-      const yellow = TextStyle(color: Colors.yellowAccent);
-      const red = TextStyle(color: Colors.redAccent);
+      const green = TextStyle(
+        color: Colors.greenAccent,
+      );
+      const yellow = TextStyle(
+        color: Colors.yellowAccent,
+      );
+      const red = TextStyle(
+        color: Colors.redAccent,
+      );
 
       // Expect no color for status code < 100.
       expect(
@@ -112,43 +139,80 @@ void main() {
     });
 
     test('select row', () {
-      expect(dataTable.rowCount, 0);
+      expect(
+        dataTable.rowCount,
+        0,
+      );
       dataTable.data = requests;
-      expect(dataTable.rowCount, requests.length);
+      expect(
+        dataTable.rowCount,
+        requests.length,
+      );
 
       DataRow row = dataTable.getRow(0);
-      expect(row.selected, false);
-      expect(dataTable.selectedRowCount, 0);
+      expect(
+        row.selected,
+        false,
+      );
+      expect(
+        dataTable.selectedRowCount,
+        0,
+      );
 
       // Check selection works properly.
       final selectListener = () {
         row = dataTable.getRow(0);
-        expect(row.selected, true);
-        expect(dataTable.selectedRowCount, 1);
+        expect(
+          row.selected,
+          true,
+        );
+        expect(
+          dataTable.selectedRowCount,
+          1,
+        );
       };
-      dataTableListenerScope(selectListener, () => row.onSelectChanged(true));
+      dataTableListenerScope(
+        selectListener,
+        () => row.onSelectChanged(true),
+      );
 
       // Check deselection works properly...
       final deselectListener = () {
         row = dataTable.getRow(0);
-        expect(row.selected, false);
-        expect(dataTable.selectedRowCount, 0);
+        expect(
+          row.selected,
+          false,
+        );
+        expect(
+          dataTable.selectedRowCount,
+          0,
+        );
       };
 
       // with clearSelection
       dataTableListenerScope(
-          deselectListener, () => dataTable.clearSelection());
+        deselectListener,
+        () => dataTable.clearSelection(),
+      );
 
       // and with onSelectChanged(false)
       row.onSelectChanged(true);
       dataTableListenerScope(
-          deselectListener, () => row.onSelectChanged(false));
+        deselectListener,
+        () => row.onSelectChanged(false),
+      );
     });
 
     test('verify rows', () {
-      expect(dataTable.rowCount, 0);
+      expect(
+        dataTable.rowCount,
+        0,
+      );
       dataTable.data = requests;
-      expect(dataTable.rowCount, requests.length);
+      expect(
+        dataTable.rowCount,
+        requests.length,
+      );
 
       // Check each row is in the data table and the data cells contain the
       // correct strings.
@@ -162,22 +226,54 @@ void main() {
           expect(cell.child, isA<Text>());
         }
         final cellsText = cells.map((cell) => cell.child as Text).toList();
-        expect(cellsText[0].data, request.name);
-        expect(cellsText[1].data, request.method);
-        expect(cellsText[2].data, request.status);
-        expect(cellsText[2].style, dataTable.getStatusColor(request.status));
-        expect(cellsText[3].data, dataTable.formatDuration(request.duration));
-        expect(cellsText[4].data,
-            dataTable.formatRequestTime(request.requestTime));
+        expect(
+          cellsText[0].data,
+          request.name,
+        );
+        expect(
+          cellsText[1].data,
+          request.method,
+        );
+        expect(
+          cellsText[2].data,
+          request.status,
+        );
+        expect(
+          cellsText[2].style,
+          dataTable.getStatusColor(
+            request.status,
+          ),
+        );
+        expect(
+          cellsText[3].data,
+          dataTable.formatDuration(
+            request.duration,
+          ),
+        );
+        expect(
+          cellsText[4].data,
+          dataTable.formatRequestTime(
+            request.requestTime,
+          ),
+        );
       }
     });
 
     test('sorting', () {
-      expect(dataTable.rowCount, 0);
+      expect(
+        dataTable.rowCount,
+        0,
+      );
       dataTable.data = requests;
-      expect(dataTable.rowCount, requests.length);
+      expect(
+        dataTable.rowCount,
+        requests.length,
+      );
 
-      void verifyOrder(Function(HttpRequestData) getField, bool ascending) {
+      void verifyOrder(
+        Function(HttpRequestData) getField,
+        bool ascending,
+      ) {
         for (int i = 0; i < dataTable.rowCount - 1; i++) {
           HttpRequestData a = dataTable.data[i];
           HttpRequestData b = dataTable.data[i + 1];
@@ -188,26 +284,57 @@ void main() {
           }
           final fieldA = getField(a);
           final fieldB = getField(b);
-          expect(Comparable.compare(fieldA, fieldB), lessThanOrEqualTo(0));
+          expect(
+            Comparable.compare(fieldA, fieldB),
+            lessThanOrEqualTo(0),
+          );
         }
       }
 
       // Verify sorting both ascending and descending.
-      void verifySorting(Function(HttpRequestData) getField) {
+      void verifySorting(
+        Function(HttpRequestData) getField,
+      ) {
         bool ascending = true;
-        dataTableListenerScope(() => verifyOrder(getField, ascending),
-            () => dataTable.sort(getField, ascending));
+        dataTableListenerScope(
+          () => verifyOrder(
+            getField,
+            ascending,
+          ),
+          () => dataTable.sort(
+            getField,
+            ascending,
+          ),
+        );
 
         ascending = false;
-        dataTableListenerScope(() => verifyOrder(getField, ascending),
-            () => dataTable.sort(getField, ascending));
+        dataTableListenerScope(
+          () => verifyOrder(
+            getField,
+            ascending,
+          ),
+          () => dataTable.sort(
+            getField,
+            ascending,
+          ),
+        );
       }
 
-      verifySorting((data) => data.name);
-      verifySorting((data) => data.method);
-      verifySorting((data) => data.status);
-      verifySorting((data) => data.duration);
-      verifySorting((data) => data.requestTime);
+      verifySorting(
+        (data) => data.name,
+      );
+      verifySorting(
+        (data) => data.method,
+      );
+      verifySorting(
+        (data) => data.status,
+      );
+      verifySorting(
+        (data) => data.duration,
+      );
+      verifySorting(
+        (data) => data.requestTime,
+      );
     });
   });
 }
