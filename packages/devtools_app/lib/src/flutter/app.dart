ffb20f0e825700d37a4e67ee73ca1bd4a12ecd27
Ben Konyi <bkonyi@google.com>
2020-01-16 09:20:43 -0800

    Disable networking page

--- packages/devtools_app/lib/src/flutter/app.dart
+++ packages/devtools_app/lib/src/flutter/app.dart
@@ -21,7 +21,7 @@ import 'scaffold.dart';
 import 'theme.dart';
 
 // TODO(bkonyi): remove this bool when page is ready.
-const showNetworkPage = true;
+const showNetworkPage = false;
 
 /// Top-level configuration for the app.
 @immutable
