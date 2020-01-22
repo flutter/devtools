95cd2bd6942a886700377776b0c39721ad7d1050
Devon Carew <devoncarew@google.com>
2020-01-22 14:57:23 -0800

    create a repo management tool for DevTools (devman) (#1555)
    create a repo management tool for DevTools (`repo_tool`)

---
+++ tool/lib/repo_tool.dart
@@ -0,0 +1,18 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:args/command_runner.dart';
+
+import 'commands/analyze.dart';
+import 'commands/list.dart';
+import 'commands/packages_get.dart';
+
+class DevToolsCommandRunner extends CommandRunner {
+  DevToolsCommandRunner()
+      : super('repo_tool', 'A repo management tool for DevTools.') {
+    addCommand(PackagesGetCommand());
+    addCommand(AnalyzeCommand());
+    addCommand(ListCommand());
+  }
+}
