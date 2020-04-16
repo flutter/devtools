d58182ce8d0021c93481ab790db2ba67e93c3fb7
Danny Tuppeny <danny@tuppeny.com>
2020-04-16 10:42:49 +0100

    Add test that --allow-embedding removes x-frame-options header

--- packages/devtools_app/test/support/devtools_server_driver.dart
+++ packages/devtools_app/test/support/devtools_server_driver.dart
@@ -48,6 +48,7 @@ class DevToolsServerDriver {
   static Future<DevToolsServerDriver> create({
     int port = 0,
     int tryPorts,
+    List<String> additionalArgs = const [],
   }) async {
     // These tests assume that the devtools package is present in a sibling
     // directory of the devtools_app package.
@@ -56,6 +57,7 @@ class DevToolsServerDriver {
       '--machine',
       '--port',
       '$port',
+      ...additionalArgs
     ];
 
     if (tryPorts != null) {
