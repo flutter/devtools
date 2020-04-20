9fda78fd7895c7decb325fcc645c61596ecec613
Helin Shiah <helinx@google.com>
2020-04-20 16:27:43 -0700

    Remove unused convert import

--- packages/devtools_app/test/ansi_up_test.dart
+++ packages/devtools_app/test/ansi_up_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert';
-
 import 'package:ansi_up/ansi_up.dart';
 import 'package:ansicolor/ansicolor.dart';
 import 'package:test/test.dart';
