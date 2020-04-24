37b11261c23754597e5f509aecf3644fda0320f3
Jacob Richman <jacob314@gmail.com>
2020-04-24 13:03:17 -0700

    Fix controller test.

--- packages/devtools_app/test/network_controller_test.dart
+++ packages/devtools_app/test/network_controller_test.dart
@@ -50,7 +50,10 @@ void main() {
           expect(notifier.value, true);
           expect(controller.isPolling, true);
         },
-        callback: () async => await controller.startRecording(),
+        callback: () async {
+          await controller.addClient();
+          await controller.startRecording();
+        },
       );
 
       await addListenerScope(
@@ -70,6 +73,7 @@ void main() {
         },
         callback: () async => await controller.startRecording(),
       );
+      controller.removeClient();
     });
 
     test('process HTTP timeline events', () async {
