530b80fe628bc8d644fd49e9f5159441e919399b
Danny Tuppeny <danny@tuppeny.com>
2020-05-16 20:06:42 +0100

    Remove dart:html from build_release.sh

--- tool/build_release.sh
+++ tool/build_release.sh
@@ -11,23 +11,8 @@ pushd packages/devtools_app
 rm -rf build
 rm -rf ../devtools/build
 flutter pub get
-flutter pub run build_runner build -o web:build --release
-mv ./build/packages ./build/pack
 
-# move release to the devtools package from the devtools_app package for deployment
-mv build ../devtools
-
-# Build the flutter release of the app as well.
-
-rm -rf build
 flutter build web --dart-define=FLUTTER_WEB_USE_SKIA=true --no-tree-shake-icons
-mkdir build/web/flutter
-mv build/web/main.* build/web/flutter/
-
-sed 's|main.dart.js|flutter\/main.dart.js|' build/web/index.html > build/web/tmp.html
-sed 's|<head>|<head><style>.legacy-dart {visibility: hidden;}</style>|' build/web/tmp.html > build/web/flutter.html
-rm build/web/index.html build/web/tmp.html
-
-mv build/web/* ../devtools/build/
+mv build/web ../devtools/build
 
 popd
