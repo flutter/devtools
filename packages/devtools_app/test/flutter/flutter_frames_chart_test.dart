8fe6914decd227c460fa945f1e6129702f7d95e6
Kenzie Schmoll <kenzieschmoll@google.com>
2020-05-27 09:42:57 -0700

    fix lint

--- packages/devtools_app/test/flutter/flutter_frames_chart_test.dart
+++ packages/devtools_app/test/flutter/flutter_frames_chart_test.dart
@@ -9,7 +9,6 @@ import 'package:devtools_app/src/timeline/flutter/flutter_frames_chart.dart';
 import 'package:devtools_app/src/timeline/flutter/timeline_model.dart';
 import 'package:devtools_app/src/timeline/flutter/timeline_controller.dart';
 import 'package:devtools_app/src/ui/colors.dart';
-import 'package:devtools_app/src/utils.dart';
 import 'package:devtools_testing/support/flutter/timeline_test_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
