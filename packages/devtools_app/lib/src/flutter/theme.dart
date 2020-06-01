d8db3efb19dc369ce50628543bc31018ea287744
Devon Carew <devoncarew@google.com>
2020-06-01 07:51:07 -0700

    fps chart tweaks (#2023)

--- packages/devtools_app/lib/src/flutter/theme.dart
+++ packages/devtools_app/lib/src/flutter/theme.dart
@@ -159,6 +159,8 @@ Color titleSolidBackgroundColor(ThemeData theme) {
 final chartBackgroundColor = ThemedColor(Colors.grey[50], Colors.grey[850]);
 const chartAccentColor = ThemedColor(Color(0xFFCCCCCC), Color(0xFF585858));
 const chartTextColor = ThemedColor(Colors.black, Colors.white);
+const chartSubtleColor = ThemedColor(Color(0xFF999999), Color(0xFF8A8A8A));
+const chartFontSizeSmall = 12.0;
 
 final chartLightTypeFace = TypeFace(
   fontFamily: 'OpenSans',
