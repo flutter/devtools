0fab541716d75900dcee3b5f614adf616f4a996c
Kenzie Schmoll <kenzieschmoll@google.com>
2020-07-08 14:43:22 -0700

    no-op to kick off cla

--- packages/devtools_app/lib/devtools.dart
+++ packages/devtools_app/lib/devtools.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The DevTools application version.
+/// The DevTools application version
 // This version should only be updated by running tools/update_version.sh
 // that updates all versions for DevTools.
 // Note: a regexp in tools/update_version.sh matches the following line so
