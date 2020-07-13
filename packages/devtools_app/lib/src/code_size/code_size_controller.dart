aec3488ca9ff05097b7720cf96059378803c8b3b
Kenzie Schmoll <kenzieschmoll@google.com>
2020-07-13 12:05:27 -0700

    add missing import

--- packages/devtools_app/lib/src/code_size/code_size_controller.dart
+++ packages/devtools_app/lib/src/code_size/code_size_controller.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:vm_snapshot_analysis/treemap.dart';
 import 'package:vm_snapshot_analysis/utils.dart';
