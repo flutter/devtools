7191a1f7851b7b8d395f80479371e493717c5388
Danny Tuppeny <danny@tuppeny.com>
2020-07-27 16:28:03 +0100

    Restore "@TestOn('vm')" lines removed as part of organizing imports

--- packages/devtools_app/test/timeline_screen_test.dart
+++ packages/devtools_app/test/timeline_screen_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@TestOn('vm')
 import 'package:devtools_app/src/globals.dart';
 import 'package:devtools_app/src/service_manager.dart';
 import 'package:devtools_app/src/split.dart';
