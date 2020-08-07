71548dd5668f81c3d169a1b173944b1c0b66b891
Kenzie Schmoll <kenzieschmoll@google.com>
2020-08-07 08:35:15 -0700

    Add settings and info actions to connect screen

--- packages/devtools_app/lib/src/scaffold.dart
+++ packages/devtools_app/lib/src/scaffold.dart
@@ -316,7 +316,7 @@ class DevToolsScaffoldState extends State<DevToolsScaffold>
 
     // Add a leading [BulletSpacer] to the actions if the screen is not narrow.
     final actions = List<Widget>.from(widget.actions ?? []);
-    if (!isNarrow && actions.isNotEmpty) {
+    if (!isNarrow && actions.isNotEmpty && widget.tabs.length > 1) {
       actions.insert(0, const BulletSpacer(useAccentColor: true));
     }
 
