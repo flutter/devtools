fdf4ad44336408d5f6518593271e71e5c116f879
Michael Goderbauer <goderbauer@google.com>
2020-08-13 13:26:54 -0700

    re-add import

--- packages/devtools_app/lib/src/custom_pointer_scroll_view.dart
+++ packages/devtools_app/lib/src/custom_pointer_scroll_view.dart
@@ -9,6 +9,7 @@ import 'dart:ui';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 /// A [ScrollView] that uses a single child layout model and allows for custom
