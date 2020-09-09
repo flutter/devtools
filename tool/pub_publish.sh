657e3be67f58b17f0b5778011d82a43817579c10
Jacob Richman <jacob314@gmail.com>
2020-09-09 09:45:10 -0700

    Add pub_publish.sh script and update readme.

---
+++ tool/pub_publish.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -x #echo on
+echo "Publishing devtools_* packages"
+
+pushd packages/devtools_shared
+pub publish --force
+
+popd
+pushd packages/devtools_server
+pub publish --force
+
+popd
+pushd packages/devtools_testing
+pub publish --force
+
+popd
+pushd packages/devtools_app
+pub publish --force
+
+popd
+pushd packages/devtools
+pub publish --force
+
+popd
