6d523d32508eca68cad7038aa68b181f32b007fe
Devon Carew <devoncarew@google.com>
2020-09-21 14:42:43 -0700

    more pubspec.lock surgery

--- packages/devtools_app/test/url_utils_test.dart
+++ packages/devtools_app/test/url_utils_test.dart
@@ -23,66 +23,68 @@ void main() {
         equals('package:flutter_web/lib/src/widgets/binding.dart'),
       );
     });
-  });
 
-  group('normalizeVmServiceUri', () {
-    test('normalizes simple URIs', () {
-      expect(
-        normalizeVmServiceUri('http://127.0.0.1:60667/72K34Xmq0X0=').toString(),
-        equals('http://127.0.0.1:60667/72K34Xmq0X0='),
-      );
-      expect(
-        normalizeVmServiceUri('http://127.0.0.1:60667/72K34Xmq0X0=/   ')
-            .toString(),
-        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
-      );
-      expect(
-        normalizeVmServiceUri('http://127.0.0.1:60667').toString(),
-        equals('http://127.0.0.1:60667'),
-      );
-      expect(
-        normalizeVmServiceUri('http://127.0.0.1:60667/').toString(),
-        equals('http://127.0.0.1:60667/'),
-      );
-    });
+    group('normalizeVmServiceUri', () {
+      test('normalizes simple URIs', () {
+        expect(
+          normalizeVmServiceUri('http://127.0.0.1:60667/72K34Xmq0X0=')
+              .toString(),
+          equals('http://127.0.0.1:60667/72K34Xmq0X0='),
+        );
+        expect(
+          normalizeVmServiceUri('http://127.0.0.1:60667/72K34Xmq0X0=/   ')
+              .toString(),
+          equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+        );
+        expect(
+          normalizeVmServiceUri('http://127.0.0.1:60667').toString(),
+          equals('http://127.0.0.1:60667'),
+        );
+        expect(
+          normalizeVmServiceUri('http://127.0.0.1:60667/').toString(),
+          equals('http://127.0.0.1:60667/'),
+        );
+      });
 
-    test('properly strips leading whitespace and trailing URI fragments', () {
-      expect(
-        normalizeVmServiceUri('  http://127.0.0.1:60667/72K34Xmq0X0=/#/vm')
-            .toString(),
-        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
-      );
-      expect(
-        normalizeVmServiceUri('  http://127.0.0.1:60667/72K34Xmq0X0=/#/vm  ')
-            .toString(),
-        equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
-      );
-    });
+      test('properly strips leading whitespace and trailing URI fragments', () {
+        expect(
+          normalizeVmServiceUri('  http://127.0.0.1:60667/72K34Xmq0X0=/#/vm')
+              .toString(),
+          equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+        );
+        expect(
+          normalizeVmServiceUri('  http://127.0.0.1:60667/72K34Xmq0X0=/#/vm  ')
+              .toString(),
+          equals('http://127.0.0.1:60667/72K34Xmq0X0=/'),
+        );
+      });
 
-    test('properly handles encoded urls', () {
-      expect(
-        normalizeVmServiceUri('http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D')
-            .toString(),
-        equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
-      );
+      test('properly handles encoded urls', () {
+        expect(
+          normalizeVmServiceUri(
+                  'http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D')
+              .toString(),
+          equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
+        );
 
-      expect(
-        normalizeVmServiceUri(
-          'http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D  ',
-        ).toString(),
-        equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
-      );
+        expect(
+          normalizeVmServiceUri(
+            'http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D  ',
+          ).toString(),
+          equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
+        );
 
-      expect(
-        normalizeVmServiceUri(
-          '  http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D   ',
-        ).toString(),
-        equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
-      );
-    });
+        expect(
+          normalizeVmServiceUri(
+            '  http%3A%2F%2F127.0.0.1%3A58824%2FCnvgRrQJG7w%3D   ',
+          ).toString(),
+          equals('http://127.0.0.1:58824/CnvgRrQJG7w='),
+        );
+      });
 
-    test('Returns null when given a non-absolute url', () {
-      expect(normalizeVmServiceUri('my/page'), null);
+      test('Returns null when given a non-absolute url', () {
+        expect(normalizeVmServiceUri('my/page'), null);
+      });
     });
   });
 }
