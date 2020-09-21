9775663ccfa8a16277df2bdd233d8282f82ca47a
Kenzie Schmoll <kenzieschmoll@google.com>
2020-09-21 08:46:44 -0700

    fix formatting

--- packages/devtools_app/lib/src/memory/memory_heap_tree_view.dart
+++ packages/devtools_app/lib/src/memory/memory_heap_tree_view.dart
@@ -347,7 +347,9 @@ class HeapTreeViewState extends State<HeapTree>
                     ? 'Graphing...'
                     : _isSnapshotGrouping
                         ? 'Grouping...'
-                        : _isSnapshotComplete ? 'Done' : '...',
+                        : _isSnapshotComplete
+                            ? 'Done'
+                            : '...',
           ),
         ],
       );
