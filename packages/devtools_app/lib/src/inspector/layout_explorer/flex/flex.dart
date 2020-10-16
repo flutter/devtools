1e49a8a9527a36399fdbfbdb509a51d44d2e692c
Jacob Richman <jacob314@gmail.com>
2020-10-16 14:18:49 -0700

    Fix layout explorer assert attempting to model negative padding.

--- packages/devtools_app/lib/src/inspector/layout_explorer/flex/flex.dart
+++ packages/devtools_app/lib/src/inspector/layout_explorer/flex/flex.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -1165,8 +1166,8 @@ class FlexChildVisualizer extends StatelessWidget {
         opacity: min([state.entranceCurve.value * 5, 1.0]),
         child: Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: (renderSize.width - size.width) / 2,
-            vertical: (renderSize.height - size.height) / 2,
+            horizontal: math.max(0.0, (renderSize.width - size.width) / 2),
+            vertical: math.max(0.0, (renderSize.height - size.height) / 2),
           ),
           child: child,
         ),
