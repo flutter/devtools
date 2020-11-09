62df243ac4858ab5e8421854c74d9323ae1e7505
Jacob Richman <jacob314@gmail.com>
2020-11-09 09:43:55 -0800

    Run pub upgrade and update VMService wrapper.

--- packages/devtools_app/lib/src/vm_service_wrapper.dart
+++ packages/devtools_app/lib/src/vm_service_wrapper.dart
@@ -404,8 +404,8 @@ class VmServiceWrapper implements VmService {
   }
 
   @override
-  Future<Stack> getStack(String isolateId) {
-    return trackFuture('getStack', _vmService.getStack(isolateId));
+  Future<Stack> getStack(String isolateId, {int limit}) {
+    return trackFuture('getStack', _vmService.getStack(isolateId, limit: limit));
   }
 
   @override
