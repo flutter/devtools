26da05764919b6228b5ec377d71f50c34d824801
Jacob Richman <jacob314@gmail.com>
2020-12-30 09:24:57 -0800

    Remove unused import.

--- packages/devtools_app/test/inspector_controller_test.dart
+++ packages/devtools_app/test/inspector_controller_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:test/test.dart';
-
 import 'package:devtools_testing/inspector_controller_test.dart';
 import 'package:devtools_testing/support/flutter_test_driver.dart'
     show FlutterRunConfiguration;
