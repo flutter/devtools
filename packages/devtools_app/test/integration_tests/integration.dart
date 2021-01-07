2f1bf151cbab95a5df339d7d9d887bd05291b43b
Danny Tuppeny <danny@tuppeny.com>
2021-01-07 19:11:05 +0000

    Remove use of Flutter service worker (#2586)

--- packages/devtools_app/test/integration_tests/integration.dart
+++ packages/devtools_app/test/integration_tests/integration.dart
@@ -352,6 +352,7 @@ class WebBuildFixture {
     cliArgs.addAll([
       'build',
       'web',
+      '--pwa-strategy=none',
       '--dart-define=FLUTTER_WEB_USE_SKIA=true',
       '--no-tree-shake-icons'
     ]);
