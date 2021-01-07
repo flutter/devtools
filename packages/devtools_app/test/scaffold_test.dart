444739baf45769f2d4d133bca63bb8be6ca0c177
Jacob Richman <jacob314@gmail.com>
2021-01-07 10:29:14 -0800

    Fix test.

--- packages/devtools_app/test/scaffold_test.dart
+++ packages/devtools_app/test/scaffold_test.dart
@@ -23,6 +23,7 @@ void main() {
     setUp(() {
       mockServiceManager = MockServiceManager();
       when(mockServiceManager.service).thenReturn(null);
+      when(mockServiceManager.hasConnection).thenReturn(false);
       when(mockServiceManager.onStateChange)
           .thenAnswer((_) => const Stream<bool>.empty());
 
