378fcdce9c04a13cc5f7cd03eb465ba942b635da
Dan Field <dnfield@google.com>
2021-01-28 13:05:22 -0800

    grep

--- tool/build_release.sh
+++ tool/build_release.sh
@@ -4,24 +4,36 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-set -x #echo on
+set -ex #echo on
+
+# This avoids requiring an internet connection for CanvasKit at runtime.
+# This URL should be updated to keep in sync with the version from the engine.
+# See https://github.com/flutter/engine/blob/353efcdf3c0ec0ecf0275d55e4a22329397f899f/lib/web_ui/lib/src/engine/canvaskit/initialization.dart#L51-L79,
+# but compare with the code in master for getting the current version.
+function download_canvaskit() {
+  local canvaskit_url=https://unpkg.com/canvaskit-wasm@0.22.0/bin
+
+  local flutter_bin=$(which flutter)
+  local canvaskit_dart_file=$(dirname $flutter_bin)/cache/flutter_web_sdk/lib/_engine/engine/canvaskit/initialization.dart
+  if ! grep -q "$canvaskit_url" "$canvaskit_dart_file"; then
+    echo "CanvasKit $canvaskit_url does not match local web engine copy. Please update before continuing."
+    exit -1
+  fi
+
+  mkdir -p build/web/assets/canvaskit/profiling
+
+  curl $canvaskit_url/canvaskit.js -o build/web/assets/canvaskit/canvaskit.js
+  curl $canvaskit_url/canvaskit.wasm -o build/web/assets/canvaskit/canvaskit.wasm
+  curl $canvaskit_url/profiling/canvaskit.js -o build/web/assets/canvaskit/profiling/canvaskit.js
+  curl $canvaskit_url/profiling/canvaskit.wasm -o build/web/assets/canvaskit/profiling/canvaskit.wasm
+}
 
 pushd packages/devtools_app
 
 rm -rf build
 rm -rf ../devtools/build
 
-# This avoids requiring an internet connection for CanvasKit at runtime.
-# This URL should be updated to keep in sync with the version from the engine.
-# See https://github.com/flutter/engine/blob/353efcdf3c0ec0ecf0275d55e4a22329397f899f/lib/web_ui/lib/src/engine/canvaskit/initialization.dart#L51-L79,
-# but compare with the code in master for getting the current version.
-mkdir -p build/web/assets/canvaskit/profiling
-
-CANVASKIT_URL=https://unpkg.com/canvaskit-wasm@0.22.0/bin
-curl $CANVASKIT_URL/canvaskit.js -o build/web/assets/canvaskit/canvaskit.js
-curl $CANVASKIT_URL/canvaskit.wasm -o build/web/assets/canvaskit/canvaskit.wasm
-curl $CANVASKIT_URL/profiling/canvaskit.js -o build/web/assets/canvaskit/profiling/canvaskit.js
-curl $CANVASKIT_URL/profiling/canvaskit.wasm -o build/web/assets/canvaskit/profiling/canvaskit.wasm
+download_canvaskit
 
 flutter pub get
 
