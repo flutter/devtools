4807285186abb4bb6638115207e234377f594807
Alex <alex_lindroos@hotmail.com>
2021-03-31 16:32:21 +0300

    Memory legends renaming (#2833)
    * Renamed memory legend names(#1081)
    
    * Update goldens(#1081)
    
    * Added a missing comma(#1081)
    
    * Extracted memory legend display strings to consts (#1081)
    
    * Renamed legend name(#1081)
    
    * Added padding in legend container(#1081)

--- packages/devtools_app/lib/src/memory/memory_screen.dart
+++ packages/devtools_app/lib/src/memory/memory_screen.dart
@@ -92,6 +92,19 @@ class MemoryScreen extends Screen {
 
   static const memorySourceMenuItemPrefix = 'Source: ';
 
+  @visibleForTesting
+  static const allocatedDisplay = 'Allocated';
+  @visibleForTesting
+  static const usedDisplay = 'Dart/Flutter';
+  @visibleForTesting
+  static const externalDisplay = 'Dart/Flutter Native';
+  @visibleForTesting
+  static const rssDisplay = 'RSS';
+  @visibleForTesting
+  static const layerDisplay = 'Raster Layer';
+  @visibleForTesting
+  static const pictureDisplay = 'Raster Picture';
+
   static void gaAction({Key key, String name}) {
     final recordName = key != null ? keyName(key) : name;
     assert(recordName != null);
@@ -645,7 +658,7 @@ class MemoryBodyState extends State<MemoryBody>
   final hoverKey = GlobalKey(debugLabel: MemoryScreen.hoverKeyName);
   static const hoverXOffset = 10;
   static const hoverYOffset = 0.0;
-  static const hoverWidth = 203.0;
+  static const hoverWidth = 210.0;
   static const hover_card_border_width = 2.0;
 
   // TODO(terry): Compute below heights dynamically.
@@ -681,7 +694,7 @@ class MemoryBodyState extends State<MemoryBody>
   static const eventLegend = '${base}event_glyph.png';
   static const eventsLegend = '${base}events_glyph.png';
 
-  static const capacityLegend = '${base}capacity_glyph.png';
+  static const allocatedLegend = '${base}capacity_glyph.png';
   static const usedLegend = '${base}used_glyph.png';
   static const externalLegend = '${base}external_glyph.png';
   static const rssLegend = '${base}rss_glyph.png';
@@ -707,10 +720,12 @@ class MemoryBodyState extends State<MemoryBody>
       final rightSide = <Widget>[];
       if (name != null && image != null) {
         rightSide.addAll([
-          Container(
-            padding: EdgeInsets.fromLTRB(leftEdge, 0, 0, 2),
-            width: legendTextWidth + leftEdge,
-            child: Text(name, style: legendEntry),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(leftEdge, 0, 0, 2),
+              width: legendTextWidth + leftEdge,
+              child: Text(name, style: legendEntry),
+            ),
           ),
           const PaddedDivider(
             padding: EdgeInsets.only(left: denseRowSpacing),
@@ -728,11 +743,15 @@ class MemoryBodyState extends State<MemoryBody>
       rowChildren.addAll(legendPart(name2, image2, 20.0));
     }
 
-    return Container(
-        padding: const EdgeInsets.fromLTRB(10, 0, 0, 2),
-        child: Row(
-          children: rowChildren,
-        ));
+    return Expanded(
+      child: Container(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 2),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: rowChildren,
+          ),
+        ),
+    );
   }
 
   Widget hoverRow({
@@ -1013,13 +1032,6 @@ class MemoryBodyState extends State<MemoryBody>
       : nf.format(number);
 
   List<Widget> displayVmDataInHover(ChartsValues chartsValues) {
-    const rssDisplay = 'RSS';
-    const capacityDisplay = 'Capacity';
-    const usedDisplay = 'Used';
-    const externalDisplay = 'External';
-    const layerDisplay = 'Raster Layer';
-    const pictureDisplay = 'Raster Picture';
-
     final results = <Widget>[];
 
     final vmDataDisplayed = <String, String>{};
@@ -1027,22 +1039,22 @@ class MemoryBodyState extends State<MemoryBody>
     final data = chartsValues.vmData;
 
     final rssValueDisplay = formatNumeric(data[rssJsonName]);
-    vmDataDisplayed['$rssDisplay $rssValueDisplay'] = rssLegend;
+    vmDataDisplayed['${MemoryScreen.rssDisplay} $rssValueDisplay'] = rssLegend;
 
     final capacityValueDisplay = formatNumeric(data[capacityJsonName]);
-    vmDataDisplayed['$capacityDisplay $capacityValueDisplay'] = capacityLegend;
+    vmDataDisplayed['${MemoryScreen.allocatedDisplay} $capacityValueDisplay'] = allocatedLegend;
 
     final usedValueDisplay = formatNumeric(data[usedJsonName]);
-    vmDataDisplayed['$usedDisplay $usedValueDisplay'] = usedLegend;
+    vmDataDisplayed['${MemoryScreen.usedDisplay} $usedValueDisplay'] = usedLegend;
 
     final externalValueDisplay = formatNumeric(data[externalJsonName]);
-    vmDataDisplayed['$externalDisplay $externalValueDisplay'] = externalLegend;
+    vmDataDisplayed['${MemoryScreen.externalDisplay} $externalValueDisplay'] = externalLegend;
 
     final layerValueDisplay = formatNumeric(data[rasterLayerJsonName]);
-    vmDataDisplayed['$layerDisplay $layerValueDisplay'] = rasterLayerLegend;
+    vmDataDisplayed['${MemoryScreen.layerDisplay} $layerValueDisplay'] = rasterLayerLegend;
 
     final pictureValueDisplay = formatNumeric(data[rasterPictureJsonName]);
-    vmDataDisplayed['$pictureDisplay $pictureValueDisplay'] =
+    vmDataDisplayed['${MemoryScreen.pictureDisplay} $pictureValueDisplay'] =
         rasterPictureLegend;
 
     for (var entry in vmDataDisplayed.entries) {
@@ -1226,7 +1238,7 @@ class MemoryBodyState extends State<MemoryBody>
             ? legendHeight2Charts
             : legendHeight1Chart,
         child: Container(
-          padding: const EdgeInsets.fromLTRB(0, 5, 0, 8),
+          padding: const EdgeInsets.fromLTRB(0, 5, 5, 8),
           decoration: BoxDecoration(
             color: Colors.black,
             border: Border.all(color: Colors.yellow),
@@ -1262,10 +1274,10 @@ class MemoryBodyState extends State<MemoryBody>
                 padding: const EdgeInsets.fromLTRB(5, 0, 0, 4),
                 child: Text('Memory Legend', style: legendHeading),
               ),
-              legendRow(name1: 'Capacity', image1: capacityLegend),
-              legendRow(name1: 'Used', image1: usedLegend),
-              legendRow(name1: 'External', image1: externalLegend),
-              legendRow(name1: 'RSS', image1: rssLegend),
+              legendRow(name1: MemoryScreen.allocatedDisplay, image1: allocatedLegend),
+              legendRow(name1: MemoryScreen.usedDisplay, image1: usedLegend),
+              legendRow(name1: MemoryScreen.externalDisplay, image1: externalLegend),
+              legendRow(name1: MemoryScreen.rssDisplay, image1: rssLegend),
               if (controller.isAndroidChartVisible)
                 const Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 9)),
               if (controller.isAndroidChartVisible)
