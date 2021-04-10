ee681b567f9872f87b009147cd720de826312de0
Terry Lucas <terry@google.com>
2021-04-10 08:37:46 -0700

    Updated hover computation.

--- packages/devtools_app/lib/src/memory/memory_screen.dart
+++ packages/devtools_app/lib/src/memory/memory_screen.dart
@@ -654,9 +654,9 @@ class MemoryBodyState extends State<MemoryBody>
   static const hover_card_border_width = 2.0;
 
   // TODO(terry): Compute below heights dynamically.
-  static const hoverHeightMinimum = 40.0;
-  static const hoverItemHeight = 19.0;
-  // One extension event to display (3 lines).
+  static const hoverHeightMinimum = 42.0;
+  static const hoverItemHeight = 18.0;
+  // One extension event to display (4 lines).
   static const hoverOneEventsHeight = 82.0;
   // Many extension events to display.
   static const hoverEventsHeight = 120.0;
