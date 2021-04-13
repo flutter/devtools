336288849103b3d291b7b8cc331b5701b9f4293a
Alex Lindroos <alex.lindroos@codemate.com>
2021-04-13 11:52:18 +0300

    Matching landing page title to subheadings (#2874)

--- packages/devtools_app/lib/src/scaffold.dart
+++ packages/devtools_app/lib/src/scaffold.dart
@@ -398,7 +398,7 @@ class DevToolsScaffoldState extends State<DevToolsScaffold>
       automaticallyImplyLeading: false,
       title: Text(
         title,
-        style: Theme.of(context).textTheme.subtitle1,
+        style: Theme.of(context).textTheme.headline5,
       ),
       centerTitle: false,
       toolbarHeight: defaultToolbarHeight,
