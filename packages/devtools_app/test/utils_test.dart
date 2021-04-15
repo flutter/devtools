414e8ddabb4f81f28e2c79c4bd378579db54f51d
Kenzie Schmoll <43759233+kenzieschmoll@users.noreply.github.com>
2021-04-15 10:20:25 -0700

    Use "fuzzy match" in debugger libraries search (#2904)
    * Use "fuzzy match" in debugger libraries search

--- packages/devtools_app/test/utils_test.dart
+++ packages/devtools_app/test/utils_test.dart
@@ -1072,6 +1072,24 @@ void main() {
         expect(() => immutableList.shuffle(), throwsException);
       });
     });
+
+    group('StringExtension', () {
+      test('fuzzyMatch', () {
+        const str = 'hello_world_file';
+        expect(str.caseInsensitiveFuzzyMatch('h'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('o_'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('hw'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('hwf'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('_e'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('HWF'), isTrue);
+        expect(str.caseInsensitiveFuzzyMatch('_E'), isTrue);
+
+        expect(str.caseInsensitiveFuzzyMatch('hwfh'), isFalse);
+        expect(str.caseInsensitiveFuzzyMatch('hfw'), isFalse);
+        expect(str.caseInsensitiveFuzzyMatch('gello'), isFalse);
+        expect(str.caseInsensitiveFuzzyMatch('files'), isFalse);
+      });
+    });
   });
 }
 
