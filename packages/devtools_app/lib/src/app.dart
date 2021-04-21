75f754dde7426f3cba5bead69518fc1ba0069011
Kenzie Schmoll <kenzieschmoll@google.com>
2021-04-21 10:13:34 -0700

    Add report feedback button to top level actions in DevTools

--- packages/devtools_app/lib/src/app.dart
+++ packages/devtools_app/lib/src/app.dart
@@ -169,6 +169,7 @@ class DevToolsAppState extends State<DevToolsApp> {
         analyticsProvider: widget.analyticsProvider,
         actions: [
           OpenSettingsAction(),
+          ReportFeedbackButton(),
           OpenAboutAction(),
         ],
       );
@@ -217,6 +218,7 @@ class DevToolsAppState extends State<DevToolsApp> {
                     HotRestartButton(),
                   ],
                   OpenSettingsAction(),
+                  ReportFeedbackButton(),
                   OpenAboutAction(),
                 ],
               ),
@@ -255,6 +257,7 @@ class DevToolsAppState extends State<DevToolsApp> {
           ideTheme: ideTheme,
           actions: [
             OpenSettingsAction(),
+            ReportFeedbackButton(),
             OpenAboutAction(),
           ],
         );
@@ -431,6 +434,31 @@ class OpenSettingsAction extends StatelessWidget {
   }
 }
 
+class ReportFeedbackButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DevToolsTooltip(
+      tooltip: 'Report feedback',
+      child: InkWell(
+        onTap: () async {
+          ga.select(devToolsMain, feedbackButton);
+          await launchUrl(
+              devToolsExtensionPoints.issueTrackerLink().url, context);
+        },
+        child: Container(
+          width: DevToolsScaffold.actionWidgetSize,
+          height: DevToolsScaffold.actionWidgetSize,
+          alignment: Alignment.center,
+          child: const Icon(
+            Icons.bug_report,
+            size: actionsIconSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class DevToolsAboutDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -469,7 +497,7 @@ class DevToolsAboutDialog extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
     return InkWell(
       onTap: () async {
-        ga.select(devToolsMain, feedback);
+        ga.select(devToolsMain, feedbackLink);
         await launchUrl(reportIssuesLink.url, context);
       },
       child: Text(reportIssuesLink.display, style: linkTextStyle(colorScheme)),
