5dc759d5e01493cabe3f916688a6438256ad10aa
Remi Rousselet <darky12s@gmail.com>
2021-04-21 18:19:46 +0100

    revert unvoluntary fixture change

--- packages/devtools_testing/fixtures/provider_app/lib/main.dart
+++ packages/devtools_testing/fixtures/provider_app/lib/main.dart
@@ -27,7 +27,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => Counter()),
