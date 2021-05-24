556563090331d75caeac4d693c4f7072b48223a3
Helin Shiah <helinx@google.com>
2021-05-24 12:36:52 -0700

    Allow ideTheme to be null

--- packages/devtools_app/lib/src/theme.dart
+++ packages/devtools_app/lib/src/theme.dart
@@ -32,11 +32,11 @@ ThemeData themeFor({
     primaryTextTheme: (theme != null
             ? theme.primaryTextTheme.merge(colorTheme.primaryTextTheme)
             : colorTheme.primaryTextTheme)
-        .apply(fontSizeFactor: ideTheme.fontSizeFactor),
+        .apply(fontSizeFactor: ideTheme?.fontSizeFactor),
     textTheme: (theme != null
             ? theme.textTheme.merge(colorTheme.textTheme)
             : colorTheme.textTheme)
-        .apply(fontSizeFactor: ideTheme.fontSizeFactor),
+        .apply(fontSizeFactor: ideTheme?.fontSizeFactor),
   );
 }
 
