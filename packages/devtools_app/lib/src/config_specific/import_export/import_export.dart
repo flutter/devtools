6dec52e2126c6004ea7650c3f913cc5aaf5a0e1c
Kenzie Schmoll <kenzieschmoll@google.com>
2021-06-02 16:44:29 -0700

    formatting

--- packages/devtools_app/lib/src/config_specific/import_export/import_export.dart
+++ packages/devtools_app/lib/src/config_specific/import_export/import_export.dart
@@ -107,7 +107,8 @@ abstract class ExportController {
     // This is a workaround to guarantee that DevTools exports are compatible
     // with other trace viewers (catapult, perfetto, chrome://tracing), which
     // require a top level field named "traceEvents".
-    if (activeScreenId == PerformanceScreen.id || activeScreenId == LegacyPerformanceScreen.id) {
+    if (activeScreenId == PerformanceScreen.id ||
+        activeScreenId == LegacyPerformanceScreen.id) {
       final traceEvents = List<Map<String, dynamic>>.from(
           contents[PerformanceData.traceEventsKey]);
       _contents[PerformanceData.traceEventsKey] = traceEvents;
