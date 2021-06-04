29fdfda1c80ca3feb74a5d892b924da868a96e17
Kenzie Schmoll <43759233+kenzieschmoll@users.noreply.github.com>
2021-06-04 11:41:25 -0700

    Fix a bug with exporting the performance screen (#3076)
    * Fix a bug with exporting the performance screen

--- packages/devtools_app/lib/src/config_specific/import_export/import_export.dart
+++ packages/devtools_app/lib/src/config_specific/import_export/import_export.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import '../../../devtools.dart';
 import '../../globals.dart';
 import '../../notifications.dart';
+import '../../performance/legacy/performance_screen.dart';
 import '../../performance/performance_model.dart';
 import '../../performance/performance_screen.dart';
 import '../../utils.dart';
@@ -106,7 +107,8 @@ abstract class ExportController {
     // This is a workaround to guarantee that DevTools exports are compatible
     // with other trace viewers (catapult, perfetto, chrome://tracing), which
     // require a top level field named "traceEvents".
-    if (activeScreenId == PerformanceScreen.id) {
+    if (activeScreenId == PerformanceScreen.id ||
+        activeScreenId == LegacyPerformanceScreen.id) {
       final traceEvents = List<Map<String, dynamic>>.from(
           contents[PerformanceData.traceEventsKey]);
       _contents[PerformanceData.traceEventsKey] = traceEvents;
