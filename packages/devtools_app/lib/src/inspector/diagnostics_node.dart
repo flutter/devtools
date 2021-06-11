9c84c81d16f146b93f84ccc9ecb7ac88cf16dac0
Jacob Richman <jacobr@google.com>
2021-06-11 11:00:59 -0700

    Surface extra field in RemoteDiagnosticsNode.

--- packages/devtools_app/lib/src/inspector/diagnostics_node.dart
+++ packages/devtools_app/lib/src/inspector/diagnostics_node.dart
@@ -274,7 +274,21 @@ class RemoteDiagnosticsNode extends DiagnosticableTree {
     if (rawValues == null) {
       return null;
     }
-    return rawValues.toList();
+    return List<String>.from(rawValues);
+  }
+
+  /// Whether each of the values is itself a primitive value.
+  ///
+  /// For example, bool|num|string are primitive values. This is useful as for
+  /// non-primitive values, the user may want to view the value with an
+  /// interactive object debugger view to get more information on what the value
+  /// is.
+  List<bool> get primitiveValues {
+    final List<Object> rawValues = json['primitiveValues'];
+    if (rawValues == null) {
+      return null;
+    }
+    return List<bool>.from(rawValues);
   }
 
   bool get hasValues => json.containsKey('values');
@@ -706,6 +720,8 @@ class InspectorSourceLocation {
   }
 }
 
+// TODO(jacobr): rename this class or merge with SourcePosition class in
+// debugger_model.dart
 class SourcePosition {
   const SourcePosition({this.file, this.line, this.column});
 
