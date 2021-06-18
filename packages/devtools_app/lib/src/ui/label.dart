26ee1bc0c93a6661e023e33b129be95356fcdee3
Helin Shiah <helinx@google.com>
2021-06-18 11:47:46 -0700

    Fix lint errors

--- packages/devtools_app/lib/src/ui/label.dart
+++ packages/devtools_app/lib/src/ui/label.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../theme.dart';
-import '../utils.dart';
 
 /// Label including an image icon and optional text.
 class ImageIconLabel extends StatelessWidget {
