5d17c4508779a4a531cbe83765b440314008b11f
Tom Gilder <tom.gilder@codemate.com>
2021-08-03 19:29:21 +0300

    Add text previews to widget tree (#3218)

--- packages/devtools_app/test/inspector_tree_test.dart
+++ packages/devtools_app/test/inspector_tree_test.dart
@@ -9,24 +9,28 @@ import 'package:devtools_app/src/inspector/inspector_tree.dart';
 import 'package:devtools_app/src/inspector/inspector_tree_flutter.dart';
 import 'package:devtools_app/src/service_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart' hide Fake;
 import 'package:mockito/mockito.dart';
 
+import 'support/inspector_tree.dart';
 import 'support/mocks.dart';
+import 'support/utils.dart';
 import 'support/wrappers.dart';
 
 void main() {
   FakeServiceManager fakeServiceManager;
+
+  setUp(() {
+    fakeServiceManager = FakeServiceManager();
+    when(fakeServiceManager.connectedApp.isFlutterAppNow).thenReturn(true);
+    when(fakeServiceManager.connectedApp.isProfileBuildNow).thenReturn(false);
+
+    setGlobal(ServiceConnectionManager, fakeServiceManager);
+    mockIsFlutterApp(serviceManager.connectedApp);
+  });
+
   group('InspectorTreeController', () {
-    setUp(() {
-      fakeServiceManager = FakeServiceManager();
-      when(fakeServiceManager.connectedApp.isFlutterAppNow).thenReturn(true);
-      when(fakeServiceManager.connectedApp.isProfileBuildNow).thenReturn(false);
-
-      setGlobal(ServiceConnectionManager, fakeServiceManager);
-      mockIsFlutterApp(serviceManager.connectedApp);
-    });
-
     testWidgets('Row with negative index regression test',
         (WidgetTester tester) async {
       final controller = InspectorTreeControllerFlutter()
@@ -62,6 +66,64 @@ void main() {
       // This operation would previously throw an exception in debug builds
       // and infinite loop in release builds.
       controller.scrollToRect(const Rect.fromLTWH(0, -20, 100, 100));
+    });
+  });
+
+  group('Inspector tree content preview', () {
+    testWidgets('Shows simple text preview', (WidgetTester tester) async {
+      final diagnosticNode = await widgetToInspectorTreeDiagnosticsNode(
+        widget: const Text('Content'),
+        tester: tester,
+      );
+
+      final treeController = inspectorTreeControllerFromNode(diagnosticNode);
+      await tester.pumpWidget(wrap(InspectorTree(
+        controller: treeController,
+        debuggerController: DebuggerController(),
+      )));
+
+      expect(find.richText('Text: "Content"'), findsOneWidget);
+    });
+
+    testWidgets('Shows preview from Text.rich', (WidgetTester tester) async {
+      final diagnosticNode = await widgetToInspectorTreeDiagnosticsNode(
+        widget: const Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(text: 'Rich '),
+              TextSpan(text: 'text'),
+            ],
+          ),
+        ),
+        tester: tester,
+      );
+
+      final treeController = inspectorTreeControllerFromNode(diagnosticNode);
+      await tester.pumpWidget(wrap(InspectorTree(
+        controller: treeController,
+        debuggerController: DebuggerController(),
+      )));
+
+      expect(find.richText('Text: "Rich text"'), findsOneWidget);
+    });
+
+    testWidgets('Strips new lines from text preview',
+        (WidgetTester tester) async {
+      final diagnosticNode = await widgetToInspectorTreeDiagnosticsNode(
+        widget: const Text('Multiline\ntext\n\ncontent'),
+        tester: tester,
+      );
+
+      final treeController = inspectorTreeControllerFromNode(diagnosticNode);
+
+      await tester.pumpWidget(
+        wrap(InspectorTree(
+          controller: treeController,
+          debuggerController: DebuggerController(),
+        )),
+      );
+
+      expect(find.richText('Text: "Multiline text  content"'), findsOneWidget);
     });
   });
 }
