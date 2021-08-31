bb5e4a0a095f095158df74e6848296622379fa24
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2021-08-31 12:45:43 -0700

    Revert "Don't wait on listening for streams in DevTools startup (#3320)" (#3321)
    This reverts commit c2114dd78cf93315b2e0cdddab75806ec99ff482.

--- packages/devtools_app/lib/src/service_manager.dart
+++ packages/devtools_app/lib/src/service_manager.dart
@@ -273,7 +273,7 @@ class ServiceConnectionManager {
       serviceStreamName,
     ];
 
-    unawaited(Future.wait(streamIds.map((String id) async {
+    await Future.wait(streamIds.map((String id) async {
       try {
         await service.streamListen(id);
       } catch (e) {
@@ -287,7 +287,7 @@ class ServiceConnectionManager {
           );
         }
       }
-    })));
+    }));
     if (service != this.service) {
       // A different service has been opened.
       return;
