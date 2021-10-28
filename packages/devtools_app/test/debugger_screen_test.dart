08013338b83cf209a1b4fda5b1f4a9ff508ad827
Kenzie Schmoll <kenzieschmoll@google.com>
2021-10-28 11:49:36 -0700

    fix more tests

--- packages/devtools_app/test/debugger_screen_test.dart
+++ packages/devtools_app/test/debugger_screen_test.dart
@@ -26,17 +26,24 @@ void main() {
   DebuggerScreen screen;
   FakeServiceManager fakeServiceManager;
   MockDebuggerController debuggerController;
-  fakeServiceManager = FakeServiceManager();
-  when(fakeServiceManager.connectedApp.isProfileBuildNow).thenReturn(false);
-  when(fakeServiceManager.connectedApp.isDartWebAppNow).thenReturn(false);
-  setGlobal(ServiceConnectionManager, fakeServiceManager);
 
   const windowSize = Size(4000.0, 4000.0);
   const smallWindowSize = Size(1000.0, 1000.0);
 
+  setUp(() {
+    fakeServiceManager = FakeServiceManager();
+    when(fakeServiceManager.connectedApp.isProfileBuildNow).thenReturn(false);
+    when(fakeServiceManager.connectedApp.isDartWebAppNow).thenReturn(false);
+    setGlobal(ServiceConnectionManager, fakeServiceManager);
+    print('initializing');
+    fakeServiceManager.consoleService.ensureServiceInitialized();
+  });
+
   group('DebuggerScreen', () {
     Future<void> pumpDebuggerScreen(
-        WidgetTester tester, DebuggerController controller) async {
+      WidgetTester tester,
+      DebuggerController controller,
+    ) async {
       await tester.pumpWidget(wrapWithControllers(
         const DebuggerScreenBody(),
         debugger: controller,
@@ -44,7 +51,9 @@ void main() {
     }
 
     Future<void> pumpConsole(
-        WidgetTester tester, DebuggerController controller) async {
+      WidgetTester tester,
+      DebuggerController controller,
+    ) async {
       await tester.pumpWidget(wrapWithControllers(
         Row(
           children: [
@@ -134,21 +143,23 @@ void main() {
       }
 
       group('Clipboard', () {
-        _appendStdioLines();
         var _clipboardContents = '';
         final _expected = _stdio.join('\n');
 
         setUp(() {
+          _appendStdioLines();
           // This intercepts the Clipboard.setData SystemChannel message,
           // and stores the contents that were (attempted) to be copied.
           SystemChannels.platform.setMockMethodCallHandler((MethodCall call) {
             switch (call.method) {
               case 'Clipboard.setData':
                 _clipboardContents = call.arguments['text'];
-                return Future.value(true);
                 break;
               case 'Clipboard.getData':
                 return Future.value(<String, dynamic>{});
+                break;
+              case 'Clipboard.hasStrings':
+                return Future.value(<String, dynamic>{'value': true});
                 break;
               default:
                 break;
@@ -166,8 +177,6 @@ void main() {
         testWidgetsWithWindowSize(
             'Tapping the Copy to Clipboard button attempts to copy stdio to clipboard.',
             windowSize, (WidgetTester tester) async {
-          _appendStdioLines();
-
           await pumpConsole(tester, debuggerController);
 
           final copyButton =
