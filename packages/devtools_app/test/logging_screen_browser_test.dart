928dd908a2890b08564de245ca07a32ac11f2941
Polina <->
2022-01-31 19:52:04 -0800

    fix tests

--- packages/devtools_app/test/logging_screen_browser_test.dart
+++ packages/devtools_app/test/logging_screen_browser_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@TestOn('vm')
+@TestOn('browser')
 
 import 'package:ansicolor/ansicolor.dart';
 import 'package:devtools_app/src/logging/logging_controller.dart';
