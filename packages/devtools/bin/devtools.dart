0f7ad6cbd3885833851a33415ce5e728c7adb404
Kenzie Schmoll <kenzieschmoll@google.com>
2022-02-02 14:21:17 -0800

    Specify a customDevToolsPath.

--- packages/devtools/bin/devtools.dart
+++ packages/devtools/bin/devtools.dart
@@ -3,12 +3,27 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:dds/devtools_server.dart';
+import 'package:path/path.dart' as path;
 
 void main(List<String> arguments) async {
   // ignore: unawaited_futures
   // TODO(kenz): uncomment this out once a new version of dds is published.
   // unawaited(DevToolsServer().serveDevToolsWithArgs(arguments));
-  unawaited(DevToolsServer().serveDevTools());
+  final buildDir = await _resolveBuildDir();
+  unawaited(DevToolsServer().serveDevTools(customDevToolsPath: buildDir));
+}
+
+Future<String> _resolveBuildDir() async {
+  final resourceUri = await Isolate.resolvePackageUri(
+    Uri(
+      scheme: 'package',
+      path: 'devtools/devtools.dart',
+    ),
+  );
+
+  final packageDir = path.dirname(path.dirname(resourceUri!.toFilePath()));
+  return path.join(packageDir, 'build');
 }
