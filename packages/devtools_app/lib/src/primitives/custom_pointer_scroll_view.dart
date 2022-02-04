baa932f12a1ef6a6986e9fab0874f01b74fa607c
Polina <polinach@google.com>
2022-02-03 17:14:58 -0800

    Revert "Update custom_pointer_scroll_view.dart"
    This reverts commit a6af69e8006181af49cebc8750ed02ad4fbf3199.

--- packages/devtools_app/lib/src/primitives/custom_pointer_scroll_view.dart
+++ packages/devtools_app/lib/src/primitives/custom_pointer_scroll_view.dart
@@ -604,6 +604,7 @@ class _CustomPointerScrollableState extends State<CustomPointerScrollable>
 
   @override
   Widget build(BuildContext context) {
+    assert(position != null);
     // _ScrollableScope must be placed above the BuildContext returned by
     // notificationContext so that we can get this _CustomPointerScrollableState
     // by doing the following:
