4cc8869a3a0517d2d42be231da48a1969fa171e7
Polina <polinach@google.com>
2022-02-04 17:58:59 -0800

    ignore

--- packages/devtools_app/lib/src/debugger/program_explorer_controller.dart
+++ packages/devtools_app/lib/src/debugger/program_explorer_controller.dart
@@ -98,6 +98,7 @@ class ProgramExplorerController extends DisposableController
       return;
     }
     _selectScriptNode(script, _rootObjectNodes.value);
+    // ignore: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
     _rootObjectNodes.notifyListeners();
   }
 
