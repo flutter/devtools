a38009184db910ce52e4ec4b3a72f9ebe99122d4
Polina <polinach@google.com>
2022-02-15 15:00:06 -0800

    Update device_dialog_test.dart

--- packages/devtools_app/test/device_dialog_test.dart
+++ packages/devtools_app/test/device_dialog_test.dart
@@ -4,6 +4,7 @@
 
 // @dart=2.9
 
+import 'package:devtools_app/src/config_specific/ide_theme/ide_theme.dart';
 import 'package:devtools_app/src/shared/device_dialog.dart';
 import 'package:devtools_app/src/shared/globals.dart';
 import 'package:devtools_app/src/shared/service_manager.dart';
@@ -35,6 +36,7 @@ void main() {
       when(fakeServiceManager.connectedApp.isDartWebAppNow).thenReturn(false);
       when(fakeServiceManager.connectedApp.isRunningOnDartVM).thenReturn(true);
       setGlobal(ServiceConnectionManager, fakeServiceManager);
+      setGlobal(IdeTheme, IdeTheme());
     }
 
     DeviceDialog deviceDialog;
