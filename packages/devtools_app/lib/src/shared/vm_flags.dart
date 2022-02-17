d862321359f044c6fca4342892d64428df9e622c
Polina Cherkasova <polinach@google.com>
2022-02-16 16:37:39 -0800

    Migrate vm_flags.dart to null safety. (#3690)

--- packages/devtools_app/lib/src/shared/vm_flags.dart
+++ packages/devtools_app/lib/src/shared/vm_flags.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
+// ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'dart:core';
 
@@ -21,24 +21,23 @@ const profiler = 'profiler';
 const profilePeriod = 'profile_period';
 
 class VmFlagManager extends Disposer {
-  VmServiceWrapper get service => _service;
-  VmServiceWrapper _service;
+  VmServiceWrapper? get service => _service;
+  VmServiceWrapper? _service;
 
   ValueListenable get flags => _flags;
-  final _flags = ValueNotifier<FlagList>(null);
+  final _flags = ValueNotifier<FlagList?>(null);
 
-  final _flagNotifiers = <String, ValueNotifier<Flag>>{};
+  final _flagNotifiers = <String?, ValueNotifier<Flag>>{};
 
-  ValueNotifier<Flag> flag(String name) {
+  ValueNotifier<Flag>? flag(String name) {
     return _flagNotifiers.containsKey(name) ? _flagNotifiers[name] : null;
   }
 
   Future<void> _initFlags() async {
-    final flagList = await service.getFlagList();
+    final flagList = await service!.getFlagList() ?? FlagList(flags: []);
     _flags.value = flagList;
-    if (flagList == null) return;
 
-    for (var flag in flagList.flags) {
+    for (var flag in flagList.flags ?? <Flag>[]) {
       _flagNotifiers[flag.name] = ValueNotifier<Flag>(flag);
     }
   }
@@ -47,14 +46,14 @@ class VmFlagManager extends Disposer {
   void handleVmEvent(Event event) async {
     if (event.kind == EventKind.kVMFlagUpdate) {
       if (_flagNotifiers.containsKey(event.flag)) {
-        final currentFlag = _flagNotifiers[event.flag].value;
-        _flagNotifiers[event.flag].value = Flag.parse({
+        final currentFlag = _flagNotifiers[event.flag]!.value;
+        _flagNotifiers[event.flag]!.value = Flag.parse({
           'name': currentFlag.name,
           'comment': currentFlag.comment,
           'modified': true,
           'valueAsString': event.newValue,
-        });
-        _flags.value = await service.getFlagList();
+        })!;
+        _flags.value = await service!.getFlagList();
       }
     }
   }
