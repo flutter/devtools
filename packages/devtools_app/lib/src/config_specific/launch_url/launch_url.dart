5b2e5fe62e741beca5ea513addb74aac9b2c240f
Polina <polinach@google.com>
2022-02-26 19:03:55 -0800

    Update launch_url.dart

--- packages/devtools_app/lib/src/config_specific/launch_url/launch_url.dart
+++ packages/devtools_app/lib/src/config_specific/launch_url/launch_url.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-// @dart=2.9
+
 
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
@@ -15,7 +15,7 @@ Future<void> launchUrl(String url, BuildContext context) async {
   if (await url_launcher.canLaunch(url)) {
     await url_launcher.launch(url);
   } else {
-    Notifications.of(context).push('Unable to open $url.');
+    Notifications.of(context)!.push('Unable to open $url.');
   }
 
   // When embedded in VSCode, url_launcher will silently fail, so we send a
