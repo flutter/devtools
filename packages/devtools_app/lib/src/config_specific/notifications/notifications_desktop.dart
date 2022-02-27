395cb420120c5de45002f60a3806d7d2109f5d41
Polina <polinach@google.com>
2022-02-27 13:11:47 -0800

    -

--- packages/devtools_app/lib/src/config_specific/notifications/notifications_desktop.dart
+++ packages/devtools_app/lib/src/config_specific/notifications/notifications_desktop.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-
-
 class Notification {
   Notification(String title, {String? body}) {
     throw Exception('unsupported platform');
