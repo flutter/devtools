54ca31be882b7a3af396328f539f44d0ca3ec65b
Polina <polinach@google.com>
2022-02-27 13:09:04 -0800

    Update launch_url.dart

--- packages/devtools_app/lib/src/config_specific/launch_url/launch_url.dart
+++ packages/devtools_app/lib/src/config_specific/launch_url/launch_url.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-
-
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 import '../../shared/notifications.dart';
