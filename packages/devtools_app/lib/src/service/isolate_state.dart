afd7f5ce3633aaa468a9ff42eb50424514a074b3
Polina <polinach@google.com>
2022-03-04 09:02:44 -0800

    Update isolate_state.dart

--- packages/devtools_app/lib/src/service/isolate_state.dart
+++ packages/devtools_app/lib/src/service/isolate_state.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:async';
 import 'dart:core';
 
@@ -13,25 +11,25 @@ import 'package:vm_service/vm_service.dart' hide Error;
 class IsolateState {
   IsolateState(this.isolateRef);
 
-  ValueListenable<bool> get isPaused => _isPaused;
+  ValueListenable<bool?> get isPaused => _isPaused;
 
   final IsolateRef isolateRef;
 
-  Future<Isolate> get isolate => _isolate.future;
-  Completer<Isolate> _isolate = Completer();
+  Future<Isolate> get isolate => _completer.future.then((value) => value!);
+  Completer<Isolate?> _completer = Completer();
 
-  Isolate get isolateNow => _isolateNow;
-  Isolate _isolateNow;
+  Isolate? get isolateNow => _isolateNow;
+  Isolate? _isolateNow;
 
   /// Paused is null until we know whether the isolate is paused or not.
-  final _isPaused = ValueNotifier<bool>(null);
+  final _isPaused = ValueNotifier<bool?>(null);
 
   void onIsolateLoaded(Isolate isolate) {
     _isolateNow = isolate;
-    _isolate.complete(isolate);
+    _completer.complete(isolate);
     if (_isPaused.value == null) {
       if (isolate.pauseEvent != null &&
-          isolate.pauseEvent.kind != EventKind.kResume) {
+          isolate.pauseEvent!.kind != EventKind.kResume) {
         _isPaused.value = true;
       } else {
         _isPaused.value = false;
@@ -41,14 +39,14 @@ class IsolateState {
 
   void dispose() {
     _isolateNow = null;
-    if (!_isolate.isCompleted) {
-      _isolate.complete(null);
+    if (!_completer.isCompleted) {
+      _completer.complete(null);
     } else {
-      _isolate = Completer()..complete(null);
+      _completer = Completer()..complete(null);
     }
   }
 
-  void handleDebugEvent(String kind) {
+  void handleDebugEvent(String? kind) {
     switch (kind) {
       case EventKind.kResume:
         _isPaused.value = false;
