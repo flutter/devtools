e3a6b2f4bf1d9df0c2ebf6aa9dc8e1660433149d
Polina <polinach@google.com>
2022-03-04 08:01:19 -0800

    -

--- packages/devtools_app/lib/src/screens/vm_developer/vm_developer_tools_screen.dart
+++ packages/devtools_app/lib/src/screens/vm_developer/vm_developer_tools_screen.dart
@@ -8,9 +8,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../primitives/auto_dispose_mixin.dart';
-import '../shared/screen.dart';
-import '../shared/theme.dart';
+import '../../primitives/auto_dispose_mixin.dart';
+import '../../shared/screen.dart';
+import '../../shared/theme.dart';
 import 'isolate_statistics_view.dart';
 import 'vm_developer_tools_controller.dart';
 import 'vm_statistics_view.dart';
