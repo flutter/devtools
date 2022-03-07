e5f2c78504d6cd94108dd016b1459348736e0dc2
Polina <polinach@google.com>
2022-03-07 14:49:35 -0800

    Update debugger_evalution_test.dart

--- packages/devtools_app/test/debugger_evalution_test.dart
+++ packages/devtools_app/test/debugger_evalution_test.dart
@@ -6,6 +6,7 @@
 
 import 'dart:async';
 
+import 'package:devtools_app/src/primitives/message_bus.dart';
 import 'package:devtools_app/src/primitives/utils.dart';
 import 'package:devtools_app/src/screens/debugger/debugger_controller.dart';
 import 'package:devtools_app/src/screens/debugger/evaluate.dart';
@@ -22,6 +23,8 @@ void main() {
   final FlutterTestEnvironment env = FlutterTestEnvironment(
     const FlutterRunConfiguration(withDebugger: true),
   );
+
+  setGlobal(MessageBus, MessageBus());
 
   Disposable isAlive;
   DebuggerController debuggerController;
