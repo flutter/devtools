03891761b81d0054a8f676fe5b9ae40b868df3d1
Polina <polinach@google.com>
2022-03-10 15:05:32 -0800

    Split logging_screen.

---
+++ packages/devtools_app/lib/src/screens/logging/_message_column.dart
@@ -0,0 +1,107 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart=2.9
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import '../../primitives/utils.dart';
+import '../../shared/table.dart';
+import '../../shared/table_data.dart';
+import '../../shared/theme.dart';
+import '../../ui/colors.dart';
+import 'logging_controller.dart';
+
+@visibleForTesting
+class MessageColumn extends ColumnData<LogData>
+    implements ColumnRenderer<LogData> {
+  MessageColumn() : super.wide('Message');
+
+  @override
+  bool get supportsSorting => false;
+
+  @override
+  String getValue(LogData dataObject) =>
+      dataObject.summary ?? dataObject.details;
+
+  @override
+  int compare(LogData a, LogData b) {
+    final String valueA = getValue(a);
+    final String valueB = getValue(b);
+    // Matches frame descriptions (e.g. '#12  11.4ms ')
+    final regex = RegExp(r'#(\d+)\s+\d+.\d+ms\s*');
+    final valueAIsFrameLog = valueA.startsWith(regex);
+    final valueBIsFrameLog = valueB.startsWith(regex);
+    if (valueAIsFrameLog && valueBIsFrameLog) {
+      final frameNumberA = regex.firstMatch(valueA)[1];
+      final frameNumberB = regex.firstMatch(valueB)[1];
+      return int.parse(frameNumberA).compareTo(int.parse(frameNumberB));
+    } else if (valueAIsFrameLog && !valueBIsFrameLog) {
+      return -1;
+    } else if (!valueAIsFrameLog && valueBIsFrameLog) {
+      return 1;
+    }
+    return valueA.compareTo(valueB);
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    LogData data, {
+    bool isRowSelected = false,
+    VoidCallback onPressed,
+  }) {
+    TextStyle textStyle = Theme.of(context).fixedFontStyle;
+    if (isRowSelected) {
+      textStyle = textStyle.copyWith(color: defaultSelectionForegroundColor);
+    }
+
+    if (data.kind == 'flutter.frame') {
+      const Color color = Color.fromARGB(0xff, 0x00, 0x91, 0xea);
+      final Text text = Text(
+        getDisplayValue(data),
+        overflow: TextOverflow.ellipsis,
+        style: textStyle,
+      );
+
+      double frameLength = 0.0;
+      try {
+        final int micros = jsonDecode(data.details)['elapsed'];
+        frameLength = micros * 3.0 / 1000.0;
+      } catch (e) {
+        // ignore
+      }
+
+      return Row(
+        children: <Widget>[
+          text,
+          Flexible(
+            child: Container(
+              height: 12.0,
+              width: frameLength,
+              decoration: const BoxDecoration(color: color),
+            ),
+          ),
+        ],
+      );
+    } else if (data.kind == 'stdout') {
+      return RichText(
+        text: TextSpan(
+          children: processAnsiTerminalCodes(
+            // TODO(helin24): Recompute summary length considering ansi codes.
+            //  The current summary is generally the first 200 chars of details.
+            getDisplayValue(data),
+            textStyle,
+          ),
+        ),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      );
+    } else {
+      return null;
+    }
+  }
+}
