95d8a8707c372e7133bf9fdd866f308675960ff4
Polina <polinach@google.com>
2022-03-15 10:20:42 -0700

    ip

--- packages/devtools_app/lib/src/screens/inspector/layout_explorer/ui/overflow_indicator_painter.dart
+++ packages/devtools_app/lib/src/screens/inspector/layout_explorer/ui/overflow_indicator_painter.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
+
 
 import 'dart:ui' as ui;
 
@@ -29,7 +29,7 @@ import '../../inspector_data_models.dart';
 class OverflowIndicatorPainter extends CustomPainter {
   const OverflowIndicatorPainter(this.side, this.size);
 
-  final OverflowSide side;
+  final OverflowSide? side;
   final double size;
 
   /// These static variables  are taken from [DebugOverflowIndicatorMixin]
--- packages/devtools_app/lib/src/screens/inspector/layout_explorer/ui/widgets_theme.dart
+++ packages/devtools_app/lib/src/screens/inspector/layout_explorer/ui/widgets_theme.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
+
 
 import 'package:flutter/material.dart';
 
@@ -12,10 +12,10 @@ class WidgetTheme {
     this.color = otherWidgetColor,
   });
 
-  final String iconAsset;
+  final String? iconAsset;
   final Color color;
 
-  static WidgetTheme fromName(String widgetType) {
+  static WidgetTheme fromName(String? widgetType) {
     if (widgetType == null) {
       return const WidgetTheme();
     }
