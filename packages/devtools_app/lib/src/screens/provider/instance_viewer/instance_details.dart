927c54d4ce606085b963e49116c96443c4da2382
Remi Rousselet <darky12s@gmail.com>
2022-03-24 14:56:14 +0100

    add missing import

--- packages/devtools_app/lib/src/screens/provider/instance_viewer/instance_details.dart
+++ packages/devtools_app/lib/src/screens/provider/instance_viewer/instance_details.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:vm_service/vm_service.dart';
 
