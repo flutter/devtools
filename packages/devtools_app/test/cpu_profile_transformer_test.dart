73fa69bb98d06c00f46d478f23995ea24305eb5e
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2022-04-13 22:10:00 +0000

    Move shared profiler code to profiler_utils.dart (#3977)

--- packages/devtools_app/test/cpu_profile_transformer_test.dart
+++ packages/devtools_app/test/cpu_profile_transformer_test.dart
@@ -4,6 +4,7 @@
 
 import 'package:devtools_app/src/screens/profiler/cpu_profile_model.dart';
 import 'package:devtools_app/src/screens/profiler/cpu_profile_transformer.dart';
+import 'package:devtools_app/src/shared/profiler_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_data/cpu_profile_test_data.dart';
