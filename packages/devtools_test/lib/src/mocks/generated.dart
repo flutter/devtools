8025e14d54bd0d20d8fb423202abf266b7d3d3c3
Polina <polinach@google.com>
2022-04-13 08:25:52 -0700

    Update generated.dart

--- packages/devtools_test/lib/src/mocks/generated.dart
+++ packages/devtools_test/lib/src/mocks/generated.dart
@@ -8,5 +8,6 @@ import 'package:devtools_app/src/service/vm_service_wrapper.dart';
 import 'package:mockito/annotations.dart';
 
 // See https://github.com/dart-lang/mockito/blob/master/NULL_SAFETY_README.md
+// After generating mocks, add `// ignore_for_file: require_trailing_commas`
 @GenerateMocks([VmServiceWrapper])
 void main() {}
