67a4bfddd0ba5c1120964bb562a6ff86e4003e23
Polina <polinach@google.com>
2022-04-14 13:43:52 -0700

    Update generate_code.sh

--- tool/generate_code.sh
+++ tool/generate_code.sh
@@ -5,11 +5,13 @@
 # found in the LICENSE file.
 
 echo "Generating code..."
+
+echo $(pwd)
+
 pushd packages
 
 pushd devtools_test
 flutter pub run build_runner build
-popd
 
 echo "Adding 'ignore_for_file: require_trailing_commas' to generated mocks..."
 TMP_FILE=/tmp/generated.mocks.dart
@@ -17,6 +19,7 @@ MOCK_FILE=devtools_test/lib/src/mocks/generated.mocks.dart
 awk '!x{x=sub(/\/\/ ignore_for_file:/,"// ignore_for_file: require_trailing_commas\n// ignore_for_file:")}1' $MOCK_FILE > $TMP_FILE
 mv $TMP_FILE $MOCK_FILE
 rm $TMP_FILE
+popd
 
 popd
 echo "Done generating code."
