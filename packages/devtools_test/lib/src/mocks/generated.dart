9079bb13061fd5fec845ab103bd64fdf436a7914
Polina <polinach@google.com>
2022-04-15 09:19:52 -0700

    Update generated.dart

--- packages/devtools_test/lib/src/mocks/generated.dart
+++ packages/devtools_test/lib/src/mocks/generated.dart
@@ -8,5 +8,6 @@ import 'package:devtools_app/src/service/vm_service_wrapper.dart';
 import 'package:mockito/annotations.dart';
 
 // See https://github.com/dart-lang/mockito/blob/master/NULL_SAFETY_README.md
+// Run `sh tools/generate_code.sh` to regenerate mocks.
 @GenerateMocks([VmServiceWrapper])
 void main() {}
