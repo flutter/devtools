6eb2d405f1a3ee0bcbc5679a045650d555c32f55
Polina <polinach@google.com>
2022-04-16 16:45:04 -0700

    Update devtools_test.dart

--- packages/devtools_test/lib/devtools_test.dart
+++ packages/devtools_test/lib/devtools_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
-
 export 'src/mocks/fake_service_extension_manager.dart';
 export 'src/mocks/fake_service_manager.dart';
 export 'src/mocks/fake_vm_service_wrapper.dart';
