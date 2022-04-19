bc8a4f56b7cc97b762d292bc1240a02878821cfb
Polina Cherkasova <polina.c@live.com>
2022-04-19 11:38:34 -0700

    trailing comma

--- packages/devtools_app/test/fixtures/flutter_app/lib/overflow_errors.dart
+++ packages/devtools_app/test/fixtures/flutter_app/lib/overflow_errors.dart
@@ -25,27 +25,28 @@ class OverflowingApp extends StatefulWidget {
 class _OverflowingAppState extends State<OverflowingApp> {
   @override
   Widget build(BuildContext context) => MaterialApp(
-    title: 'Overflowing App',
-    home: Column(
-      children: [
-        for (var i = 0; i < 5; i++)
-          Row(
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
-                          ' do eiusmod tempor incididunt ut labore et dolore magna '
-                          'aliqua. Ut enim ad minim veniam, quis nostrud '
-                          'exercitation ullamco laboris nisi ut aliquip ex ea '
-                          'commodo consequat.'),
+        title: 'Overflowing App',
+        home: Column(
+          children: [
+            for (var i = 0; i < 5; i++)
+              Row(
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
+                        ' do eiusmod tempor incididunt ut labore et dolore magna '
+                        'aliqua. Ut enim ad minim veniam, quis nostrud '
+                        'exercitation ullamco laboris nisi ut aliquip ex ea '
+                        'commodo consequat.',
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-      ],
-    ),
-  );
+          ],
+        ),
+      );
 }
