9e9467a70e9bc80a5f9e7819286f2e5d558853c9
Polina Cherkasova <polina.c@live.com>
2022-04-21 13:15:44 -0700

    -

--- packages/devtools_app/test/test_utils/fake_inspector_tree.dart
+++ packages/devtools_app/test/test_utils/fake_inspector_tree.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: implementation_imports
-
 import 'dart:async';
 import 'dart:math';
 
