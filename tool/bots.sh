809e92199a6f7248ad376e84f82d29145724df03
Polina Cherkasova <polina.c@live.com>
2022-04-22 17:35:10 -0700

    -

--- tool/bots.sh
+++ tool/bots.sh
@@ -135,6 +135,8 @@ elif [ "$BOT" = "test_ddc" ]; then
         flutter test $DART_DEFINE_ARGS test/cpu_profiler_test
         flutter test $DART_DEFINE_ARGS test/cpu_profiler_controller_test.dart
         flutter test $DART_DEFINE_ARGS test/device_dialog_test.dart
+        flutter test $DART_DEFINE_ARGS test/profiler_screen_controller_test.dart
+        flutter test $DART_DEFINE_ARGS test/profiler_screen_test.dart
 
     elif [ "$PLATFORM" = "chrome" ]; then
         flutter test --platform chrome $DART_DEFINE_ARGS test/*.dart test/fixtures/ --no-sound-null-safety
