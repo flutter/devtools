35d544660d57b73bdfc909181caa961b06cb6890
Polina Cherkasova <polina.c@live.com>
2022-04-23 17:21:21 -0700

    Revert "split"
    This reverts commit 594e86addca7c7ec1024b2d9f88ed578448a5554.

--- packages/devtools_app/test/logging_screen_test.dart
+++ packages/devtools_app/test/logging_screen_test.dart
@@ -14,6 +14,7 @@ import 'package:devtools_app/src/screens/logging/logging_controller.dart';
 import 'package:devtools_app/src/screens/logging/logging_screen.dart';
 import 'package:devtools_app/src/service/service_extensions.dart';
 import 'package:devtools_app/src/service/service_manager.dart';
+import 'package:devtools_app/src/shared/common_widgets.dart';
 import 'package:devtools_app/src/shared/globals.dart';
 import 'package:devtools_app/src/ui/service_extension_widgets.dart';
 import 'package:devtools_test/devtools_test.dart';
@@ -120,6 +121,160 @@ void main() {
       expect(toggle.value, true);
 
       // TODO(djshuckerow): Hook up fake extension state querying.
+    });
+
+    group('with data', () {
+      setUp(() {
+        when(mockLoggingController.data).thenReturn(fakeLogData);
+        when(mockLoggingController.filteredData)
+            .thenReturn(ListValueNotifier<LogData>(fakeLogData));
+      });
+
+      testWidgetsWithWindowSize('shows log items', windowSize,
+          (WidgetTester tester) async {
+        await pumpLoggingScreen(tester);
+        await tester.pumpAndSettle();
+        expect(find.byType(LogsTable), findsOneWidget);
+        expect(
+          find.byKey(ValueKey(fakeLogData.first)),
+          findsOneWidget,
+        );
+        expect(
+          find.byKey(ValueKey(fakeLogData.last)),
+          findsOneWidget,
+        );
+      });
+
+      testWidgetsWithWindowSize('can show non-computing log data', windowSize,
+          (WidgetTester tester) async {
+        await pumpLoggingScreen(tester);
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[6])));
+        await tester.pumpAndSettle();
+        expect(
+          find.selectableText('log event 6'),
+          findsOneWidget,
+          reason:
+              'The log details should be visible both in the details section.',
+        );
+        expect(
+          find.selectableText('log event 6'),
+          findsOneWidget,
+          reason: 'The log details should be visible both in the table.',
+        );
+      });
+
+      testWidgetsWithWindowSize('can show null log data', windowSize,
+          (WidgetTester tester) async {
+        await pumpLoggingScreen(tester);
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[7])));
+        await tester.pumpAndSettle();
+      });
+
+      testWidgetsWithWindowSize('search field can enter text', windowSize,
+          (WidgetTester tester) async {
+        await pumpLoggingScreen(tester);
+        verifyNever(mockLoggingController.clear());
+
+        final textFieldFinder = find.byKey(loggingSearchFieldKey);
+        expect(textFieldFinder, findsOneWidget);
+        final TextField textField = tester.widget(textFieldFinder) as TextField;
+        expect(textField.enabled, isTrue);
+        await tester.enterText(find.byType(TextField), 'abc');
+      });
+
+      testWidgetsWithWindowSize(
+          'Copy to clipboard button enables/disables correctly', windowSize,
+          (WidgetTester tester) async {
+        await pumpLoggingScreen(tester);
+
+        // Locates the copy to clipboard button's IconButton.
+        final copyButton = () => find
+            .byKey(LogDetails.copyToClipboardButtonKey)
+            .evaluate()
+            .first
+            .widget as ToolbarAction;
+
+        expect(
+          copyButton().onPressed,
+          isNull,
+          reason:
+              'Copy to clipboard button should be disabled when no logs are selected',
+        );
+
+        await tester.tap(find.byKey(ValueKey(fakeLogData[5])));
+        await tester.pumpAndSettle();
+
+        expect(
+          copyButton().onPressed,
+          isNotNull,
+          reason:
+              'Copy to clipboard button should be enabled when a log with content is selected',
+        );
+
+        await tester.tap(find.byKey(ValueKey(fakeLogData[7])));
+        await tester.pumpAndSettle();
+
+        expect(
+          copyButton().onPressed,
+          isNull,
+          reason:
+              'Copy to clipboard button should be disabled when the log details are null',
+        );
+      });
+
+      testWidgetsWithWindowSize(
+          'can compute details of non-json log data', windowSize,
+          (WidgetTester tester) async {
+        const index = 8;
+        final log = fakeLogData[index];
+
+        await pumpLoggingScreen(tester);
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(log)));
+        await tester.pump();
+        expect(
+          find.selectableText(nonJsonOutput),
+          findsNothing,
+          reason:
+              "The details of the log haven't computed yet, so they shouldn't "
+              'be available.',
+        );
+
+        await tester.pumpAndSettle();
+        expect(find.selectableText(nonJsonOutput), findsOneWidget);
+      });
+
+      testWidgetsWithWindowSize('can show details of json log data', windowSize,
+          (WidgetTester tester) async {
+        const index = 9;
+        bool containsJson(Widget widget) {
+          if (widget is! SelectableText) return false;
+          final content = widget.data!.trim();
+          return content.startsWith('{') && content.endsWith('}');
+        }
+
+        final findJson = find.descendant(
+          of: find.byType(LogDetails),
+          matching: find.byWidgetPredicate(containsJson),
+        );
+
+        await pumpLoggingScreen(tester);
+        await tester.pumpAndSettle();
+        await tester.tap(find.byKey(ValueKey(fakeLogData[index])));
+        await tester.pump();
+
+        expect(
+          findJson,
+          findsNothing,
+          reason:
+              "The details of the log haven't computed yet, so they shouldn't be available.",
+        );
+
+        await tester.pumpAndSettle();
+        expect(findJson, findsOneWidget);
+      });
     });
 
     group('MessageColumn', () {
