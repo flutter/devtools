0f4b0591c2b0a8f9dc1054994dabd86e17783477
Dan Chevalier <chevalier.dan@gmail.com>
2022-04-29 11:11:31 -0400

    oops removing the hacks for triggering the viewer

--- packages/devtools_app/lib/src/shared/release_notes/release_notes.dart
+++ packages/devtools_app/lib/src/shared/release_notes/release_notes.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../devtools.dart' as devtools;
 import '../../config_specific/launch_url/launch_url.dart';
 import '../../config_specific/logger/logger.dart' as logger;
 import '../../config_specific/server/server.dart' as server;
@@ -224,15 +225,11 @@ class ReleaseNotesController {
     } else {
       previousVersion = SemanticVersion.parse(lastReleaseNotesShownVersion);
     }
-    previousVersion = SemanticVersion(major: 2, minor: 0, patch: 0);
     // Parse the current version instead of using [devtools.version] directly to
     // strip off any build metadata (any characters following a '+' character).
     // Release notes will be hosted on the Flutter website with a version number
     // that does not contain any build metadata.
-    final parsedCurrentVersion = SemanticVersion(
-      major: 2,
-      minor: 10,
-    );
+    final parsedCurrentVersion = SemanticVersion.parse(devtools.version);
     final parsedCurrentVersionStr = parsedCurrentVersion.toString();
     if (parsedCurrentVersion > previousVersion) {
       try {
