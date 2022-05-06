df1630f1f46a7df2c5e3dc9e535d0c05fed492fa
Dan Chevalier <chevalier.dan@gmail.com>
2022-05-06 11:34:06 -0400

    more magic per commit

--- packages/devtools_app/lib/src/screens/performance/performance_screen.dart
+++ packages/devtools_app/lib/src/screens/performance/performance_screen.dart
@@ -371,27 +371,6 @@ class MoreDebuggingOptionsButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textStyle = theme.subtleTextStyle;
-    final overlayChildren = <Widget>[
-      Text(
-        'When toggling on/off a rendering layer, you will need '
-        'to reproduce activity in your app to see the effects of the '
-        'debugging option. All layers are rendered by default - disabling a '
-        'layer may help you identify expensive operations in your app.',
-        style: Theme.of(context).subtleTextStyle,
-      )
-    ];
-    if (serviceManager.connectedApp!.isProfileBuildNow!) {
-      overlayChildren.addAll([
-        const SizedBox(height: denseSpacing),
-        RichText(
-          text: TextSpan(
-            text:
-                'These debugging options are not available for a profile build. To use them, run your app in debug mode.',
-            style: textStyle.copyWith(color: theme.colorScheme.errorTextColor),
-          ),
-        ),
-      ]);
-    }
 
     return ServiceExtensionCheckboxGroupButton(
       title: 'More debugging options',
@@ -406,7 +385,26 @@ class MoreDebuggingOptionsButton extends StatelessWidget {
       ],
       overlayDescription: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: overlayChildren,
+        children: [
+          Text(
+            'When toggling on/off a rendering layer, you will need '
+            'to reproduce activity in your app to see the effects of the '
+            'debugging option. All layers are rendered by default - disabling a '
+            'layer may help you identify expensive operations in your app.',
+            style: Theme.of(context).subtleTextStyle,
+          ),
+          if (serviceManager.connectedApp!.isProfileBuildNow!) ...[
+            const SizedBox(height: denseSpacing),
+            RichText(
+              text: TextSpan(
+                text:
+                    'These debugging options are not available for a profile build. To use them, run your app in debug mode.',
+                style:
+                    textStyle.copyWith(color: theme.colorScheme.errorTextColor),
+              ),
+            )
+          ]
+        ],
       ),
       overlayWidthBeforeScaling: _width,
     );
