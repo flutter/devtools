b22d092e7d0c08240ee4ca3c0f4fec2b5307ac7b
Polina Cherkasova <polina.c@live.com>
2022-06-07 15:31:08 -0700

    Update retaining_path.dart

--- packages/devtools_app/lib/src/screens/memory/panes/leaks/retaining_path.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/retaining_path.dart
@@ -3,6 +3,7 @@ import 'package:memory_tools/model.dart';
 import 'package:vm_service/vm_service.dart';
 
 import '../../memory_controller.dart';
+import 'path_finder.dart';
 
 typedef IdentityHashCode = int;
 typedef Retainers = Map<IdentityHashCode, Set<IdentityHashCode>>;
@@ -19,16 +20,16 @@ Future<void> setRetainingPaths(
   }
 }
 
-class _PathItem {
-  _PathItem(this.options);
-
-  final List<IdentityHashCode> options;
-  int _index = 0;
-
-  IdentityHashCode get selection => options[_index];
-  bool get canChange => _index < options.length - 1;
-  void change() => _index++;
-}
+// class _PathItem {
+//   _PathItem(this.options);
+//
+//   final List<IdentityHashCode> options;
+//   int _index = 0;
+//
+//   IdentityHashCode get selection => options[_index];
+//   bool get canChange => _index < options.length - 1;
+//   void change() => _index++;
+// }
 
 class _PathAnalyzer {
   _PathAnalyzer(this.graph) {
@@ -45,13 +46,13 @@ class _PathAnalyzer {
     _byCode = {};
 
     for (var object in graph.objects) {
-      if (_shouldSkip(object.identityHashCode)) continue;
+      if (_shouldSkip(object)) continue;
 
       _byCode[object.identityHashCode] = object;
 
       for (var r in object.references) {
         final reference = graph.objects[r];
-        if (_shouldSkip(reference.identityHashCode)) continue;
+        if (_shouldSkip(reference)) continue;
         if (reference.identityHashCode == object.identityHashCode) continue;
 
         if (!_retainers.containsKey(reference.identityHashCode))
@@ -59,74 +60,68 @@ class _PathAnalyzer {
         _retainers[reference.identityHashCode]!.add(object.identityHashCode);
       }
     }
-    _assertThereAreRoots();
-  }
-
-  void _assertThereAreRoots() {
-    var rootCount = 0;
-    for (var object in graph.objects) {
-      if (_retainers[object.identityHashCode]?.isEmpty != false) rootCount++;
-    }
-    print('!!!!!!!! $rootCount roots out of ${graph.objects.length} objects');
-    assert(rootCount > 0);
   }
 
   String getPath(IdentityHashCode code) {
-    final path = [
-      _PathItem([code])
-    ];
-    IdentityHashCode current = code;
-
-    while (_retainers[current]?.isNotEmpty == true) {
-      if (path.length > 1000) throw 'Too large path.';
-
-      if (_retainers[current]!.length > 1) {
-        final list = _retainers[current]!
-            .toList()
-            .map((e) => _name(_byCode[e]!))
-            .join(', ');
-        print(list);
-      }
-
-      final options = _retainers[current]!.where((retainer) {
-        final loop =
-            path.firstWhereOrNull((element) => element.selection == retainer);
-        return loop == null;
-      }).toList();
-
-      if (options.isNotEmpty) {
-        path.insert(0, _PathItem(options));
-      } else {
-        int toChange = 0;
-        while (!path[toChange].canChange && toChange < path.length - 1) {
-          toChange++;
-        }
-        assert(path[toChange].canChange, 'All retainers looped the path.');
-        path[toChange].change();
-        path.removeRange(0, toChange);
-      }
-
-      current = path.first.selection;
-
-      assert(
-        current != code,
-        'loop found',
-      );
-      assert(!_shouldSkip(path.first.selection));
-    }
-
-    final result = path.map((e) => _name(_byCode[e.selection]!)).join('/');
+    final path = findPathFromRoot(_retainers, code);
+    if (path == null) throw 'All retainers looped.';
+    final result = path.map((i) => _name(_byCode[i]!)).join('/');
     return '/$result/';
   }
+
+  // String getPath(IdentityHashCode code) {
+  //   final path = [
+  //     _PathItem([code])
+  //   ];
+  //   IdentityHashCode current = code;
+  //
+  //   while (_retainers[current]?.isNotEmpty == true) {
+  //     if (path.length > 1000) throw 'Too large path.';
+  //
+  //     if (_retainers[current]!.length > 1) {
+  //       final list = _retainers[current]!
+  //           .toList()
+  //           .map((e) => _name(_byCode[e]!))
+  //           .join(', ');
+  //       print(list);
+  //     }
+  //
+  //     final options = _retainers[current]!.where((retainer) {
+  //       final loop =
+  //           path.firstWhereOrNull((element) => element.selection == retainer);
+  //       return loop == null;
+  //     }).toList();
+  //
+  //     if (options.isNotEmpty) {
+  //       path.insert(0, _PathItem(options));
+  //     } else {
+  //       int toChange = 0;
+  //       while (!path[toChange].canChange && toChange < path.length - 1) {
+  //         toChange++;
+  //       }
+  //       assert(path[toChange].canChange, 'All retainers looped the path.');
+  //       path[toChange].change();
+  //       path.removeRange(0, toChange);
+  //     }
+  //
+  //     current = path.first.selection;
+  //
+  //     assert(
+  //       current != code,
+  //       'loop found',
+  //     );
+  //     assert(!_shouldSkip(path.first.selection));
+  //   }
+  //
+  //   final result = path.map((e) => _name(_byCode[e.selection]!)).join('/');
+  //   return '/$result/';
+  // }
 
   static String _name(HeapSnapshotObject object) {
     return '${object.identityHashCode}-${object.klass.name}';
   }
 
-  bool _shouldSkip(IdentityHashCode code) {
-    //if (object.identityHashCode == 0) return true;
-    final object = _byCode[code]!;
-
+  bool _shouldSkip(HeapSnapshotObject object) {
     const toSkip = {
       '_WeakReferenceImpl',
       'FinalizerEntry',
@@ -136,8 +131,7 @@ class _PathAnalyzer {
       // 'DebugCreator',
       // '_WidgetTicker',
     };
-    if (toSkip.contains(object.klass.name)) return true;
 
-    return false;
+    return toSkip.contains(object.klass.name);
   }
 }
