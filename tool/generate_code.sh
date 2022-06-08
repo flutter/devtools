8ca4c9d3dbda84bc27c7b00702946af34d0b31ef
Dan Chevalier <chevalier.dan@gmail.com>
2022-06-08 15:25:23 -0400

    typing too fast and made an oopsie

--- tool/generate_code.sh
+++ tool/generate_code.sh
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Contains a path the this script, relative to the directory it was called from.
+# Contains a path to this script, relative to the directory it was called from.
 RELATIVE_PATH_TO_SCRIPT="${BASH_SOURCE[0]}"
 
 # The directory that this script is located in.
