77c2bc70ba3c9ad0fd2037eaa68fb39945d4fd1c
Polina Cherkasova <polina.c@live.com>
2022-06-08 17:02:05 -0700

    demo

--- packages/devtools_app/lib/src/screens/memory/panes/leaks/model.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/model.dart
@@ -6,11 +6,29 @@ import 'retaining_path.dart';
 class HeapObject {
   HeapObject({required this.references, required this.klass});
 
+  factory HeapObject.fromJson(Map<String, dynamic> json) => HeapObject(
+        references: json['references'],
+        klass: json['klass'],
+      );
+
   final List<IdentityHashCode> references;
   final String klass;
+
+  Map<String, dynamic> toJson() => {
+        'references': references,
+        'klass': klass,
+      };
 }
 
 class LeakAnalysisTask {
+  factory LeakAnalysisTask.fromJson(Map<String, dynamic> json) =>
+      LeakAnalysisTask._(
+        objects: json['objects'],
+        reports: json['reports'],
+      );
+
+  LeakAnalysisTask._({required this.objects, required this.reports});
+
   LeakAnalysisTask.fromSnapshot(HeapSnapshotGraph graph, this.reports) {
     objects = Map.fromIterable(
       graph.objects,
@@ -26,4 +44,9 @@ class LeakAnalysisTask {
 
   late Map<IdentityHashCode, HeapObject> objects;
   final Iterable<ObjectReport> reports;
+
+  Map<String, dynamic> toJson() => {
+        'objects': objects,
+        'reports': reports,
+      };
 }
--- packages/devtools_app/lib/src/screens/memory/panes/leaks/retaining_path.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/retaining_path.dart
@@ -14,23 +14,12 @@ Future<void> setRetainingPaths(
 ) async {
   final graph = (await controller.snapshotMemory())!;
 
-  final pathAnalyzer = _PathAnalyzer(graph, reports);
+  final pathAnalyzer = _PathAnalyzer(graph);
 
   for (var report in reports) {
     report.retainingPath = pathAnalyzer.getPath(report.theIdentityHashCode);
   }
 }
-
-// class _PathItem {
-//   _PathItem(this.options);
-//
-//   final List<IdentityHashCode> options;
-//   int _index = 0;
-//
-//   IdentityHashCode get selection => options[_index];
-//   bool get canChange => _index < options.length - 1;
-//   void change() => _index++;
-// }
 
 class _PathAnalyzer {
   _PathAnalyzer(this.graph) {
@@ -38,7 +27,6 @@ class _PathAnalyzer {
   }
 
   final HeapSnapshotGraph graph;
-  final List<ObjectReport> reports;
   late Retainers _retainers;
   // All objects by hashcode.
   late Map<int, HeapSnapshotObject> _byCode;
@@ -70,54 +58,6 @@ class _PathAnalyzer {
     final result = path.map((i) => _name(_byCode[i]!)).join('/');
     return '/$result/';
   }
-
-  // String getPath(IdentityHashCode code) {
-  //   final path = [
-  //     _PathItem([code])
-  //   ];
-  //   IdentityHashCode current = code;
-  //
-  //   while (_retainers[current]?.isNotEmpty == true) {
-  //     if (path.length > 1000) throw 'Too large path.';
-  //
-  //     if (_retainers[current]!.length > 1) {
-  //       final list = _retainers[current]!
-  //           .toList()
-  //           .map((e) => _name(_byCode[e]!))
-  //           .join(', ');
-  //       print(list);
-  //     }
-  //
-  //     final options = _retainers[current]!.where((retainer) {
-  //       final loop =
-  //           path.firstWhereOrNull((element) => element.selection == retainer);
-  //       return loop == null;
-  //     }).toList();
-  //
-  //     if (options.isNotEmpty) {
-  //       path.insert(0, _PathItem(options));
-  //     } else {
-  //       int toChange = 0;
-  //       while (!path[toChange].canChange && toChange < path.length - 1) {
-  //         toChange++;
-  //       }
-  //       assert(path[toChange].canChange, 'All retainers looped the path.');
-  //       path[toChange].change();
-  //       path.removeRange(0, toChange);
-  //     }
-  //
-  //     current = path.first.selection;
-  //
-  //     assert(
-  //       current != code,
-  //       'loop found',
-  //     );
-  //     assert(!_shouldSkip(path.first.selection));
-  //   }
-  //
-  //   final result = path.map((e) => _name(_byCode[e.selection]!)).join('/');
-  //   return '/$result/';
-  // }
 
   static String _name(HeapSnapshotObject object) {
     return '${object.identityHashCode}-${object.klass.name}';
