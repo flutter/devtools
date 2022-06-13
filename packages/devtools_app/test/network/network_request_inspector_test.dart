4d8dab994d3c7b3d178de9a0ed6b239d7ad22f4c
Dan Chevalier <chevalier.dan@gmail.com>
2022-06-13 11:44:26 -0400

    got the test working!

--- packages/devtools_app/test/network/network_request_inspector_test.dart
+++ packages/devtools_app/test/network/network_request_inspector_test.dart
@@ -3,10 +3,12 @@ import 'package:devtools_app/src/screens/network/network_request_inspector.dart'
 import 'package:devtools_test/devtools_test.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:vm_service/vm_service.dart';
 
 import '../fixtures/riverpod_app/lib/tester.dart';
+import 'network_profiler_test.dart';
 import 'utils/network_test_utils.dart';
 
 void main() {
@@ -15,9 +17,11 @@ void main() {
     SocketProfile socketProfile;
     HttpProfile httpProfile;
     FakeServiceManager fakeServiceManager;
+    late String _clipboardContents;
 
-    setUpAll(() {
+    setUp(() {
       setGlobal(IdeTheme, IdeTheme());
+      _clipboardContents = '';
       socketProfile = loadSocketProfile();
       httpProfile = loadHttpProfile();
       fakeServiceManager = FakeServiceManager(
@@ -28,13 +32,26 @@ void main() {
       );
       setGlobal(ServiceConnectionManager, fakeServiceManager);
       controller = NetworkController();
+      // This intercepts the Clipboard.setData SystemChannel message,
+      // and stores the contents that were (attempted) to be copied.
+      SystemChannels.platform.setMockMethodCallHandler((MethodCall call) {
+        switch (call.method) {
+          case 'Clipboard.setData':
+            _clipboardContents = call.arguments['text'];
+            break;
+          case 'Clipboard.getData':
+            return Future.value(<String, dynamic>{});
+          case 'Clipboard.hasStrings':
+            return Future.value(<String, dynamic>{'value': true});
+          default:
+            break;
+        }
+
+        return Future.value(true);
+      });
     });
     testWidgets('response', (tester) async {
-      await controller.startRecording();
       final requestsNotifier = controller.requests;
-      await controller.networkService.refreshNetworkData();
-      final firstRequest = requestsNotifier.value.requests.first;
-      controller.selectRequest(firstRequest);
       final widget = NetworkRequestInspector(controller);
 
       final localizations = Localizations(
@@ -59,8 +76,29 @@ void main() {
         data: const MediaQueryData(platformBrightness: Brightness.dark),
         child: localizations,
       );
+      final debuggerController = createMockDebuggerControllerWithDefaults();
+
+      await controller.startRecording();
+      await tester
+          .pumpWidget(wrapWithControllers(media, debugger: debuggerController));
+      await controller.networkService.refreshNetworkData();
+      final firstRequest = requestsNotifier.value.requests[5];
+      print("THE REQS: ${requestsNotifier.value.requests}");
+      controller.selectRequest(firstRequest);
+      await tester.pumpAndSettle();
+      debugDumpApp();
+      await tester.tap(find.text('Response'));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(CopyToClipboardControl));
+      expect(_clipboardContents, isNotEmpty);
+      expect(
+        _clipboardContents,
+        equals((firstRequest as DartIOHttpRequestData).responseBody),
+      );
       controller.stopRecording();
-      await tester.pumpWidget(media);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
     });
   });
 }
+
+void buildNetworkRequestInspector() {}
