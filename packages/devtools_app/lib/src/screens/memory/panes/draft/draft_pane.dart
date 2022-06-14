ab310ee1f5904d89495b2b1aa62697469458adca
Polina Cherkasova <polina.c@live.com>
2022-06-14 15:47:34 -0700

    Update draft_pane.dart

--- packages/devtools_app/lib/src/screens/memory/panes/draft/draft_pane.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/draft/draft_pane.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vm_service/vm_service.dart';
+
+import '../../../../shared/globals.dart';
 
 class DraftPane extends StatefulWidget {
   const DraftPane({Key? key}) : super(key: key);
@@ -8,12 +11,55 @@ class DraftPane extends StatefulWidget {
 }
 
 class _DraftPaneState extends State<DraftPane> {
+  ClassHeapStats? _classHeapStats;
+  String _isolateId = '';
+
+  ObjRef? _instance;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Text('hello!!'),
+      children: [
+        const Text('hello!!'),
+        MaterialButton(
+            child: const Text('Try It'),
+            onPressed: () async {
+              setState(() {
+                _isolateId =
+                    serviceManager.isolateManager!.mainIsolate!.value!.id!;
+              });
+
+              final profile =
+                  await serviceManager.service!.getAllocationProfile(
+                _isolateId,
+                gc: true,
+              );
+
+              for (var item in profile.members!) {
+                if (item.classRef!.name == 'MyClass') {
+                  final isolateRef =
+                      serviceManager.isolateManager!.mainIsolate!.value!;
+                  final instances = await serviceManager.service!.getInstances(
+                    _isolateId,
+                    item.classRef!.id!,
+                    20,
+                  );
+
+                  setState(() {
+                    _classHeapStats = item;
+                    _instance = instances.instances!.first!;
+                    
+                  });
+
+                  _instance
+
+                  return;
+                }
+              }
+            }),
+        Text('${_classHeapStats?.type}-${_classHeapStats?.classRef?.id}'),
+        Text('${_instances?.totalCount}'),
       ],
     );
   }
