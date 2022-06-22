ab942360ead01ab6af97f23e15fbdd9aaec1d463
Daniel Chevalier <chevalier.dan@gmail.com>
2022-06-22 14:17:21 -0400

    Copy Network Reponse (#4190)
    Add the ability to copy the network response in our network tab

--- packages/devtools_app/test/test_utils/test_utils.dart
+++ packages/devtools_app/test/test_utils/test_utils.dart
@@ -4,7 +4,10 @@
 
 import 'dart:convert';
 
-import 'package:devtools_app/devtools_app.dart';
+import 'package:devtools_app/src/primitives/trace_event.dart';
+import 'package:devtools_app/src/screens/performance/performance_model.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 SyncTimelineEvent testSyncTimelineEvent(TraceEventWrapper eventWrapper) =>
     SyncTimelineEvent(eventWrapper);
@@ -15,4 +18,31 @@ TraceEvent testTraceEvent(Map<String, dynamic> json) =>
 int _testTimeReceived = 0;
 TraceEventWrapper testTraceEventWrapper(Map<String, dynamic> json) {
   return TraceEventWrapper(testTraceEvent(json), _testTimeReceived++);
+}
+
+/// Overrides the system's clipboard behaviour so that strings sent to the
+/// clipboard are instead passed to [clipboardContentsCallback]
+///
+/// [clipboardContentsCallback]  when Clipboard.setData is triggered, the text
+/// contents will be passed to [clipboardContentsCallback]
+void setupClipboardCopyListener({
+  required Function(String?) clipboardContentsCallback,
+}) {
+  // This intercepts the Clipboard.setData SystemChannel message,
+  // and stores the contents that were (attempted) to be copied.
+  SystemChannels.platform.setMockMethodCallHandler((MethodCall call) {
+    switch (call.method) {
+      case 'Clipboard.setData':
+        clipboardContentsCallback(call.arguments['text']);
+        break;
+      case 'Clipboard.getData':
+        return Future.value(<String, dynamic>{});
+      case 'Clipboard.hasStrings':
+        return Future.value(<String, dynamic>{'value': true});
+      default:
+        break;
+    }
+
+    return Future.value(true);
+  });
 }
