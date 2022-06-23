22fadd5bffdd2afececa3c89d80d1984bdfa9e88
Polina Cherkasova <polina.c@live.com>
2022-06-22 18:37:28 -0700

    Update constants.dart

--- packages/devtools_shared/lib/src/memory/leaks/constants.dart
+++ packages/devtools_shared/lib/src/memory/leaks/constants.dart
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// Name fo extension that enables leak tracking for applications.
+/// Name of extension that enables leak tracking for applications.
 const leakTrackingExtensionName = 'ext.memory-leak-tracking';
