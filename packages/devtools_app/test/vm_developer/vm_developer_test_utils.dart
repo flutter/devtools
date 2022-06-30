bb46a3c9a9a7de0dba2a7680900daf2945b86eea
Miguel Ty-Lee <mtaylee@google.com>
2022-06-30 16:22:15 -0700

    missed one copyright statement.

--- packages/devtools_app/test/vm_developer/vm_developer_test_utils.dart
+++ packages/devtools_app/test/vm_developer/vm_developer_test_utils.dart
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:devtools_app/src/screens/debugger/debugger_model.dart';
 import 'package:devtools_app/src/screens/vm_developer/object_inspector_view_controller.dart';
 import 'package:devtools_app/src/screens/vm_developer/object_viewport.dart';
