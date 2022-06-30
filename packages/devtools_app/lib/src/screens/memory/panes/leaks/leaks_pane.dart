06a64be34df9732d831e9e218a77fb09d87c1446
Polina Cherkasova <polina.c@live.com>
2022-06-30 04:19:32 -0700

    Update leaks_pane.dart

--- packages/devtools_app/lib/src/screens/memory/panes/leaks/leaks_pane.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/leaks_pane.dart
@@ -1,3 +1,4 @@
+import 'package:devtools_app/src/primitives/auto_dispose.dart';
 import 'package:flutter/material.dart';
 
 class LeaksPane extends StatefulWidget {
@@ -8,6 +9,32 @@ class LeaksPane extends StatefulWidget {
 }
 
 class _LeaksPaneState extends State<LeaksPane> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _subscribeForMemoryLeaksSummary();
+  }
+
+  void _subscribeForMemoryLeaksSummary() {
+    autoDisposeStreamSubscription(
+      serviceManager.service!.onExtensionEventWithHistory.listen((event) async {
+        if (event.extensionKind == 'memory_leaks_summary') {
+          final newSummary =
+              LeakSummary.fromJson(event.json!['extensionData']!);
+          if (newSummary.equals(_previous)) return;
+          _previous = newSummary;
+          final time = event.timestamp != null
+              ? DateTime.fromMicrosecondsSinceEpoch(event.timestamp!)
+              : DateTime.now();
+          setState(() {
+            _leaksSummary =
+                '${_timeForConsole(time)}: ${newSummary.toMessage()}\n$_leaksSummary';
+          });
+        }
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
