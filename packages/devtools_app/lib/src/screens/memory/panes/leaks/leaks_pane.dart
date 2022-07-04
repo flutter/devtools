06e07a7a32de2f7935286959b9e1aeb8fbc6cdb7
Polina Cherkasova <polina.c@live.com>
2022-07-04 09:43:08 -0700

    -

--- packages/devtools_app/lib/src/screens/memory/panes/leaks/leaks_pane.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/leaks_pane.dart
@@ -4,12 +4,16 @@ import '../../../../primitives/auto_dispose_mixin.dart';
 import '../../../../primitives/utils.dart';
 import '../../../../shared/globals.dart';
 import 'instrumentation/model.dart';
+import 'primitives/processing_status.dart';
+import '../../config_specific/logger/logger.dart' as logger;
 
 // TODO(polinach): reference this constant in dart SDK, when it gets submitted
 // there.
 // https://github.com/flutter/devtools/issues/3951
-const _extensionKindToRecieveLeaksSummary = 'memory_leaks_summary';
+const _extensionKindToReceiveLeaksSummary = 'memory_leaks_summary';
 
+// TODO(polina-c): review UX with UX specialists
+// https://github.com/flutter/devtools/issues/3951
 class LeaksPane extends StatefulWidget {
   const LeaksPane({Key? key}) : super(key: key);
 
@@ -20,44 +24,121 @@ class LeaksPane extends StatefulWidget {
 class _LeaksPaneState extends State<LeaksPane> with AutoDisposeMixin {
   LeakSummary? _lastLeakSummary;
   String _leakSummaryHistory = '';
+  final AnalysisStatusController _analysis = AnalysisStatusController();
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     cancelStreamSubscriptions();
-    _subscribeForMemoryLeaksSummary();
+    _subscribeForMemoryLeaksMessages();
   }
 
-  void _subscribeForMemoryLeaksSummary() {
-    autoDisposeStreamSubscription(
-      serviceManager.service!.onExtensionEventWithHistory.listen((event) async {
-        if (event.extensionKind == _extensionKindToRecieveLeaksSummary) {
-          final newSummary =
-              LeakSummary.fromJson(event.json!['extensionData']!);
-          if (newSummary.matches(_lastLeakSummary)) return;
-          _lastLeakSummary = newSummary;
-          final time = event.timestamp != null
-              ? DateTime.fromMicrosecondsSinceEpoch(event.timestamp!)
-              : DateTime.now();
-          setState(() {
-            _leakSummaryHistory =
-                '${formatDateTime(time)}: ${newSummary.toMessage()}\n$_leakSummaryHistory';
-          });
-        }
-      }),
-    );
+  // void _subscribeForMemoryLeaksMessages() {
+  //   autoDisposeStreamSubscription(
+  //     serviceManager.service!.onExtensionEventWithHistory.listen((event) async {
+  //       if (event.extensionKind == _extensionKindToReceiveLeaksSummary) {
+  //         final newSummary =
+  //             LeakSummary.fromJson(event.json!['extensionData']!);
+  //         if (newSummary.matches(_lastLeakSummary)) return;
+  //         _lastLeakSummary = newSummary;
+  //         final time = event.timestamp != null
+  //             ? DateTime.fromMicrosecondsSinceEpoch(event.timestamp!)
+  //             : DateTime.now();
+  //         setState(() {
+  //           _leakSummaryHistory =
+  //               '${formatDateTime(time)}: ${newSummary.toMessage()}\n$_leakSummaryHistory';
+  //         });
+  //       }
+  //
+  //       if (event.extensionKind == 'memory_leaks_details') {
+  //         try {
+  //           await setHeavyState(() {
+  //             _leakController.message = 'Received leaks. Parsing.';
+  //           });
+  //           final leakDetails = Leaks.fromJson(event.json!['extensionData']!);
+  //
+  //           await setHeavyState(() {
+  //             _leakController.message = 'Getting retaining paths.';
+  //           });
+  //           final notGCed = leakDetails.leaks[LeakType.notGCed] ?? [];
+  //
+  //           if (notGCed.isNotEmpty) {
+  //             final task = await getTask(controller, notGCed);
+  //             assert(task.reports.isNotEmpty);
+  //
+  //             await setHeavyState(() {
+  //               _leakController.message = 'Getting retaining paths.';
+  //               _leakController.previousAnalysisTask =
+  //                   jsonEncode(task.toJson());
+  //             });
+  //
+  //             calculateRetainingPathsOrRetainers(task);
+  //
+  //             assert(task.reports.first.retainingPath != null ||
+  //                 task.reports.first.retainers != null);
+  //           }
+  //
+  //           setState(
+  //             () => _leakController.message =
+  //                 'Obtained paths. Copying to clipboard',
+  //           );
+  //
+  //           await Clipboard.setData(
+  //             ClipboardData(text: analyzeAndYaml(leakDetails)),
+  //           );
+  //
+  //           setState(() {
+  //             _leakController.message = 'Copied to clipboard';
+  //             _leakController.isComplete = true;
+  //           });
+  //
+  //           await Future.delayed(const Duration(seconds: 1));
+  //
+  //           setState(() {
+  //             _leakController.reset();
+  //           });
+  //         } catch (e, trace) {
+  //           handleError(e, trace);
+  //         }
+  //       }
+  //     }),
+  //   );
+  // }
+
+  void _reportError(Object error, StackTrace trace) {
+    setState(() {
+      _analysis.message = 'Processing error: $error';
+      _analysis.status.value = AnalysisStatus.ShowingError;
+    });
+    logger.log(error);
+    logger.log(trace);
+  }
+
+  Future<void> _requestLeaks() async {
+    await eval.safeEval('sendLeaks()', isAlive: Stub());
+    print('!!!!!!!!! Requested leaks.');
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_leakSummaryHistory.isEmpty) return const Text('No information yet.');
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (_leakSummaryHistory.isEmpty) const Text('No information yet.'),
-        if (_leakSummaryHistory.isNotEmpty)
-          Expanded(
-            child: SingleChildScrollView(child: Text(_leakSummaryHistory)),
-          ),
+        (_analysis.status.value == AnalysisStatus.NotStarted)
+            ? Tooltip(
+                message: 'Analyze the leaks and save the result\n'
+                    'to the file Downloads/leaks_<time>.yaml.',
+                child: MaterialButton(
+                  child: const Text('Analyze and Save'),
+                  onPressed: () async => _requestLeaks(),
+                ),
+              )
+            : AnalysisStatusView(controller: _analysis),
+        Expanded(
+          child: SingleChildScrollView(child: Text(_leakSummaryHistory)),
+        ),
       ],
     );
   }
---
+++ packages/devtools_app/lib/src/screens/memory/panes/leaks/primitives/processing_status.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../../../../primitives/auto_dispose_mixin.dart';
+
+enum AnalysisStatus {
+  NotStarted,
+  Ongoing,
+
+  /// The process is complete and we want user to see the result.
+  ShowingResult,
+
+  /// The process is complete and we want user to see the error.
+  ShowingError,
+}
+
+Duration _showingResultDelay = const Duration(seconds: 4);
+Duration _delayForUiToHandleState = const Duration(milliseconds: 5);
+
+/// Describes status of the ongoing process.
+class AnalysisStatusController {
+  ValueNotifier<AnalysisStatus> status =
+      ValueNotifier<AnalysisStatus>(AnalysisStatus.NotStarted);
+
+  String message = '';
+
+  void reset() {
+    status.value = AnalysisStatus.NotStarted;
+    message = '';
+  }
+}
+
+class AnalysisStatusView extends StatefulWidget {
+  const AnalysisStatusView({Key? key, required this.controller})
+      : super(key: key);
+  final AnalysisStatusController controller;
+
+  @override
+  State<AnalysisStatusView> createState() => _AnalysisStatusViewState();
+}
+
+class _AnalysisStatusViewState extends State<AnalysisStatusView>
+    with AutoDisposeMixin {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    addAutoDisposeListener(widget.controller.status, () async {
+      setState(() {});
+      if (widget.controller.status.value == AnalysisStatus.ShowingResult) {
+        await Future.delayed(_showingResultDelay);
+        setState(
+          () => widget.controller.reset(),
+        );
+      }
+      // We need this delay, because analysis may include heavy computations,
+      // and we want to give a space for UI thread to show status.
+      await Future.delayed(_delayForUiToHandleState);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final c = widget.controller;
+    if (c.status.value != AnalysisStatus.NotStarted) {
+      return const SizedBox.shrink();
+    }
+
+    return Column(
+      children: [
+        Text(c.message),
+        if (c.status.value == AnalysisStatus.ShowingError)
+          Row(
+            children: [
+              MaterialButton(
+                child: const Icon(Icons.copy),
+                onPressed: () async =>
+                    await Clipboard.setData(ClipboardData(text: c.message)),
+              ),
+              MaterialButton(
+                child: const Text('OK'),
+                onPressed: () => setState(() => c.reset()),
+              ),
+            ],
+          )
+      ],
+    );
+  }
+}
