91153980cbe184f23b7b4d92b77299588492adbc
Dan Chevalier <chevalier.dan@gmail.com>
2022-07-13 16:22:36 -0400

    removeAll unit test

--- packages/devtools_app/test/shared/utils_test.dart
+++ packages/devtools_app/test/shared/utils_test.dart
@@ -1161,6 +1161,13 @@ void main() {
         expect(notifier.value, equals([1, 3]));
       });
 
+      test('notifies on removeAll', () {
+        setUpWithInitialValue([1, 2, 3, 4]);
+        notifier.removeAll([1, 3]);
+        expect(didNotify, isTrue);
+        expect(notifier.value, equals([2, 4]));
+      });
+
       test('does not notify on remove of missing element', () {
         setUpWithInitialValue([1, 2, 3]);
         notifier.remove(0);
