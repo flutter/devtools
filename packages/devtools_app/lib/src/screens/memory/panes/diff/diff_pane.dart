e6744e03ff94f8835e1bbc8d99507ae4ad832c6d
Polina Cherkasova <polina.c@live.com>
2022-07-14 14:36:10 -0700

    -

---
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/diff_pane.dart
@@ -0,0 +1,21 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+const shouldShowDiffPane = false;
+
+class DiffPane extends StatefulWidget {
+  const DiffPane({Key? key}) : super(key: key);
+
+  @override
+  State<DiffPane> createState() => _DiffPaneState();
+}
+
+class _DiffPaneState extends State<DiffPane> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hello, I am diff pane');
+  }
+}
