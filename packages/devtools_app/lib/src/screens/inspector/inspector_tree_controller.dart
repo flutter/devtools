19c25bbfa20ed9b053f0cd05ade01ab1b5f821b7
Dan Chevalier <chevalier.dan@gmail.com>
2022-07-15 14:40:10 -0400

    Revert "Don't scroll proportionally, scroll accurately"
    This reverts commit a5f781951202646db52a249f99be69b21025a896.

--- packages/devtools_app/lib/src/screens/inspector/inspector_tree_controller.dart
+++ packages/devtools_app/lib/src/screens/inspector/inspector_tree_controller.dart
@@ -889,7 +889,9 @@ class _InspectorTreeState extends State<InspectorTree>
       _scrollControllerY = ScrollController(initialScrollOffset: targetY);
     }
 
-    final targetX = rect.left;
+    // Determine a target X coordinate consistent with the target Y coordinate
+    // we will end up as so we get a smooth animation to the final destination.
+    final targetX = _computeTargetX(targetY);
     if (_scrollControllerX.hasClients) {
       unawaited(
         _scrollControllerX.animateTo(
