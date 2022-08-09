59661072ea5b6bf4c6136a3d3a727fc00566836b
Miguel Ty-Lee <mtaylee@google.com>
2022-08-09 15:28:41 -0700

    loadtime non-nullable

--- packages/devtools_app/lib/src/screens/vm_developer/vm_service_private_extensions.dart
+++ packages/devtools_app/lib/src/screens/vm_developer/vm_service_private_extensions.dart
@@ -260,5 +260,5 @@ enum GuardClassKind {
 /// An extension on [Script] which allows for access to VM internal fields.
 extension ScriptPrivateViewExtension on Script {
   static const _loadTimeKey = '_loadTime';
-  int? get loadTime => json![_loadTimeKey];
+  int get loadTime => json![_loadTimeKey]!;
 }
