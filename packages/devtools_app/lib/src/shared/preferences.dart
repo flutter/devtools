434ac9e278dcbe2ccc129b6edf0fbc7377f21953
Dan Chevalier <chevalier.dan@gmail.com>
2022-08-17 14:45:45 -0400

    prevent //// as pub root dir

--- packages/devtools_app/lib/src/shared/preferences.dart
+++ packages/devtools_app/lib/src/shared/preferences.dart
@@ -200,6 +200,13 @@ class InspectorPreferencesController extends DisposableController
   Future<void> addPubRootDirectories(
     List<String> pubRootDirectories,
   ) async {
+    // TODO(https://github.com/flutter/devtools/issues/4380):
+    // Add validation to EditableList Input.
+    // Directories of just / will break the inspector tree local package checks.
+    pubRootDirectories.removeWhere(
+      (element) => element.replaceAll(RegExp('\/'), '').trim() == '',
+    );
+
     if (!serviceManager.hasConnection) return;
     await _customPubRootDirectoryBusyTracker(() async {
       final inspectorService = _inspectorService;
