8be3dd4af0cd09e460318a9c5b64df7f62a3ef0c
Ben Konyi <bkonyi@google.com>
2022-09-28 15:32:17 -0400

    Update allocation profile column names (#4545)

--- packages/devtools_app/lib/src/screens/memory/shared/heap/primitives.dart
+++ packages/devtools_app/lib/src/screens/memory/shared/heap/primitives.dart
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 const String shallowSizeColumnTooltip =
-    'Total shallow size of all of the instances.\n'
-    'Shallow size of an object is the size of this object\n'
+    'The total shallow size of all of the instances.\n'
+    'The shallow size of an object is the size of the object\n'
     'plus the references it holds to other Dart objects\n'
     "in its fields (this doesn't include the size of\n"
     'the fields - just the size of the references).';
