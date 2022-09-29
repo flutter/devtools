a7feadb01f6664559c0f9180aa21b4ddd91e3b8d
Polina Cherkasova <polinach@google.com>
2022-09-28 18:04:57 -0700

    Update class_details.dart

--- packages/devtools_app/lib/src/screens/memory/panes/diff/widgets/class_details.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/widgets/class_details.dart
@@ -4,11 +4,31 @@
 
 import 'package:flutter/widgets.dart';
 
+import '../controller/diff_pane_controller.dart';
+
 class HeapClassDetails extends StatelessWidget {
-  const HeapClassDetails({super.key});
+  const HeapClassDetails({
+    Key? key, required this.controller,
+  }) : super(key: key);
+
+  final DiffPaneController controller;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Class details will be here');
+    return ValueListenableBuilder<HeapClass?>(
+      valueListenable: item.selectedHeapClass,
+      builder: (_, classStats, __) {
+        if (classStats == null) {
+          return const Center(
+            child: Text('Select class to see details here.'),
+          );
+        }
+
+        return ClassStatsTable(
+          data: classStats,
+          sorting: sorting,
+        );
+      },
+    );
   }
 }
