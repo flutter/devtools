be1f0b7e5ef1bc15b2953a266d082252be615976
Polina Cherkasova <polinach@google.com>
2022-10-14 13:04:18 -0700

    Create path.dart

---
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/widgets/class_details/path.dart
@@ -0,0 +1,41 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/widgets.dart';
+
+import '../../../../shared/heap/model.dart';
+
+class RetainingPathView extends StatelessWidget {
+  const RetainingPathView({super.key, required this.path});
+
+  final ClassOnlyHeapPath path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _PathControlPane extends StatefulWidget {
+
+
+  @override
+  State<_PathControlPane> createState() => _PathControlPaneState();
+}
+
+class _PathControlPaneState extends State<_PathControlPane> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _PathText extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
