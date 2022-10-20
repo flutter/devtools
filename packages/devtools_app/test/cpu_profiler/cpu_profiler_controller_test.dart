d03244e2bff5a47fc284acabf56f078cdfdcdb51
Ben Konyi <bkonyi@google.com>
2022-10-20 15:22:44 -0400

    Fix tests

--- packages/devtools_app/test/cpu_profiler/cpu_profiler_controller_test.dart
+++ packages/devtools_app/test/cpu_profiler/cpu_profiler_controller_test.dart
@@ -7,6 +7,7 @@ import 'package:devtools_app/src/screens/profiler/cpu_profile_controller.dart';
 import 'package:devtools_app/src/screens/profiler/cpu_profile_model.dart';
 import 'package:devtools_app/src/service/service_manager.dart';
 import 'package:devtools_app/src/shared/globals.dart';
+import 'package:devtools_app/src/shared/preferences.dart';
 import 'package:devtools_test/devtools_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -30,6 +31,7 @@ void main() {
     setUp(() {
       setGlobal(ServiceConnectionManager, fakeServiceManager);
       setGlobal(OfflineModeController, OfflineModeController());
+      setGlobal(PreferencesController, PreferencesController());
       controller = CpuProfilerController();
     });
 
