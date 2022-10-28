522eacb5a805cf0b1a99e7c90a0440b20a4e4035
Elliott Brooks (she/her) <21270878+elliette@users.noreply.github.com>
2022-10-28 15:03:41 -0700

    Prepare dev release for g3 roll (#4655)

--- packages/devtools_app/lib/devtools.dart
+++ packages/devtools_app/lib/devtools.dart
@@ -7,4 +7,4 @@
 // that updates all versions for DevTools.
 // Note: a regexp in tools/update_version.dart matches the following line so
 // if you change it you must also modify tools/update_version.dart.
-const String version = '2.19.0-dev.1';
+const String version = '2.19.0-dev.2';
