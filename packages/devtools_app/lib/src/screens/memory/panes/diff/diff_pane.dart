7c569b877d66d3d315b9704ca2a5d0dd4f640bc2
Polina Cherkasova <polinach@google.com>
2022-11-02 12:47:22 -0700

    Update diff_pane.dart

--- packages/devtools_app/lib/src/screens/memory/panes/diff/diff_pane.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/diff_pane.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../../../../shared/common_widgets.dart';
 import '../../../../shared/split.dart';
@@ -55,7 +56,7 @@ class _SnapshotItemContent extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(_snapshotDocumentation),
+                const Expanded(child: Markdown(data: _snapshotDocumentation)),
                 const SizedBox(height: denseSpacing),
                 IconLabelButton(
                   onPressed: () async => await controller.takeSnapshot(),
@@ -84,22 +85,25 @@ class _SnapshotItemContent extends StatelessWidget {
   }
 }
 
+/// `\v` adds vertical space
 const _snapshotDocumentation = '''
-Take a heap snapshot to view current memory allocation:
+Take a **heap snapshot** to view current memory allocation:
 
 1. In the Snapshots panel, click the ● button
-(or click *Take Snapshot* below these instructions)
-2. Use the Filter button to refine the results
+(or click **Take Snapshot** below these instructions)
+2. Use the **Filter** button to refine the results
 3. Select a class from the snapshot table to view its retaining paths
-4. View the path detail by selecting from the Shortest retaining paths… table
+4. View the path detail by selecting from the **Shortest Retaining Paths…** table
 
-Check the diff between snapshots to detect allocation issues:
+\v
 
-1. Take a snapshot
+Check the **diff** between snapshots to detect allocation issues:
+
+1. Take a **snapshot**
 2. Execute the feature in your application
 3. Take a second snapshot
-4. While viewing the second snapshot, click Diff with: and select the first snapshot from the drop-down menu;
+4. While viewing the second snapshot, click **Diff with:** and select the first snapshot from the drop-down menu;
 the results area will display the diff
-5. Use the Filter button to refine the diff results, if needed
+5. Use the **Filter** button to refine the diff results, if needed
 6. Select a class from the diff to view its retaining paths, and see which objects hold the references to those instances
 ''';
