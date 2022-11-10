820622357e3811ef1e5e36e74aa902bf86bdc14e
Dan Chevalier <chevalier.dan@gmail.com>
2022-11-10 14:27:06 -0500

    another bit of cleanup

--- tool/latest_flutter_candidate.sh
+++ tool/latest_flutter_candidate.sh
@@ -3,7 +3,9 @@
 # Copyright 2022 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 RESPONSE=$(gh api --paginate /repos/flutter/flutter/branches)
+
 CANDIDATES=$(echo "$RESPONSE" | jq '.[].name' | grep candidate)
 
 VERSIONS=$(echo "$CANDIDATES" | sed -E 's/.*([0-9]+\.[0-9]+-candidate\.[0-9]+).*/\1/' )
