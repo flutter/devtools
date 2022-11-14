bae03a4cc11fb9366e627907af9a548d07a12172
Polina Cherkasova <polinach@google.com>
2022-11-14 10:57:01 -0800

    Update cpu_profiler_test.dart

--- packages/devtools_app/test/cpu_profiler/cpu_profiler_test.dart
+++ packages/devtools_app/test/cpu_profiler/cpu_profiler_test.dart
@@ -409,7 +409,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_call_tree_no_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_call_tree_no_guidelines.png',
         ),
       );
       await tester.tap(find.byType(DisplayTreeGuidelinesToggle));
@@ -419,7 +419,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_call_tree_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_call_tree_guidelines.png',
         ),
       );
       await tester.tap(find.byType(DisplayTreeGuidelinesToggle));
@@ -429,7 +429,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_call_tree_no_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_call_tree_no_guidelines.png',
         ),
       );
 
@@ -445,7 +445,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_bottom_up_no_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_bottom_up_no_guidelines.png',
         ),
       );
       await tester.tap(find.byType(DisplayTreeGuidelinesToggle));
@@ -455,7 +455,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_bottom_up_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_bottom_up_guidelines.png',
         ),
       );
       await tester.tap(find.byType(DisplayTreeGuidelinesToggle));
@@ -465,7 +465,7 @@ void main() {
       await expectLater(
         find.byType(CpuProfiler),
         matchesDevToolsGolden(
-          '../goldens/cpu_profiler_bottom_up_no_guidelines.png',
+          '../test_infra/goldens/cpu_profiler_bottom_up_no_guidelines.png',
         ),
       );
     });
