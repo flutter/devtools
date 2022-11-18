e27888c3c46d142246d44e166dcafc4d116683bb
Polina Cherkasova <polinach@google.com>
2022-11-18 15:40:27 -0800

    Update legend.dart

--- packages/devtools_app/lib/src/screens/memory/panes/chart/legend.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/chart/legend.dart
@@ -96,7 +96,7 @@ class MemoryChartWidget extends StatelessWidget {
     return Container(
       width: legendWidth,
       height: isAndroidVisible ? legendHeight2Charts : legendHeight1Chart,
-      padding: const EdgeInsets.fromLTRB(0, 5, 5, 8),
+      padding: const EdgeInsets.fromLTRB(0, 5, 5, 0),
       decoration: BoxDecoration(
         color: colorScheme.defaultBackgroundColor,
         border: Border.all(color: theme.focusColor),
