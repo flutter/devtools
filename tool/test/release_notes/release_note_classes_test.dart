f4b0f1f907bf4acef3e46c90c21168abe1d72df3
Dan Chevalier <chevalier.dan@gmail.com>
2022-11-30 13:53:28 -0500

    only grab branch name from lfc.sh clean up beta readme

---
+++ tool/test/release_notes/release_note_classes_test.dart
@@ -0,0 +1,15 @@
+// import 'package:devtools_repo/release_note_classes.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   group('release_note_classes', () {
+//     test('can serialize and desi')
+//     group('ReleaseNotes', () {});
+//     group('Release', () {});
+//     group('ReleaseSection', ()   {});
+//     group('ReleaseNote', () {
+//       test('validates that images exist', () => null)
+
+//     });
+//   });
+// }
