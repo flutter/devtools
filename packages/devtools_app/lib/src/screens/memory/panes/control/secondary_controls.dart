f5ae6347dccdbdb336c462e26138e6a4ba2a87a2
Polina Cherkasova <polina.c@live.com>
2022-12-05 13:26:59 -0800

    -

--- packages/devtools_app/lib/src/screens/memory/panes/control/secondary_controls.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/control/secondary_controls.dart
@@ -13,7 +13,7 @@ import '../../../../shared/globals.dart';
 import '../../../../shared/theme.dart';
 import '../../memory_controller.dart';
 import '../../primitives/ui.dart';
-import '../../shared/constants.dart';
+import '../../shared/primitives.dart';
 import '../chart/chart_pane_controller.dart';
 import 'primitives.dart';
 import 'settings_dialog.dart';
