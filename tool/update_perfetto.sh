28aa720867c3e8416501bd210ef4bddbaa5c43ad
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2022-12-06 11:32:37 -0800

    Delete perfetto files related to legacy catapult trace viewer (#4899)

--- tool/update_perfetto.sh
+++ tool/update_perfetto.sh
@@ -42,6 +42,8 @@ fi
 
 echo "UPDATE_PERFETTO: Deleting unnecessary js source map files"
 find ./ -name '*.js.map' -exec rm {} \;
+find ./ -name 'traceconv.wasm' -exec rm {} \;
+find ./ -name 'traceconv_bundle.js' -exec rm {} \;
 
 echo "UPDATE_PERFETTO: Moving DevTools-Perfetto integration files back from _tmp/"
 mkdir dist/devtools
