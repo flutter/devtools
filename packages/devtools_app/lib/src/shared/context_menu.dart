14b2a580368c17cc036fceec7d521de4d24b01a1
Polina Cherkasova <polina.c@live.com>
2022-12-28 19:34:04 -0800

    -

--- packages/devtools_app/lib/src/shared/context_menu.dart
+++ packages/devtools_app/lib/src/shared/context_menu.dart
@@ -3,35 +3,30 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'analytics/analytics.dart' as ga;
 
 class ContextMenuButton extends StatefulWidget {
   const ContextMenuButton({
-    this.tooltip = 'Open context menu',
-    this.buttonKey,
-    this.size,
     this.style,
     this.gaScreen,
     this.gaItem,
+    required this.menu,
   });
 
   static const double width = 14;
 
   final TextStyle? style;
-  final String tooltip;
-  final Key? buttonKey;
-  final double? size;
   final String? gaScreen;
   final String? gaItem;
+  final List<Widget> menu;
 
   @override
   State<ContextMenuButton> createState() => _ContextMenuButtonState();
 }
 
 class _ContextMenuButtonState extends State<ContextMenuButton> {
-  final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+  //final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
 
   @override
   Widget build(BuildContext context) {
@@ -42,69 +37,26 @@ class _ContextMenuButtonState extends State<ContextMenuButton> {
     };
 
     return MenuAnchor(
-      menuChildren: [
-        MenuItemButton(
-          child: Text('MenuEntry.about.label'),
-          onPressed: () => print('click'),
-        ),
-        MenuItemButton(
-          child: Text('MenuEntry.about.label'),
-          onPressed: () => print('click'),
-        ),
-        MenuItemButton(
-          child: Text('MenuEntry.about.label'),
-          onPressed: () => print('click'),
-        ),
-        MenuItemButton(
-          child: Text('MenuEntry.about.label'),
-          onPressed: () => print('click'),
-        ),
-        SubmenuButton(
-          menuChildren: <Widget>[
-            MenuItemButton(
-              onPressed: () => print('click'),
-              child: Text('MenuEntry.colorRed.label'),
-            ),
-            MenuItemButton(
-              onPressed: () => print('click'),
-              child: Text('MenuEntry.colorGreen.label'),
-            ),
-            MenuItemButton(
-              onPressed: () => print('click'),
-              child: Text('MenuEntry.colorBlue.label'),
-            ),
-          ],
-          child: const Text('Background Color'),
-        ),
-      ],
+      menuChildren: widget.menu,
       builder:
           (BuildContext context, MenuController controller, Widget? child) {
-        return TextButton(
-          focusNode: _buttonFocusNode,
-          onPressed: () {
-            if (controller.isOpen) {
-              controller.close();
-            } else {
-              controller.open();
-            }
-          },
-          child: _MenuButton(style: widget.style),
-        );
+        return _MenuButton(style: widget.style, controller: controller);
       },
     );
   }
 
   @override
   void dispose() {
-    _buttonFocusNode.dispose();
+    //_buttonFocusNode.dispose();
     super.dispose();
   }
 }
 
 class _MenuButton extends StatelessWidget {
-  const _MenuButton({this.style});
+  const _MenuButton({this.style, required this.controller});
 
   final TextStyle? style;
+  final MenuController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -112,8 +64,15 @@ class _MenuButton extends StatelessWidget {
       width: ContextMenuButton.width,
       child: MaterialButton(
         padding: const EdgeInsets.all(0),
-        onPressed: null,
         child: Text('⋮', style: style, textAlign: TextAlign.center),
+        //focusNode: _buttonFocusNode,
+        onPressed: () {
+          if (controller.isOpen) {
+            controller.close();
+          } else {
+            controller.open();
+          }
+        },
       ),
     );
   }
