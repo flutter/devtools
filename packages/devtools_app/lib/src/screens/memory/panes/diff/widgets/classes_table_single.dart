f845858256f2f952676ae26f4683fabc17044e1d
Polina Cherkasova <polina.c@live.com>
2023-01-03 10:52:34 -0800

    Update classes_table_single.dart

--- packages/devtools_app/lib/src/screens/memory/panes/diff/widgets/classes_table_single.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/widgets/classes_table_single.dart
@@ -6,12 +6,14 @@ import 'package:flutter/material.dart';
 
 import '../../../../../shared/analytics/analytics.dart' as ga;
 import '../../../../../shared/analytics/constants.dart' as gac;
+import '../../../../../shared/feature_flags.dart';
 import '../../../../../shared/primitives/utils.dart';
 import '../../../../../shared/table/table.dart';
 import '../../../../../shared/table/table_data.dart';
 import '../../../../../shared/theme.dart';
 import '../../../../../shared/utils.dart';
 import '../../../shared/heap/heap.dart';
+import '../../../shared/primitives/instance_set_view.dart';
 import '../../../shared/primitives/simple_elements.dart';
 import '../../../shared/shared_memory_widgets.dart';
 
@@ -54,7 +56,8 @@ class _ClassNameColumn extends ColumnData<SingleClassStats>
   }
 }
 
-class _InstanceColumn extends ColumnData<SingleClassStats> {
+class _InstanceColumn extends ColumnData<SingleClassStats>
+    implements ColumnRenderer<SingleClassStats> {
   _InstanceColumn()
       : super(
           'Instances',
@@ -68,6 +71,32 @@ class _InstanceColumn extends ColumnData<SingleClassStats> {
 
   @override
   bool get numeric => true;
+
+  @override
+  Widget? build(
+    BuildContext context,
+    SingleClassStats data, {
+    bool isRowSelected = false,
+    VoidCallback? onPressed,
+  }) {
+    if (!FeatureFlags.evalAndBrowse) return null;
+
+    final theme = Theme.of(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        InstanceSetView(
+          textStyle:
+              isRowSelected ? theme.selectedTextStyle : theme.regularTextStyle,
+          count: getValue(data),
+          gaContext: gac.MemoryAreas.snapshotSingle,
+          sampleObtainer: isRowSelected ? () => throw 'not implemented' : null,
+          showMenu: isRowSelected,
+        ),
+      ],
+    );
+  }
 }
 
 class _ShallowSizeColumn extends ColumnData<SingleClassStats> {
