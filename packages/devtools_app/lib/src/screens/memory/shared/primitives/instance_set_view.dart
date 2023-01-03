63df8763ce43cb03d6e47e77b64e57ed18e28a85
Polina Cherkasova <polina.c@live.com>
2023-01-03 10:41:18 -0800

    -

---
+++ packages/devtools_app/lib/src/screens/memory/shared/primitives/instance_set_view.dart
@@ -0,0 +1,95 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:vm_service/vm_service.dart';
+
+import '../../../../shared/analytics/constants.dart';
+import '../../../../shared/common_widgets.dart';
+
+typedef SampleObtainer = InstanceRef Function();
+
+class InstanceSetView extends StatelessWidget {
+  const InstanceSetView({
+    super.key,
+    required this.count,
+    required this.sampleObtainer,
+    required this.showMenu,
+    this.textStyle,
+    required this.gaContext,
+  }) : assert(showMenu == (sampleObtainer != null));
+
+  final int count;
+  final SampleObtainer? sampleObtainer;
+  final bool showMenu;
+  final TextStyle? textStyle;
+  final MemoryAreas gaContext;
+
+  @override
+  Widget build(BuildContext context) {
+    final format = NumberFormat.decimalPattern();
+
+    return Row(
+      children: [
+        Text(
+          format.format(count),
+          style: textStyle,
+        ),
+        if (showMenu)
+          ContextMenuButton(
+            style: textStyle,
+            menu: _menu(),
+          ),
+        if (!showMenu) const SizedBox(width: ContextMenuButton.width),
+      ],
+    );
+  }
+}
+
+List<Widget> _menu() => [
+      SubmenuButton(
+        menuChildren: <Widget>[
+          MenuItemButton(
+            onPressed: () => print('get fields'),
+            child: const Text('Fields'),
+          ),
+          const MenuItemButton(
+            child: Text('Outgoing references'),
+          ),
+          const MenuItemButton(
+            child: Text('Incoming references'),
+          ),
+        ],
+        child: const Text('Store one instance as a console variable'),
+      ),
+      const SubmenuButton(
+        menuChildren: <Widget>[
+          MenuItemButton(
+            child: Text('Fields'),
+          ),
+          MenuItemButton(
+            child: Text('Outgoing references'),
+          ),
+          MenuItemButton(
+            child: Text('Incoming references'),
+          ),
+        ],
+        child: Text('Store 100 instances as a console variable'),
+      ),
+      const SubmenuButton(
+        menuChildren: <Widget>[
+          MenuItemButton(
+            child: Text('Fields'),
+          ),
+          MenuItemButton(
+            child: Text('Outgoing references'),
+          ),
+          MenuItemButton(
+            child: Text('Incoming references'),
+          ),
+        ],
+        child: Text('Store all instances as a console variable'),
+      ),
+    ];
