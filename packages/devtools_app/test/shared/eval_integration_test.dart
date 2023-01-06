303975d741dfb4d3f84312cdc7ba3bd8ca7a0612
Kenzie Schmoll <kenzieschmoll@google.com>
2023-01-05 16:55:47 -0800

    re-enable tests

--- packages/devtools_app/test/shared/eval_integration_test.dart
+++ packages/devtools_app/test/shared/eval_integration_test.dart
@@ -10,135 +10,131 @@ import '../test_infra/flutter_test_driver.dart';
 import '../test_infra/flutter_test_environment.dart';
 
 void main() {
-  // TODO(kenz): rewrite this test as a true integration test.
-  // ignore: dead_code
-  if (false) {
-    final FlutterTestEnvironment env = FlutterTestEnvironment(
-      const FlutterRunConfiguration(withDebugger: true),
+  final FlutterTestEnvironment env = FlutterTestEnvironment(
+    const FlutterRunConfiguration(withDebugger: true),
+  );
+
+  late Disposable isAlive;
+
+  setUp(() {
+    isAlive = Disposable();
+  });
+
+  tearDown(() async {
+    isAlive.dispose();
+    await env.tearDownEnvironment(force: true);
+  });
+
+  group('EvalOnDartLibrary', () {
+    test(
+      'getHashCode',
+      () async {
+        await env.setupEnvironment();
+        final eval = EvalOnDartLibrary('dart:core', serviceManager.service!);
+
+        final instance = await eval.safeEval('42', isAlive: isAlive);
+
+        await expectLater(
+          eval.getHashCode(instance, isAlive: isAlive),
+          completion(anyOf(isPositive, 0)),
+        );
+      },
+      // timeout: const Timeout.factor(8),
     );
 
-    late Disposable isAlive;
-
-    setUp(() {
-      isAlive = Disposable();
-    });
-
-    tearDown(() async {
-      isAlive.dispose();
-      await env.tearDownEnvironment(force: true);
-    });
-
-    group('EvalOnDartLibrary', () {
+    group('asyncEval', () {
       test(
-        'getHashCode',
+        'supports expresions that do not start with the await keyword',
         () async {
           await env.setupEnvironment();
-          final eval = EvalOnDartLibrary('dart:core', serviceManager.service!);
 
-          final instance = await eval.safeEval('42', isAlive: isAlive);
-
-          await expectLater(
-            eval.getHashCode(instance, isAlive: isAlive),
-            completion(anyOf(isPositive, 0)),
+          final eval = EvalOnDartLibrary(
+            'dart:core',
+            serviceManager.service!,
           );
+
+          final instance = (await eval.asyncEval('42', isAlive: isAlive))!;
+          expect(instance.valueAsString, '42');
+
+          final instance2 =
+              (await eval.asyncEval('Future.value(42)', isAlive: isAlive))!;
+          expect(instance2.classRef!.name, '_Future');
         },
-        timeout: const Timeout.factor(8),
+        // timeout: const Timeout.factor(8),
       );
 
-      group('asyncEval', () {
-        test(
-          'supports expresions that do not start with the await keyword',
-          () async {
-            await env.setupEnvironment();
+      test(
+        'returns the result of the future completion',
+        () async {
+          await env.setupEnvironment();
+          final mainIsolate = serviceManager.isolateManager.mainIsolate;
+          expect(mainIsolate, isNotNull);
 
-            final eval = EvalOnDartLibrary(
-              'dart:core',
-              serviceManager.service!,
-            );
+          final eval = EvalOnDartLibrary(
+            'dart:core',
+            serviceManager.service!,
+            isolate: mainIsolate,
+          );
 
-            final instance = (await eval.asyncEval('42', isAlive: isAlive))!;
-            expect(instance.valueAsString, '42');
+          final instance = (await eval.asyncEval(
+            // The delay asserts that there is no issue with garbage collection
+            'await Future<int>.delayed(const Duration(milliseconds: 500), () => 42)',
+            isAlive: isAlive,
+          ))!;
 
-            final instance2 =
-                (await eval.asyncEval('Future.value(42)', isAlive: isAlive))!;
-            expect(instance2.classRef!.name, '_Future');
-          },
-          timeout: const Timeout.factor(8),
-        );
+          expect(instance.valueAsString, '42');
+        },
+        // timeout: const Timeout.factor(8),
+      );
 
-        test(
-          'returns the result of the future completion',
-          () async {
-            await env.setupEnvironment();
-            final mainIsolate = serviceManager.isolateManager.mainIsolate;
-            expect(mainIsolate, isNotNull);
+      test(
+        'throws FutureFailedException when the future is rejected',
+        () async {
+          await env.setupEnvironment();
 
-            final eval = EvalOnDartLibrary(
-              'dart:core',
-              serviceManager.service!,
-              isolate: mainIsolate,
-            );
+          final eval = EvalOnDartLibrary(
+            'dart:core',
+            serviceManager.service!,
+          );
 
-            final instance = (await eval.asyncEval(
-              // The delay asserts that there is no issue with garbage collection
-              'await Future<int>.delayed(const Duration(milliseconds: 500), () => 42)',
-              isAlive: isAlive,
-            ))!;
+          final instance = await eval
+              .asyncEval(
+                'await Future.error(StateError("foo"), StackTrace.current)',
+                isAlive: isAlive,
+              )
+              .then<FutureFailedException>(
+                (_) => throw Exception(
+                  'The FutureFailedException was not thrown as expected.',
+                ),
+                onError: (err) => err,
+              );
 
-            expect(instance.valueAsString, '42');
-          },
-          timeout: const Timeout.factor(8),
-        );
+          expect(
+            instance.expression,
+            'await Future.error(StateError("foo"), StackTrace.current)',
+          );
 
-        test(
-          'throws FutureFailedException when the future is rejected',
-          () async {
-            await env.setupEnvironment();
+          final stack = await eval.safeEval(
+            'stack.toString()',
+            isAlive: isAlive,
+            scope: {
+              'stack': instance.stacktraceRef.id!,
+            },
+          );
+          expect(
+            stack.valueAsString,
+            startsWith('#0      Eval.<anonymous closure> ()'),
+          );
 
-            final eval = EvalOnDartLibrary(
-              'dart:core',
-              serviceManager.service!,
-            );
-
-            final instance = await eval
-                .asyncEval(
-                  'await Future.error(StateError("foo"), StackTrace.current)',
-                  isAlive: isAlive,
-                )
-                .then<FutureFailedException>(
-                  (_) => throw Exception(
-                    'The FutureFailedException was not thrown as expected.',
-                  ),
-                  onError: (err) => err,
-                );
-
-            expect(
-              instance.expression,
-              'await Future.error(StateError("foo"), StackTrace.current)',
-            );
-
-            final stack = await eval.safeEval(
-              'stack.toString()',
-              isAlive: isAlive,
-              scope: {
-                'stack': instance.stacktraceRef.id!,
-              },
-            );
-            expect(
-              stack.valueAsString,
-              startsWith('#0      Eval.<anonymous closure> ()'),
-            );
-
-            final error = await eval.safeEval(
-              'error.message',
-              isAlive: isAlive,
-              scope: {'error': instance.errorRef.id!},
-            );
-            expect(error.valueAsString, 'foo');
-          },
-          timeout: const Timeout.factor(8),
-        );
-      });
+          final error = await eval.safeEval(
+            'error.message',
+            isAlive: isAlive,
+            scope: {'error': instance.errorRef.id!},
+          );
+          expect(error.valueAsString, 'foo');
+        },
+        // timeout: const Timeout.factor(8),
+      );
     });
-  }
+  });
 }
