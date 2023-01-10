ce407ba8c1f7374adf9cbe4a9790b2fe5b8b86fa
Kenzie Schmoll <kenzieschmoll@google.com>
2023-01-10 12:33:05 -0800

    formatting

--- packages/devtools_app/integration_test/test/app_test.dart
+++ packages/devtools_app/integration_test/test/app_test.dart
@@ -12,7 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
