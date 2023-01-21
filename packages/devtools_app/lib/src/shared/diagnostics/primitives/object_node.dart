13041c67629d7dfaa74a2dd8f950ec6145a51803
Polina Cherkasova <polina.c@live.com>
2023-01-21 08:52:48 -0500

    Create object_node.dart

---
+++ packages/devtools_app/lib/src/shared/diagnostics/primitives/object_node.dart
@@ -0,0 +1,24 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../primitives/trees.dart';
+import '../diagnostics_node.dart';
+
+abstract class ObjectNode extends TreeNode<ObjectNode> {
+  int get childCount;
+
+  @override
+  bool get isExpandable;
+
+  String? get displayValue;
+
+  GenericInstanceRef? ref;
+
+  /// Selects the object in the Flutter Widget inspector.
+  ///
+  /// Returns whether the inspector selection was changed.
+  Future<bool> inspectWidget();
+
+  Future<bool> get isInspectable;
+}
