22a31e5ebd8ca0cb50105a0bdc2ce824d23fa98a
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-01-23 13:34:13 -0800

    Add TODO about displaying field count when record hasn't been expanded

--- packages/devtools_app/lib/src/shared/diagnostics/dart_object_node.dart
+++ packages/devtools_app/lib/src/shared/diagnostics/dart_object_node.dart
@@ -548,7 +548,12 @@ class DartObjectNode extends TreeNode<DartObjectNode> {
         valueStr = 'StackTrace ($depth ${pluralize('frame', depth)})';
       } else if (kind == InstanceKind.kRecord) {
         final count = children.length;
-        valueStr = 'Record ($count ${pluralize('field', count)})';
+        // TODO(elliette): Use `value.length` instead of `children.length` when
+        // https://github.com/dart-lang/sdk/issues/51104 is resolved. Currently
+        // the field count can only be computed once the record is expanded:
+        valueStr = count == 0
+            ? 'Record'
+            : 'Record ($count ${pluralize('field', count)})';
       } else if (value.valueAsString == null) {
         valueStr = value.classRef?.name ?? '';
       } else {
