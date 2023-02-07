2c360a861d066d80955dd99d008009b425edf256
Polina Cherkasova <polinach@google.com>
2023-02-07 11:58:33 -0800

    Handle inbound live refs and errors. (#5199)

--- packages/devtools_app/lib/src/shared/diagnostics/references.dart
+++ packages/devtools_app/lib/src/shared/diagnostics/references.dart
@@ -3,10 +3,13 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 
 import 'package:vm_service/vm_service.dart';
 
 import '../feature_flags.dart';
+import '../globals.dart';
 import '../memory/class_name.dart';
 import '../primitives/utils.dart';
 import 'dart_object_node.dart';
@@ -113,13 +116,24 @@ Future<void> addChildReferences(
 
       break;
     case RefNodeType.liveInRefs:
-      variable.addChild(
-        DartObjectNode.references(
-          // Temporary placeholder
-          '<live inbound refs>',
-          ObjectReferences.withType(ref, RefNodeType.liveInRefs),
-        ),
-      );
+      const limit = 100;
+      final refs = (await serviceManager.service!.getInboundReferences(
+            ref.isolateRef!.id!,
+            ref.instanceRef!.id!,
+            limit + 1,
+          ))
+              .references ??
+          [];
+
+      final refsToShow = min(limit, refs.length);
+
+      for (var i = 0; i < refsToShow; i++) {
+        final item = refs[i];
+        variable.addChild(DartObjectNode.text(jsonEncode(item.toJson())));
+      }
+
+      if (refs.length > limit) variable.addChild(DartObjectNode.text('...'));
+
       break;
     case RefNodeType.liveOutRefs:
       final isolateRef = variable.ref!.isolateRef;
