c19ef8327e6fe89d53138df1ef0282b6a3b8f7fe
Polina Cherkasova <polina.c@live.com>
2023-02-14 14:25:34 -0800

    Update simple_items.dart

--- packages/devtools_app/lib/src/shared/console/primitives/simple_items.dart
+++ packages/devtools_app/lib/src/shared/console/primitives/simple_items.dart
@@ -8,15 +8,34 @@ enum FlutterTreeType {
 // TODO(jacobr): add semantics, and layer trees.
 }
 
+/// Assignment of an item in console to a named variable.
 class ConsoleVariableAssignment {
   ConsoleVariableAssignment._(this.consoleItemIndex, this.variableName);
 
+  /// Ziro based bottom up index for items in console.
   final int consoleItemIndex;
+
   final String variableName;
 
+  /// Parses expressions like `var x=$2`.
+  ///
+  /// Treats $_ as $0.
   static ConsoleVariableAssignment? tryParse(String expression) {
-    if (expression != 'var x=\$0') return null; //var x=$0
+    const variableNameGroup = '([_a-zA-Z][_a-zA-Z0-9]{0,30})';
+    const indexGroup = '([_012345])';
 
-    return ConsoleVariableAssignment._(0, 'x');
+    final regex =
+        RegExp(r'var\s+' + variableNameGroup + r'\s*=\s*\$' + indexGroup);
+
+    final matches = regex.allMatches(expression);
+    if (matches.length != 1) return null;
+    final match = matches.first;
+    assert(match.groupCount == 2);
+    final varName = match.group(1)!;
+    var indexChar = match.group(2)!;
+    if (indexChar == '_') indexChar = '0';
+    final index = int.parse(indexChar);
+
+    return ConsoleVariableAssignment._(index, varName);
   }
 }
