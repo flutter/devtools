e737d24c41a3de271dbf6408c354737ebbeb794d
Polina Cherkasova <polina.c@live.com>
2023-02-15 14:31:43 -0800

    Create scope.dart

---
+++ packages/devtools_app/lib/src/shared/console/primitives/scope.dart
@@ -0,0 +1,39 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:vm_service/vm_service.dart';
+
+class EvalScope {
+  /// Parameter `scope` for `serviceManager.service!.evaluate(...)`.
+  ///
+  /// Maps variable name to targetId.
+  Map<String, String> forEval() {
+    _refreshRefsIfOutdated();
+    return _refs.map((key, value) => MapEntry(key, value.id!));
+  }
+
+  DateTime _lastRefresh = DateTime.now();
+
+  final _refs = <String, InstanceRef>{};
+
+  void add(String name, InstanceRef ref) {
+    if (_refs.isEmpty) _lastRefresh = DateTime.now();
+    _refs[name] = ref;
+  }
+
+  /// If variables live too long, refresh them to avoid hitting expired references.
+  void _refreshRefsIfOutdated() {
+    if (_refs.isEmpty) return;
+    const refreshThreshold = Duration(milliseconds: 500);
+    if (_lastRefresh.add(refreshThreshold).isBefore(DateTime.now())) return;
+
+    for (final name in _refs.keys){
+
+    }
+
+    _lastRefresh = DateTime.now();
+  }
+
+  static InstanceRef _refreshRef
+}
