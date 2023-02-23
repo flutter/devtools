5ddfd5d8a785b81426cfe4dca42143df19b466fb
Ben Konyi <bkonyi@google.com>
2023-02-23 11:57:19 -0500

    Add support for viewing CPU profiler ticks per instruction (#5258)
    This change adds a `Profiler Ticks` column to the object inspector's
    code viewer, allowing for compiler developers to determine how "hot"
    specific instructions are in generated code.
    
    RELEASE_NOTE_EXCEPTION=VM developer mode functionality

--- packages/devtools_app/lib/src/screens/debugger/program_explorer_controller.dart
+++ packages/devtools_app/lib/src/screens/debugger/program_explorer_controller.dart
@@ -66,7 +66,7 @@ class ProgramExplorerController extends DisposableController
   }
 
   /// Initializes the program structure.
-  void initialize() {
+  Future<void> initialize() async {
     if (_initializing) {
       return;
     }
@@ -79,6 +79,10 @@ class ProgramExplorerController extends DisposableController
             .isolateNow!
             .libraries!
         : <LibraryRef>[];
+
+    if (scriptManager.sortedScripts.value.isEmpty && isolate != null) {
+      await scriptManager.retrieveAndSortScripts(isolate);
+    }
 
     // Build the initial tree.
     final nodes = VMServiceObjectNode.createRootsFrom(
@@ -157,7 +161,7 @@ class ProgramExplorerController extends DisposableController
   }
 
   /// Clears controller state and re-initializes.
-  void refresh() {
+  Future<void> refresh() {
     _scriptSelection = null;
     _outlineSelection = null;
     _isLoadingOutline.value = true;
