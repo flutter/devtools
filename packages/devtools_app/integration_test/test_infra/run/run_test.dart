90908eb164723d41c6ac054b73a93ebe731a4bcb
Polina Cherkasova <polina.c@live.com>
2023-03-09 12:32:57 -0800

    Update run_test.dart

--- packages/devtools_app/integration_test/test_infra/run/run_test.dart
+++ packages/devtools_app/integration_test/test_infra/run/run_test.dart
@@ -13,6 +13,10 @@ import '_test_app_driver.dart';
 
 bool _debugTestScript = false;
 
+/// Runs one test.
+///
+/// Do not use this method directly, but instead use the run_tests.dart
+/// which performs essential set up steps.
 Future<void> runFlutterIntegrationTest(
   TestArgs testRunnerArgs, {
   String testAppPath = 'test/test_infra/fixtures/flutter_app',
