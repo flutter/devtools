cb7cb2adbcce3243e3d2590cb39a4037b3a6f9b1
Polina Cherkasova <polina.c@live.com>
2023-03-14 10:50:18 -0700

    Update eval_and_browse_test.dart

--- packages/devtools_app/integration_test/test/live_connection/eval_and_browse_test.dart
+++ packages/devtools_app/integration_test/test/live_connection/eval_and_browse_test.dart
@@ -8,7 +8,6 @@
 
 import 'dart:ui' as ui;
 
-import 'package:devtools_app/devtools_app.dart';
 import 'package:devtools_app/src/shared/primitives/simple_items.dart';
 import 'package:devtools_test/devtools_integration_test.dart';
 import 'package:flutter/material.dart';
@@ -33,33 +32,17 @@ void main() {
     await (ui.window as dynamic).resetHistory();
   });
 
-  testWidgets('connect to app and switch tabs', (tester) async {
+  testWidgets('memory eval and browse', (tester) async {
     await pumpAndConnectDevTools(tester, testApp);
 
-    logStatus('verify that we can load each DevTools screen');
-    final availableScreenIds = <String>[];
-    for (final screen in devtoolsScreens!) {
-      if (shouldShowScreen(screen.screen)) {
-        availableScreenIds.add(screen.screen.screenId);
-      }
-    }
-    final tabs = tester.widgetList<Tab>(
-      find.descendant(
-        of: find.byType(AppBar),
-        matching: find.byType(Tab),
-      ),
-    );
-    expect(tabs.length, equals(availableScreenIds.length));
+    final screenTitle = ScreenMetaData.memory.title;
 
-    final screenTitles = (ScreenMetaData.values.toList()
-          ..removeWhere((data) => !availableScreenIds.contains(data.id)))
-        .map((data) => data.title);
-    for (final title in screenTitles) {
-      logStatus('switching to $title screen');
-      await tester.tap(find.widgetWithText(Tab, title));
-      // We use pump here instead of pumpAndSettle because pumpAndSettle will
-      // never complete if there is an animation (e.g. a progress indicator).
-      await tester.pump(safePumpDuration);
-    }
+    logStatus('switching to $screenTitle screen');
+    await tester.tap(find.widgetWithText(Tab, screenTitle));
+    // We use pump here instead of pumpAndSettle because pumpAndSettle will
+    // never complete if there is an animation (e.g. a progress indicator).
+    await tester.pump(safePumpDuration);
+
+    // TODO(polina-c): add test logic
   });
 }
