d09d6a9b88f5a63adc0071033d9d94458b6a00a1
Dan Chevalier <chevalier.dan@gmail.com>
2023-03-16 13:08:49 -0400

    more lint

--- packages/devtools_app/lib/src/shared/config_specific/logger/logger_default.dart
+++ packages/devtools_app/lib/src/shared/config_specific/logger/logger_default.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:logging/logging.dart';
-
 import 'logger.dart';
 
 void log(Object message, [LogLevel level = LogLevel.debug]) {
