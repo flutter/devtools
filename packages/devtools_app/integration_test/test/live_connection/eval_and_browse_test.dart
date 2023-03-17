636b6a9fba0f2ae5740280da812d0bd25bccc102
Polina Cherkasova <polina.c@live.com>
2023-03-17 08:19:02 -0700

    Update eval_and_browse_test.dart

--- packages/devtools_app/integration_test/test/live_connection/eval_and_browse_test.dart
+++ packages/devtools_app/integration_test/test/live_connection/eval_and_browse_test.dart
@@ -11,6 +11,7 @@ import 'dart:ui' as ui;
 import 'package:devtools_app/src/screens/memory/panes/diff/widgets/snapshot_list.dart';
 import 'package:devtools_app/src/shared/console/widgets/console_pane.dart';
 import 'package:devtools_app/src/shared/primitives/simple_items.dart';
+import 'package:devtools_app/src/shared/table/table.dart';
 import 'package:devtools_app/src/shared/ui/search.dart';
 import 'package:devtools_test/devtools_integration_test.dart';
 import 'package:flutter/material.dart';
@@ -127,8 +128,13 @@ class _EvalTester {
     await tester.pumpAndSettle();
     await tester.tap(find.byIcon(iconToTakeSnapshot));
     await tester.pump(longPumpDuration);
+
+    // Sort by class
+    await tester.tap(find.text('Class'));
+    await tester.pumpAndSettle();
+
     expect(
-      find.text('MyHomePage'),
+      find.text('MyApp'),
       findsOneWidget,
     );
   }
