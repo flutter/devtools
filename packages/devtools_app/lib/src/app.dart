2e339e8997b830ba93b38c3d745e4eb4540c6531
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-03-24 17:15:14 -0700

    Dart format

--- packages/devtools_app/lib/src/app.dart
+++ packages/devtools_app/lib/src/app.dart
@@ -518,7 +518,8 @@ class SettingsDialog extends StatelessWidget {
                 title: 'Enable analytics',
                 notifier: analyticsController.analyticsEnabled,
                 onChanged: (enable) => unawaited(
-                    analyticsController.toggleAnalyticsEnabled(enable),),
+                  analyticsController.toggleAnalyticsEnabled(enable),
+                ),
                 gaItem: gac.analytics,
               ),
             ),
