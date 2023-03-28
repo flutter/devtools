aa9527fa909a6cc9488c31b7339df5c226fe1ff5
Polina Cherkasova <polina.c@live.com>
2023-03-28 11:05:38 -0700

    Update extensions_external.dart

--- packages/devtools_app/lib/src/extension_points/extensions_external.dart
+++ packages/devtools_app/lib/src/extension_points/extensions_external.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../../devtools.dart' as devtools;
 import '../screens/debugger/codeview.dart';
 import '../shared/analytics/constants.dart' as gac;
 import '../shared/common_widgets.dart';
