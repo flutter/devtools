3614d4756a7cf19070d621c358c8b4a0e27152d5
Dan Chevalier <chevalier.dan@gmail.com>
2023-03-31 15:48:10 -0400

    more steps

--- tool/release_helper.sh
+++ tool/release_helper.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -ex
-# echo '{ "draft": "true", "updateType": "release" }' | gh workflow run .github/workflows/daily-dev-bump.yaml --json --ref=more-bump
+#!/bin/bash -e
 
 DEVTOOLS_REMOTE=$(git remote -v | grep "flutter/devtools.git" | grep "(fetch)"| tail -n1 | cut -w -f1)
 
@@ -15,6 +14,8 @@ if [[ ! -z  "$STATUS" ]] ; then
     exit 1
 fi
 
+echo "Getting a fresh copy of master"
+echo
 MASTER="tmp_master_$(date +%s)"
 git fetch $DEVTOOLS_REMOTE master
 git checkout -b $MASTER $DEVTOOLS_REMOTE/master
@@ -23,13 +24,20 @@ git checkout -b $MASTER $DEVTOOLS_REMOTE/master
 RELEASE_BRANCH="clean_release_$(date +%s)"
 git checkout -b "$RELEASE_BRANCH"
 
+echo "Ensuring ./tool packages are ready"
+echo
 dart pub get
 
 cd ..
 
 ORIGINAL_VERSION=$(dart tool/update_version.dart current-version)
 
+echo "Setting the release version"
+echo
 dart tool/update_version.dart auto --type release
+
+echo "Updating the changelog"
+echo
 dart tool/bin/repo_tool.dart generate-changelog
 
 NEW_VERSION=$(dart tool/update_version.dart current-version)
@@ -40,14 +48,14 @@ COMMIT_MESSAGE="Releasing from $ORIGINAL_VERSION to $NEW_VERSION"
 git commit -a -m "$COMMIT_MESSAGE"
 
 git push -u $DEVTOOLS_REMOTE $RELEASE_BRANCH
-
+echo "Creating the PR"
+echo
 PR_URL=$(gh pr create --title "$COMMIT_MESSAGE" --body "RELEASE_NOTE_EXCEPTION=Automated Version Bump" $CREATION_FLAGS)
 
-echo "Release PR created successfully."
-echo "$PR_URL"
-
+echo "Release PR created successfully: $PR_URL"
+echo
 echo "Updating your flutter version to the most recent candidate."
-
+echo
 ./tool/update_flutter_sdk.sh --local
-
-echo "You can now start testing devtools to make sure it is ready for release."
+echo
+echo "$0 DONE: You can now start testing devtools to make sure it is ready for release."
