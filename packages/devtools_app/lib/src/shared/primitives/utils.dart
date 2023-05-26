b1ff5c50a41b795e3c43675f3314ed9a3db47bdd
Dan Chevalier <chevalier.dan@gmail.com>
2023-05-26 15:46:19 -0400

    silly moving too fast

--- packages/devtools_app/lib/src/shared/primitives/utils.dart
+++ packages/devtools_app/lib/src/shared/primitives/utils.dart
@@ -1183,7 +1183,7 @@ extension StringExtension on String {
 extension IterableExtension<T> on Iterable<T> {
   /// Joins the iterable with [separator], and also adds a trailing [separator].
   String joinWithTrailing([String separator = '']) {
-    return '${join(separator)}\n';
+    return '${join(separator)}$separator';
   }
 }
 
