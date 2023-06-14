385747b1f23830bd275ac1335ffdf38d16fbc2cf
Kenzie Schmoll <kenzieschmoll@google.com>
2023-06-14 15:45:38 -0700

    comment out existing test

--- packages/devtools_app/integration_test/test/live_connection/service_connection_test.dart
+++ packages/devtools_app/integration_test/test/live_connection/service_connection_test.dart
@@ -29,66 +29,66 @@ void main() {
     await resetHistory();
   });
 
-  testWidgets('initial service connection state', (tester) async {
-    await pumpAndConnectDevTools(tester, testApp);
+  // testWidgets('initial service connection state', (tester) async {
+  //   await pumpAndConnectDevTools(tester, testApp);
 
-    // Await a delay to ensure the service extensions have had a chance to
-    // be called. This delay may be able to be shortened if doing so does
-    // not cause bot flakiness.
-    await tester.pump(longDuration);
+  //   // Await a delay to ensure the service extensions have had a chance to
+  //   // be called. This delay may be able to be shortened if doing so does
+  //   // not cause bot flakiness.
+  //   await tester.pump(longDuration);
 
-    // Ensure all futures are completed before running checks.
-    await serviceManager.service!.allFuturesCompleted;
+  //   // Ensure all futures are completed before running checks.
+  //   await serviceManager.service!.allFuturesCompleted;
 
-    logStatus('verify the number of vm service calls on connect');
-    final vmServiceCallCount = serviceManager.service!.vmServiceCallCount;
-    expect(
-      // Use a range instead of an exact number because service extension
-      // calls are not consistent. This will still catch any spurious calls
-      // that are unintentionally added at start up.
-      const Range(50, 60).contains(vmServiceCallCount),
-      isTrue,
-      reason: 'Unexpected number of vm service calls upon connection: '
-          '$vmServiceCallCount. If this is expected, please update this test '
-          'to the new expected number of calls. Here are the calls for this '
-          'test run:\n ${serviceManager.service!.vmServiceCalls.toString()}',
-    );
-    // Check the ordering of the vm service calls we can expect to occur
-    // in a stable order.
-    expect(
-      serviceManager.service!.vmServiceCalls
-          // Filter out unawaited streamListen calls.
-          .where((call) => call != 'streamListen')
-          .toList()
-          .sublist(0, 5),
-      equals([
-        'getSupportedProtocols',
-        'getVersion',
-        'getFlagList',
-        'getVM',
-        'getIsolate',
-      ]),
-    );
+  //   logStatus('verify the number of vm service calls on connect');
+  //   final vmServiceCallCount = serviceManager.service!.vmServiceCallCount;
+  //   expect(
+  //     // Use a range instead of an exact number because service extension
+  //     // calls are not consistent. This will still catch any spurious calls
+  //     // that are unintentionally added at start up.
+  //     const Range(50, 60).contains(vmServiceCallCount),
+  //     isTrue,
+  //     reason: 'Unexpected number of vm service calls upon connection: '
+  //         '$vmServiceCallCount. If this is expected, please update this test '
+  //         'to the new expected number of calls. Here are the calls for this '
+  //         'test run:\n ${serviceManager.service!.vmServiceCalls.toString()}',
+  //   );
+  //   // Check the ordering of the vm service calls we can expect to occur
+  //   // in a stable order.
+  //   expect(
+  //     serviceManager.service!.vmServiceCalls
+  //         // Filter out unawaited streamListen calls.
+  //         .where((call) => call != 'streamListen')
+  //         .toList()
+  //         .sublist(0, 5),
+  //     equals([
+  //       'getSupportedProtocols',
+  //       'getVersion',
+  //       'getFlagList',
+  //       'getVM',
+  //       'getIsolate',
+  //     ]),
+  //   );
 
-    expect(
-      serviceManager.service!.vmServiceCalls
-          .where((call) => call == 'streamListen')
-          .toList()
-          .length,
-      equals(10),
-    );
+  //   expect(
+  //     serviceManager.service!.vmServiceCalls
+  //         .where((call) => call == 'streamListen')
+  //         .toList()
+  //         .length,
+  //     equals(10),
+  //   );
 
-    logStatus('verify managers have all been initialized');
-    expect(serviceManager.isolateManager, isNotNull);
-    expect(serviceManager.serviceExtensionManager, isNotNull);
-    expect(serviceManager.vmFlagManager, isNotNull);
-    expect(serviceManager.isolateManager.isolates.value, isNotEmpty);
-    expect(serviceManager.vmFlagManager.flags.value, isNotNull);
+  //   logStatus('verify managers have all been initialized');
+  //   expect(serviceManager.isolateManager, isNotNull);
+  //   expect(serviceManager.serviceExtensionManager, isNotNull);
+  //   expect(serviceManager.vmFlagManager, isNotNull);
+  //   expect(serviceManager.isolateManager.isolates.value, isNotEmpty);
+  //   expect(serviceManager.vmFlagManager.flags.value, isNotNull);
 
-    if (serviceManager.isolateManager.selectedIsolate.value == null) {
-      await whenValueNonNull(serviceManager.isolateManager.selectedIsolate);
-    }
-  });
+  //   if (serviceManager.isolateManager.selectedIsolate.value == null) {
+  //     await whenValueNonNull(serviceManager.isolateManager.selectedIsolate);
+  //   }
+  // });
 
   testWidgets('can call service extensions', (tester) async {
     await pumpAndConnectDevTools(tester, testApp);
