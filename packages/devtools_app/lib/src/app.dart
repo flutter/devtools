08002ab8316a820f3dae565be2b8196324445d30
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-07-11 15:26:45 -0700

    Remove unused import

--- packages/devtools_app/lib/src/app.dart
+++ packages/devtools_app/lib/src/app.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'example/conditional_screen.dart';
