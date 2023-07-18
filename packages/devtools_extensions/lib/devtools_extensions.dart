f97137945d20fa2f160a9219103a8790a538babb
Kenzie Schmoll <kenzieschmoll@google.com>
2023-07-18 16:01:24 -0700

    fix

--- packages/devtools_extensions/lib/devtools_extensions.dart
+++ packages/devtools_extensions/lib/devtools_extensions.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-library devtools_plugins;
+library devtools_extensions;
 
 export 'src/api.dart';
 export 'src/model.dart';
