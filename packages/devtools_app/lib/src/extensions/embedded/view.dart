679c04a64a0795703fc682116222f67a1d0e4cd1
Kenzie Schmoll <kenzieschmoll@google.com>
2023-07-18 16:45:14 -0700

    dart doc

--- packages/devtools_app/lib/src/extensions/embedded/view.dart
+++ packages/devtools_app/lib/src/extensions/embedded/view.dart
@@ -7,6 +7,13 @@ import 'package:flutter/material.dart';
 import '_view_desktop.dart' if (dart.library.html) '_view_web.dart';
 import 'controller.dart';
 
+/// A widget that displays a DevTools extension in an embedded iFrame.
+/// 
+/// A DevTools extension is provided by a pub package and is served by the
+/// DevTools server when present for a connected application.
+/// 
+/// When DevTools is run on Desktop for development, this widget displays a
+/// placeholder, since Flutter Desktop does not currently support web views.
 class EmbeddedExtensionView extends StatelessWidget {
   const EmbeddedExtensionView({Key? key, required this.controller})
       : super(key: key);
