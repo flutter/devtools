43f16096f25c0eff73981d2cf8984ef3e3c679f9
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2023-07-21 11:40:04 -0700

    Respond to PR comments

--- packages/devtools_app/test/debugger/debugger_screen_dap_variables_test.dart
+++ packages/devtools_app/test/debugger/debugger_screen_dap_variables_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,7 +17,7 @@ void main() {
   late MockScriptManager scriptManager;
   late MockVmServiceWrapper vmService;
 
-  const windowSize = Size(4000, 4000);
+  const windowSize = Size(2000, 1000);
 
   setUp(() {
     FeatureFlags.dapDebugging = true;
