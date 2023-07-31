27ce8cea40803ea2f4489c26024a1d37d97a5758
Kenzie Schmoll <kenzieschmoll@google.com>
2023-07-31 14:31:00 -0700

    prep for devtools_extensions release

--- tool/update_version.dart
+++ tool/update_version.dart
@@ -11,6 +11,8 @@ import 'package:args/command_runner.dart';
 // TODO(kenz): If changes are made to this script, first consider refactoring to
 // use https://github.com/dart-lang/pubspec_parse.
 
+// package/devtools_extensions has its own versioning strategy. Do not include
+// it in the list here.
 final _pubspecs = [
   'packages/devtools_app/pubspec.yaml',
   'packages/devtools_test/pubspec.yaml',
