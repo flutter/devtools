4ec54ab65dfe78e95828b7350c100beaf9c4aa20
Dan Chevalier <chevalier.dan@gmail.com>
2023-08-16 10:52:42 -0400

    Add select for multiples

--- tool/fix_goldens.sh
+++ tool/fix_goldens.sh
@@ -32,18 +32,24 @@ DOWNLOAD_DIR=$(mktemp -d)
 gh run download $RUN_ID -p "*golden_image_failures*" -D "$DOWNLOAD_DIR"
 NEW_GOLDENS=$(find $DOWNLOAD_DIR -type f | grep "testImage.png" )
 pushd packages/devtools_app/test/
-ERROR_COUNTER=0
 while IFS= read -r GOLDEN ; do
   FILE_NAME=$(basename $GOLDEN | sed "s|_testImage.png$|.png|")
-  DEST_PATH=$(find . -name "$FILE_NAME" )
-  DEST_PATH_COUNT=$(find . -name "$FILE_NAME" | wc -l)
+  FOUND_FILES=$(find . -name "$FILE_NAME" )
+  FOUND_FILE_COUNT=$(echo -n "$FOUND_FILES" | grep -c '^')
 
-  if [[ $DEST_PATH_COUNT -ne 1 ]] ; then
-    echo "ERROR: Expected exactly one golden named $FILE_NAME. Got: $DEST_PATH"
-    ERROR_COUNTER=$(( ERROR_COUNTER + 1 ))
+  if [[ $FOUND_FILE_COUNT -ne 1 ]] ; then
+    echo $FOUND_FILES
+    echo "Multiple goldens found, which should replace $(echo $GOLDEN| sed 's|^.*golden_image_failures[^/]*/||')"
+    
+    select SELECTED_FILE in $FOUND_FILES
+    do
+      DEST_PATH="$SELECTED_FILE"
+      break;
+    done </dev/tty
   else
-    mv "$GOLDEN" "$DEST_PATH"
+    DEST_PATH=$FOUND_FILES
   fi
+  mv "$GOLDEN" "$DEST_PATH"
 done <<< "$NEW_GOLDENS"
 
 echo "Done updating $(echo -n "$NEW_GOLDENS" | grep -c '^') lines"
