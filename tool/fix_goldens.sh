de99e44a0fa6b643fc22712fcaafeb005a92a805
Dan Chevalier <chevalier.dan@gmail.com>
2023-08-16 11:45:48 -0400

    cleanup

--- tool/fix_goldens.sh
+++ tool/fix_goldens.sh
@@ -29,17 +29,19 @@ if [ -z "$1" ]
 fi
 DOWNLOAD_DIR=$(mktemp -d)
 
+echo "Downloading the artifacts"
 gh run download $RUN_ID -p "*golden_image_failures*" -D "$DOWNLOAD_DIR"
+
 NEW_GOLDENS=$(find $DOWNLOAD_DIR -type f | grep "testImage.png" )
-pushd packages/devtools_app/test/
+cd packages/devtools_app/test/
 while IFS= read -r GOLDEN ; do
   FILE_NAME=$(basename $GOLDEN | sed "s|_testImage.png$|.png|")
   FOUND_FILES=$(find . -name "$FILE_NAME" )
   FOUND_FILE_COUNT=$(echo -n "$FOUND_FILES" | grep -c '^')
 
   if [[ $FOUND_FILE_COUNT -ne 1 ]] ; then
-    echo $FOUND_FILES
-    echo "Multiple goldens found, which should replace $(echo $GOLDEN| sed 's|^.*golden_image_failures[^/]*/||')"
+    echo "Multiple goldens found for $(echo $GOLDEN| sed 's|^.*golden_image_failures[^/]*/||')"
+    echo "Select which should be overridden:"
     
     select SELECTED_FILE in $FOUND_FILES
     do
@@ -49,11 +51,10 @@ while IFS= read -r GOLDEN ; do
   else
     DEST_PATH=$FOUND_FILES
   fi
+
+  echo "FIXED: $DEST_PATH"
   mv "$GOLDEN" "$DEST_PATH"
 done <<< "$NEW_GOLDENS"
 
-echo "Done updating $(echo -n "$NEW_GOLDENS" | grep -c '^') lines"
-if [[ $ERROR_COUNTER -gt 0 ]] ; then
-  echo "ERROR: $ERROR_COUNTER failed golden updates"
-fi
+echo "Done updating $(echo -n "$NEW_GOLDENS" | grep -c '^') goldens"
 
