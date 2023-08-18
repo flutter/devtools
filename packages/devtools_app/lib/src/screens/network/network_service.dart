056da448ae01dd0321b2c325cfc4d2e796a57649
Kenzie Schmoll <kenzieschmoll@google.com>
2023-08-17 17:41:31 -0700

    remove unused

--- packages/devtools_app/lib/src/screens/network/network_service.dart
+++ packages/devtools_app/lib/src/screens/network/network_service.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:devtools_app_shared/service.dart';
 import 'package:vm_service/vm_service.dart';
 
 import '../../shared/globals.dart';
