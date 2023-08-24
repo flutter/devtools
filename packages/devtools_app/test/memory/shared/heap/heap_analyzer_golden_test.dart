d40cc07dca0ddbe433e9269160a459c1a2223fff
Polina Cherkasova <polina.c@live.com>
2023-08-23 17:26:38 -0700

    Update heap_analyzer_golden_test.dart

--- packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
+++ packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
@@ -5,6 +5,7 @@
 import 'dart:io';
 
 import 'package:devtools_app/devtools_app.dart';
+import 'package:devtools_app/src/screens/memory/shared/heap/heap.dart';
 import 'package:devtools_app/src/screens/memory/shared/heap/spanning_tree.dart';
 import 'package:devtools_app/src/shared/memory/adapted_heap_data.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,52 +13,52 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../test_infra/test_data/memory/heap/heap_data.dart';
 
 void main() {
-  final snapshots = <String, AdaptedHeapData>{};
+  test('memory footprint', () async {
+    final snapshots = <String, AdaptedHeap>{};
 
-  setUpAll(() async {
     final before = ProcessInfo.currentRss;
     for (var t in goldenHeapTests) {
-      snapshots[t.fileName] = await t.loadHeap();
+      snapshots[t.fileName] = await AdaptedHeap.create(await t.loadHeap());
     }
 
     final after = ProcessInfo.currentRss;
     final delta = after - before;
 
-    print(prettyPrintBytes(delta, includeUnit: true)); // 905.4 - 912.0 MB
+    print(prettyPrintBytes(delta, includeUnit: true, gbFractionDigits: 3));
+
+    // baseline: 1.209 GB - 1.281 GB
   });
 
-  test('empty', () => null);
+  for (var t in goldenHeapTests) {
+    group(t.fileName, () {
+      late AdaptedHeapData heap;
 
-  // for (var t in goldenHeapTests) {
-  //   group(t.fileName, () {
-  //     late AdaptedHeapData heap;
+      setUp(() async {
+        heap = await t.loadHeap();
+      });
 
-  //     setUp(() {
-  //       heap = snapshots[t.fileName]!;
-  //     });
+      test('has many objects and roots.', () {
+        expect(heap.objects.length, greaterThan(1000));
+        expect(
+          heap.objects[heap.rootIndex].outRefs.length,
+          greaterThan(1000),
+          reason: t.fileName,
+        );
+      });
 
-  //     test('has many objects and roots.', () {
-  //       expect(heap.objects.length, greaterThan(1000));
-  //       expect(
-  //         heap.objects[heap.rootIndex].outRefs.length,
-  //         greaterThan(1000),
-  //         reason: t.fileName,
-  //       );
-  //     });
+      test('has exactly one object of type ${t.appClassName}.', () {
+        final appObjects =
+            heap.objects.where((o) => o.heapClass.className == t.appClassName);
+        expect(appObjects, hasLength(1), reason: t.fileName);
+      });
 
-  //     test('has exactly one object of type ${t.appClassName}.', () {
-  //       final appObjects =
-  //           heap.objects.where((o) => o.heapClass.className == t.appClassName);
-  //       expect(appObjects, hasLength(1), reason: t.fileName);
-  //     });
-
-  //     test('has path to the object of type ${t.appClassName}.', () async {
-  //       await calculateHeap(heap);
-  //       final appObject = heap.objects
-  //           .where((o) => o.heapClass.className == t.appClassName)
-  //           .first;
-  //       expect(appObject.retainer, isNotNull, reason: t.fileName);
-  //     });
-  //   });
-  // }
+      test('has path to the object of type ${t.appClassName}.', () async {
+        await calculateHeap(heap);
+        final appObject = heap.objects
+            .where((o) => o.heapClass.className == t.appClassName)
+            .first;
+        expect(appObject.retainer, isNotNull, reason: t.fileName);
+      });
+    });
+  }
 }
