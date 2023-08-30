2fb722de31742246be0ed25bc5e5a92b2b66d04e
Dan Chevalier <chevalier.dan@gmail.com>
2023-08-30 12:26:32 -0400

    add dummy commit

--- tool/release_helper.sh
+++ tool/release_helper.sh
@@ -41,6 +41,7 @@ dart tool/update_version.dart auto --type release
 NEW_VERSION=$(dart tool/update_version.dart current-version)
 
 COMMIT_MESSAGE="Releasing from $ORIGINAL_VERSION to $NEW_VERSION"
+echo "test" >> pubspec.yaml
 
 # Stage the file, commit and push
 git commit -a -m "$COMMIT_MESSAGE"
