de4134b5305d2828b75fa4e62ec2f10170025eee
Polina Cherkasova <polina.c@live.com>
2023-08-31 14:03:31 -0700

    -

--- packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
+++ packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
-
-import 'package:devtools_app/src/screens/memory/shared/heap/heap.dart';
 import 'package:devtools_app/src/screens/memory/shared/heap/spanning_tree.dart';
 import 'package:devtools_app/src/shared/memory/adapted_heap_data.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,28 +9,6 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../test_infra/test_data/memory/heap/heap_data.dart';
 
 void main() {
-  test('memory footprint', () async {
-    final snapshots = <String, AdaptedHeap>{};
-
-    final before = ProcessInfo.currentRss;
-    for (var t in goldenHeapTests) {
-      snapshots[t.fileName] = await AdaptedHeap.create(await t.loadHeap());
-    }
-
-    final after = ProcessInfo.currentRss;
-    final delta = after - before;
-
-    double gbToBytes(double gb) => gb * (1024 * 1024 * 1024);
-
-    final lowerThreshold = gbToBytes(0.96);
-    final upperThreshold = gbToBytes(1.08);
-
-    // Both thresholds are tested, because we want to lower the values
-    // in case of optimisation.
-    expect(delta, greaterThan(lowerThreshold));
-    expect(delta, lessThan(upperThreshold));
-  });
-
   for (var t in goldenHeapTests) {
     group(t.fileName, () {
       late AdaptedHeapData heap;
