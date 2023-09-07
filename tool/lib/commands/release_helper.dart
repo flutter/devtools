2aa14dbb5faba3c4ac604704ea1d2af5c8fe813b
Dan Chevalier <chevalier.dan@gmail.com>
2023-09-07 12:04:31 -0400

    next best attemtp

--- tool/lib/commands/release_helper.dart
+++ tool/lib/commands/release_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:path/path.dart' as path;
 
 import 'package:args/command_runner.dart';
 import 'package:devtools_tool/utils.dart';
@@ -40,7 +41,7 @@ class ReleaseHelperCommand extends Command {
     } on StateError {
       throw "ERROR: Couldn't find a remote that points to flutter/devtools git. Instead got: \n$devtoolsRemotes";
     }
-    final remoteOrigin = devtoolsRemoteResult.namedGroup('remote');
+    final remoteOrigin = devtoolsRemoteResult.namedGroup('remote')!;
 
 // STATUS=$(git status -s)
 // if [[ ! -z  "$STATUS" ]] ; then
@@ -57,43 +58,134 @@ class ReleaseHelperCommand extends Command {
 // MASTER="tmp_master_$(date +%s)"
 // git fetch $DEVTOOLS_REMOTE master
 // git checkout -b $MASTER $DEVTOOLS_REMOTE/master
+    final uniqueBranch =
+        '_release_helper_master_${DateTime.now().millisecondsSinceEpoch}';
+    print(uniqueBranch);
+    Process.run('git', ['fetch', remoteOrigin, 'master']);
+    Process.run('git', [
+      'checkout',
+      '-b',
+      uniqueBranch,
+      '$remoteOrigin/master',
+    ]);
 
 // RELEASE_BRANCH="clean_release_$(date +%s)"
 // git checkout -b "$RELEASE_BRANCH"
+    final releaseBranch =
+        '_release_helper_release_${DateTime.now().millisecondsSinceEpoch}';
+    Process.run('git', ['checkout', '-b', releaseBranch]);
 
 // echo "Ensuring ./tool packages are ready"
 // echo
 // dart pub get
+    Directory.current = pathFromRepoRoot("tool");
+    Process.run('dart', ['pub', 'get']);
 
 // cd ..
 
 // ORIGINAL_VERSION=$(dart tool/update_version.dart current-version)
+    final currentVersionResult = await Process.run('dart', [
+      'tool/update_version.dart',
+      'current-version',
+    ]);
+    if (currentVersionResult.exitCode != 0) {
+      throw "Error: failed to get current version";
+    }
+    final originalVersion = currentVersionResult.stdout;
 
 // echo "Setting the release version"
 // echo
 // dart tool/update_version.dart auto --type release
+    final createReleaseResult = await Process.run('dart', [
+      'tool/update_version.dart',
+      'auto',
+      '--type',
+      'release',
+    ]);
+    if (createReleaseResult.exitCode != 0) {
+      throw "Error: failed to create release";
+    }
 
 // NEW_VERSION=$(dart tool/update_version.dart current-version)
+    final getNewVersionResult = await Process.run('dart', [
+      'tool/update_version.dart',
+      'current-version',
+    ]);
+    if (getNewVersionResult.exitCode != 0) {
+      throw "Error: failed to get version after creating release";
+    }
+    final newVersion = getNewVersionResult.stdout;
 
 // COMMIT_MESSAGE="Releasing from $ORIGINAL_VERSION to $NEW_VERSION"
+    final commitMessage = "Releasing from $originalVersion to $newVersion";
 
 // # Stage the file, commit and push
 // git commit -a -m "$COMMIT_MESSAGE"
+    final commitResult = await Process.run('git', [
+      'commit',
+      '-a',
+      '-m',
+      commitMessage,
+    ]);
+    if (commitResult.exitCode != 0) {
+      throw "Error: failed to commit";
+    }
 
 // git push -u $DEVTOOLS_REMOTE $RELEASE_BRANCH
+    final pushResult = await Process.run('git', [
+      'push',
+      '-u',
+      remoteOrigin,
+      releaseBranch,
+    ]);
+
+    if (pushResult.exitCode != 0) {
+      throw "Error: failed to push results";
+    }
 
 // echo "$0: Creating the PR"
 // echo
-
+    print('Creating the PR');
 // PR_URL=$(gh pr create --repo flutter/devtools --draft --title "$COMMIT_MESSAGE" --fill)
+    final createPRResult = await Process.run('gh', [
+      'pr',
+      'create',
+      '--repo',
+      'flutter/devtools',
+      '--draft',
+      '--title',
+      commitMessage,
+      '--fill',
+    ]);
+    if (createPRResult.exitCode != 0) {
+      throw "Error: failed to create PR";
+    }
+    final prURL = createPRResult.stdout;
 
 // echo "$0: Updating your flutter version to the most recent candidate."
 // echo
 // ./tool/update_flutter_sdk.sh --local
+    final updateFlutterResult = await Process.run(
+        path
+            .join(
+              '.',
+              'tool',
+              'update_flutter_sdk.sh',
+            )
+            .toString(),
+        ['--local']);
+    if (updateFlutterResult.exitCode != 0) {
+      throw "Error: failed to update the flutter sdk";
+    }
 
 // echo "$0: Your Draft release PR can be found at: $PR_URL"
 // echo
 // echo "$0: DONE.
 // echo "$0: Build, run and test this release using: 'dart ./tool/build_e2e.dart'"
+    print('Your Draft release PR can be found at: $prURL');
+    print('DONE');
+    print(
+      'Build, run and test this release using: `dart ./tool/build_e2e.dart`',
+    );
   }
 }
