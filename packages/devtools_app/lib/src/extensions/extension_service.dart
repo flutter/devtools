fd9fb140a4eaf6d99718ada4d8cc7633188049b9
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2023-09-12 09:00:40 -0700

    Refresh available extensions when the main isolate changes. (#6358)

--- packages/devtools_app/lib/src/extensions/extension_service.dart
+++ packages/devtools_app/lib/src/extensions/extension_service.dart
@@ -47,6 +47,16 @@ class ExtensionService extends DisposableController
       _maybeRefreshExtensions,
     );
 
+    // TODO(https://github.com/flutter/flutter/issues/134470): refresh on
+    // hot reload and hot restart events instead.
+    addAutoDisposeListener(
+        serviceConnection.serviceManager.isolateManager.mainIsolate, () async {
+      if (serviceConnection.serviceManager.isolateManager.mainIsolate.value !=
+          null) {
+        await _maybeRefreshExtensions();
+      }
+    });
+
     // TODO(kenz): we should also refresh the available extensions on some event
     // from the analysis server that is watching the
     // .dart_tool/package_config.json file for changes.
