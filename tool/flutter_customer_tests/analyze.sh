4af3459b2620651e778ed6db8ef535e881dc3b38
Kenzie Schmoll <kenzieschmoll@google.com>
2023-10-05 17:19:17 -0600

    comment and rename

--- tool/flutter_customer_tests/analyze.sh
+++ tool/flutter_customer_tests/analyze.sh
@@ -3,6 +3,10 @@
 # https://github.com/flutter/tests
 # This is executed as a pre-submit check for every PR in flutter/flutter
 
+# At this point we can expect that mocks have already been generated
+# from the setup steps in
+# https://github.com/flutter/tests/blob/main/registry/flutter_devtools.test
+
 cd tool
 flutter pub get
 dart bin/devtools_tool.dart pub-get
