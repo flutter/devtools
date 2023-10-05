d5cc6c661fe31259ead6d94cdd1982203a2e3d56
Kenzie Schmoll <kenzieschmoll@google.com>
2023-10-05 16:28:52 -0600

    remove flutter and dart version from analyze.sh

--- tool/flutter_customer_tests/analyze.sh
+++ tool/flutter_customer_tests/analyze.sh
@@ -3,11 +3,6 @@
 # https://github.com/flutter/tests
 # This is executed as a pre-submit check for every PR in flutter/flutter
 
-which dart
-dart --version
-which flutter
-flutter --version
-
 cd tool
 flutter pub get
 dart bin/devtools_tool.dart pub-get
