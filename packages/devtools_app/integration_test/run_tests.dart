5b4ef6907f7dd84482d5fc45060504bf0e5c05c3
Polina Cherkasova <polina.c@live.com>
2023-10-18 11:21:09 -0700

    Update run_tests.dart

--- packages/devtools_app/integration_test/run_tests.dart
+++ packages/devtools_app/integration_test/run_tests.dart
@@ -17,14 +17,6 @@ import 'test_infra/run/run_test.dart';
 const _testDirectory = 'integration_test/test';
 const _offlineIndicator = 'integration_test/test/offline';
 
-/// The set of test that should be skipped for all devices.
-///
-/// This list should be empty most of the time, but may contain a broken test
-/// while a fix being worked on.
-const _skipTests = <String>[
-  'my_example_test.dart',
-];
-
 void main(List<String> args) async {
   final testRunnerArgs = DevToolsAppTestRunnerArgs(
     args,
@@ -46,9 +38,6 @@ Future<void> _runTest(
   DevToolsAppTestRunnerArgs testRunnerArgs,
 ) async {
   final testTarget = testRunnerArgs.testTarget!;
-
-  final shouldSkip = _skipTests.any((t) => testTarget.endsWith(t));
-  if (shouldSkip) return;
 
   if (!testRunnerArgs.testAppDevice.supportsTest(testTarget)) {
     // Skip test, since it is not supported for device.
