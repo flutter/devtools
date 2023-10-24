28017f8d0af23e6c8d2774402b3f0ff6d161a084
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2023-10-24 10:17:39 -0700

    Include main package subdirectories in the `devtools_tool pub-get` command (#6582)

--- tool/lib/commands/pub_get.dart
+++ tool/lib/commands/pub_get.dart
@@ -23,7 +23,7 @@ class PubGetCommand extends Command {
         _onlyMainFlag,
         negatable: false,
         help: 'Only execute on the top-level `devtools/packages/devtools_*` '
-            'packages',
+            'packages and any of their subdirectories',
       );
   }
 
@@ -55,10 +55,10 @@ class PubGetCommand extends Command {
 
     for (Package p in packages) {
       final packagePathParts = path.split(p.relativePath);
-      final isMainPackage = packagePathParts.length == 2 &&
+      final isMainPackageOrSubdirectory = packagePathParts.length >= 2 &&
           packagePathParts.first == 'packages' &&
           packagePathParts[1].startsWith('devtools_');
-      if (onlyMainPackages && !isMainPackage) continue;
+      if (onlyMainPackages && !isMainPackageOrSubdirectory) continue;
 
       final progress = log.progress('  ${p.relativePath}');
 
