944ff3d2b7f584c013d158b9cf77d436ac3cf312
Danny Tuppeny <danny@tuppeny.com>
2023-10-24 14:45:43 +0100

    Fix directory

--- tool/generate_code.sh
+++ tool/generate_code.sh
@@ -10,10 +10,7 @@ RELATIVE_PATH_TO_SCRIPT="${BASH_SOURCE[0]}"
 # The directory that this script is located in.
 TOOL_DIR=`dirname "${RELATIVE_PATH_TO_SCRIPT}"`
 
-# The devtools root directory is assumed to be the parent of this directory.
-DEVTOOLS_DIR="${TOOL_DIR}/.."
-
-pushd $DEVTOOLS_DIR
+pushd $TOOL_DIR
 
 ./bin/devtools_tool generate-code
 
