f6f865941b1c865bc6d02bf3fe730ec88c81b281
Kevin Moore <kevmoo@google.com>
2023-10-30 12:45:17 -0700

    revert on pkg:web change

--- packages/devtools_app_shared/lib/src/utils/url/_url_web.dart
+++ packages/devtools_app_shared/lib/src/utils/url/_url_web.dart
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-import 'package:web/helpers.dart';
+// ignore: avoid_web_libraries_in_flutter, as designed
+import 'dart:html';
 
 Map<String, String> loadQueryParams({String Function(String)? urlModifier}) {
   var url = getWebUrl()!;
