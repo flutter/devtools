a36090964ab474a8b5f4f0d6da736a1519ed805f
Dan Chevalier <chevalier.dan@gmail.com>
2023-10-31 14:54:25 -0400

    no unsafe plz

--- packages/devtools_app/lib/src/shared/config_specific/copy_to_clipboard/_copy_to_clipboard_web.dart
+++ packages/devtools_app/lib/src/shared/config_specific/copy_to_clipboard/_copy_to_clipboard_web.dart
@@ -3,19 +3,17 @@
 // in the LICENSE file.
 
 import 'dart:js_interop';
-import 'package:js/js_util.dart' as js;
 import 'package:web/web.dart';
 
 void copyToClipboardVSCode(String data) {
-  final message = js.newObject();
-  js.setProperty(message, 'command', 'clipboard-write');
-  js.setProperty(message, 'data', data);
-
   // This post message is only relevant in VSCode. If the parent frame is
   // listening for this command, then it will attempt to copy the contents
   // to the clipboard in the context of the parent frame.
   window.parent?.postMessage(
-    message,
+    {
+      'command': 'clipboard-write',
+      'data': data,
+    }.jsify(),
     '*'.toJS,
   );
 }
