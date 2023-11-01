16dea380578b51572765c966bd0c93ea3a7ba355
Kenzie Schmoll <kenzieschmoll@google.com>
2023-11-01 09:55:57 -0700

    remove unused import

--- packages/devtools_app_shared/lib/src/service/service_extensions.dart
+++ packages/devtools_app_shared/lib/src/service/service_extensions.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
 
 class ServiceExtension<T> {
   ServiceExtension({
