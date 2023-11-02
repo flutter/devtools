932c3da344e1143a05c3ef32b2c3ad2e55cda2ad
Bartek Pacia <barpac02@gmail.com>
2023-11-02 15:28:22 +0100

    fix DevTools extensions not being resolved when app's Dart entrypoint is integration_test/*.dart

--- packages/devtools_app/lib/src/extensions/extension_service.dart
+++ packages/devtools_app/lib/src/extensions/extension_service.dart
@@ -168,5 +168,12 @@ Future<String?> _connectedAppRootPath() async {
   if (libDirectoryIndex != -1) {
     fileUri = fileUri.substring(0, libDirectoryIndex);
   }
+
+  final libIntegrationTestDirectoryRegExp = RegExp(r'\/integration_test\/[^\/.]*.dart');
+  final libIntegrationTestDirectoryIndex = fileUri.indexOf(libIntegrationTestDirectoryRegExp);
+  if (libIntegrationTestDirectoryIndex != -1) {
+    fileUri = fileUri.substring(0, libIntegrationTestDirectoryIndex);
+  }
+
   return fileUri;
 }
