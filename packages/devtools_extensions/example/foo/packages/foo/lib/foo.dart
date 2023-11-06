899f9418a0cf45f65b3f701db09a9a2d04e523e6
Kenzie Schmoll <kenzieschmoll@google.com>
2023-11-06 11:01:43 -0800

    organize imports

--- packages/devtools_extensions/example/foo/packages/foo/lib/foo.dart
+++ packages/devtools_extensions/example/foo/packages/foo/lib/foo.dart
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/foo_widget.dart';
 export 'src/foo_controller.dart';
+export 'src/foo_widget.dart';
