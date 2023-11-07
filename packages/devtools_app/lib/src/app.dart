2b1384464c89b45b4f75c92b3d9da2e146e45280
hangyu <108393416+hangyujin@users.noreply.github.com>
2023-11-06 16:33:55 -0800

    1

--- packages/devtools_app/lib/src/app.dart
+++ packages/devtools_app/lib/src/app.dart
@@ -586,7 +586,7 @@ List<DevToolsScreen> defaultScreens({
       AppSizeScreen(),
       createController: (_) => AppSizeController(),
     ),
-    //if (FeatureFlags.deepLinkValidation)
+    if (FeatureFlags.deepLinkValidation)
       DevToolsScreen<DeepLinksController>(
         DeepLinksScreen(),
         createController: (_) => DeepLinksController(),
