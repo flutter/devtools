5c9ed3bf64a66b44663834900b0264775ad6b1e0
Kenzie Schmoll <kenzieschmoll@google.com>
2023-11-15 09:50:43 -0800

    Add better error messaging when accessing devtools extension globals too soon

---
+++ packages/devtools_extensions/test/devtools_extension_test.dart
@@ -0,0 +1,23 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:devtools_extensions/devtools_extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Global managers', () {
+    test('accessing early throws error', () {
+      expect(serviceManager, throwsStateError);
+      expect(extensionManager, throwsStateError);
+    });
+
+    testWidgets('building $DevToolsExtension initializes globals',
+        (tester) async {
+      await tester.pumpWidget(const DevToolsExtension(child: SizedBox()));
+      expect(serviceManager, isNotNull);
+      expect(extensionManager, isNotNull);
+    });
+  });
+}
