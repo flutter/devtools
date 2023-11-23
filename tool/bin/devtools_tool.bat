a5103ad61a005da3512eb9318037692782c020d5
Danny Tuppeny <danny@tuppeny.com>
2023-11-23 12:08:59 +0000

    Add an env var to force devtools_tool to use SDKs from PATH

--- tool/bin/devtools_tool.bat
+++ tool/bin/devtools_tool.bat
@@ -1,5 +1,10 @@
 @echo off
 
-%~dp0/../flutter-sdk/bin/dart run %~dp0/devtools_tool.dart %*
+IF DEFINED DEVTOOLS_TOOL_FLUTTER_FROM_PATH (
+	echo Running devtools_tool using Dart/Flutter from PATH because DEVTOOLS_TOOL_FLUTTER_FROM_PATH is set
+	dart run %~dp0/devtools_tool.dart %*
+) ELSE (
+	%~dp0/../flutter-sdk/bin/dart run %~dp0/devtools_tool.dart %*
+)
 
 EXIT /B %errorlevel%
