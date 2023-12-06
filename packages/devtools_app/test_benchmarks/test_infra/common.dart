6c51f5eff8b408a71de1522154e1cb755ea73f51
Kenzie Schmoll <kenzieschmoll@google.com>
2023-12-06 13:54:57 -0800

    add common.dart

---
+++ packages/devtools_app/test_benchmarks/test_infra/common.dart
@@ -0,0 +1,19 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// The initial page to load upon opening the DevTools benchmark app or
+/// reloading it in Chrome.
+//
+// We use an empty initial page so that the benchmark server does not attempt
+// to load the default page 'index.html', which will show up as "page not
+// found" in DevTools.
+const String benchmarkInitialPage = '';
+
+const String devtoolsBenchmarkPrefix = 'devtools';
+
+enum DevToolsBenchmark {
+  navigateThroughOfflineScreens;
+
+  String get id => '${devtoolsBenchmarkPrefix}_$name';
+}
