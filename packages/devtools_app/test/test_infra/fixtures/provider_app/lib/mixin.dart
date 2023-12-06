f486c38b3b12369ea58070838bbacfdc6724c555
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2023-12-06 14:31:50 -0800

    Remove unused test fixtures (#6907)

--- packages/devtools_app/test/test_infra/fixtures/provider_app/lib/mixin.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-mixin Mixin {
-  // ignore: unused_field, prefer_final_fields, the property is used for testing
-  int _privateMixinProperty = 0;
-}
