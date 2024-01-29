f0161e7a314f2d5e7da422519ed5bfa22f65ed79
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2024-01-29 11:00:48 -0800

    Migrate `tag_version.sh` to a `devtools_tool` command (#7129)

--- tool/tag_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURRENT_VERSION="$(sed -n -e 's/^.*version: //p' packages/devtools_app/pubspec.yaml)"
-
-echo "Tagging version: $CURRENT_VERSION"
-set -x #echo on
-
-git tag -a v$CURRENT_VERSION -m "DevTools $CURRENT_VERSION"
-
-git push upstream v$CURRENT_VERSION
