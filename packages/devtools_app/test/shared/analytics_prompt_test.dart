66c92fea58b82c0d318724e5df6829256ca8221f
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2024-02-06 15:20:24 -0500

    Dart format fix

--- packages/devtools_app/test/shared/analytics_prompt_test.dart
+++ packages/devtools_app/test/shared/analytics_prompt_test.dart
@@ -11,6 +11,7 @@ import 'package:devtools_test/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
+// ignore: depend_on_referenced_packages
 import 'package:unified_analytics/src/constants.dart' as unified_analytics;
 
 const windowSize = Size(2000.0, 1000.0);
@@ -36,8 +37,9 @@ void main() {
   }
 
   test('Unit test parseAnalyticsConsentMessage with consent message', () {
-    final result = parseAnalyticsConsentMessage(unified_analytics.kToolsMessage);
-    
+    final result =
+        parseAnalyticsConsentMessage(unified_analytics.kToolsMessage);
+
     expect(result, isNotEmpty);
     expect(result, hasLength(3));
   });
