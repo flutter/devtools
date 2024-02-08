0fc5de090ce76ff98316f538630ebab2adc3c3a9
Kenzie Schmoll <kenzieschmoll@google.com>
2024-02-08 13:35:47 -0800

    add ignore

--- packages/devtools_extensions/example/packages_with_extensions/dart_foo/packages/dart_foo/lib/dart_foo.dart
+++ packages/devtools_extensions/example/packages_with_extensions/dart_foo/packages/dart_foo/lib/dart_foo.dart
@@ -5,6 +5,7 @@
 class DartFoo {
   Future<void> loop() async {
     for (var i = 0; i < 10000; i++) {
+      // ignore: avoid_print, used in an example
       print('Looping from DartFoo.loop(): $i');
       await Future.delayed(const Duration(seconds: 10));
     }
