ced0ebfced78d52464b6eedf946abac2e47128cc
Polina Cherkasova <polina.c@live.com>
2024-02-10 16:09:53 -0800

    Update heap_data.dart

--- packages/devtools_app/lib/src/shared/memory/heap_data.dart
+++ packages/devtools_app/lib/src/shared/memory/heap_data.dart
@@ -6,25 +6,129 @@ import 'dart:typed_data';
 
 import 'package:vm_service/vm_service.dart';
 
+import '../primitives/utils.dart';
+
 class HeapData {
-  HeapData._({
-    required this.graph,
-    required this.shortestRetainers,
-    required this.retainedSizes,
+  HeapData._(
+    this.graph, {
+    this.shortestRetainers,
+    this.retainedSizes,
   });
 
+  /// Value for rootIndex is taken from the doc:
+  /// https://github.com/dart-lang/sdk/blob/main/runtime/vm/service/heap_snapshot.md#object-ids
+  static const int rootIndex = 1;
+
   final HeapSnapshotGraph graph;
+
+  /// Each cell contains index of referrer for shortest retaining path from the root to the object.
+  ///
+  /// If 0, there is no retaining path from root.
   final Uint32List? shortestRetainers;
+
   final Uint32List? retainedSizes;
 }
 
-HeapData calculateHeapData(
-  HeapSnapshotGraph graph, {
-  bool retainers = true,
-  bool retainedSizes = true,
-}) {
-  final Uint32List shortestRetainers = Uint32List(graph.objects.length);
+final UiReleaser _uiReleaser = UiReleaser();
 
-  return HeapData._(
-      graph: graph, shortestRetainers: shortestRetainers, retainedSizes: null);
+Future<HeapData> calculateHeapData(
+  HeapSnapshotGraph graph, {
+  bool shortestRetainers = true,
+  bool retainedSizes = true,
+}) async {
+  if (!shortestRetainers && !retainedSizes) {
+    return HeapData._(graph);
+  }
+
+  final weakClasses = _WeakClasses(graph);
+  final Uint32List retainers = Uint32List(graph.objects.length);
+  final Uint32List? sizes =
+      retainedSizes ? null : Uint32List(graph.objects.length);
+  sizes?[HeapData.rootIndex] = graph.objects[HeapData.rootIndex].shallowSize;
+
+  // Array of all objects where the best distance from root is n.
+  // n starts with 0 and increases by 1 on each step of the algorithm.
+  // The objects are ends of the graph cut.
+  // See description of cut:
+  // https://en.wikipedia.org/wiki/Cut_(graph_theory)
+  // On each step the algorithm moves the cut one step further from the root.
+  var cut = [HeapData.rootIndex];
+
+  // On each step of algorithm we know that all nodes at distance n or closer to
+  // root, has parent initialized.
+  while (true) {
+    if (_uiReleaser.step()) await _uiReleaser.releaseUi();
+    final nextCut = <int>[];
+    for (var r in cut) {
+      final retainer = graph.objects[r];
+      for (var child in retainer.references) {
+        if (retainers[child] > 0) continue;
+        retainers[child] = r;
+
+        if (sizes != null) _propagateSize(graph, sizes, retainers, child);
+
+        if (weakClasses.isRetainer(child)) {
+          nextCut.add(child);
+        }
+      }
+    }
+    if (nextCut.isEmpty) {
+      return HeapData._(
+        graph,
+        shortestRetainers: shortestRetainers ? retainers : null,
+        retainedSizes: sizes,
+      );
+    }
+    cut = nextCut;
+  }
+}
+
+/// Assuming the [object] is leaf, initializes its retained size
+/// and adds the size to all its shortest retainers.
+void _propagateSize(
+  HeapSnapshotGraph graph,
+  Uint32List sizes,
+  Uint32List retainers,
+  int index,
+) {
+  final addedSize = graph.objects[index].shallowSize;
+  sizes[index] = addedSize;
+
+  while (retainers[index] > 0) {
+    index = retainers[index];
+    sizes[index] += addedSize;
+  }
+}
+
+class _WeakClasses {
+  _WeakClasses(this.graph) {
+    final weakClassesToFind = <String, String>{
+      '_WeakProperty': 'dart:core',
+      '_WeakReferenceImpl': 'dart:core',
+      'FinalizerEntry': 'dart:_internal',
+    };
+
+    for (final theClass in graph.classes) {
+      if (weakClassesToFind.containsKey(theClass.name) &&
+          weakClassesToFind[theClass.name] == theClass.libraryName) {
+        _weakClasses.add(theClass.classId);
+        weakClassesToFind.remove(theClass.name);
+        if (weakClassesToFind.isEmpty) {
+          break;
+        }
+      }
+    }
+  }
+
+  final HeapSnapshotGraph graph;
+
+  final _weakClasses = <int>{};
+
+  bool isRetainer(int objectIndex) {
+    final object = graph.objects[objectIndex];
+    if (object.references.isEmpty) return false;
+    final classId = object.classId;
+    if (_weakClasses.contains(classId)) return true;
+    return false;
+  }
 }
