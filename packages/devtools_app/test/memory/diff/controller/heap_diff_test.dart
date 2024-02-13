11394095aef265beec84a0e73768002cfd05ad43
Kenzie Schmoll <kenzieschmoll@google.com>
2024-02-13 09:50:41 -0800

    add back comment

--- packages/devtools_app/test/memory/diff/controller/heap_diff_test.dart
+++ packages/devtools_app/test/memory/diff/controller/heap_diff_test.dart
@@ -1,6 +1,6 @@
 // Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// found in the LICENSE file.
 
 import 'package:devtools_app/src/screens/memory/panes/diff/controller/heap_diff.dart';
 import 'package:devtools_app/src/screens/memory/shared/heap/heap.dart';
