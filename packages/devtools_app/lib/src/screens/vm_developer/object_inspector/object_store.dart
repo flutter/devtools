6a93098df7538918d4e84265fd01cba9c83fa67a
Sam Rawlins <srawlins@google.com>
2024-02-21 10:04:01 -0800

    Enforce no_dynamic_calls (#7216)

--- packages/devtools_app/lib/src/screens/vm_developer/object_inspector/object_store.dart
+++ packages/devtools_app/lib/src/screens/vm_developer/object_inspector/object_store.dart
@@ -49,7 +49,10 @@ class _ObjectColumn extends ColumnData<ObjectStoreEntry>
     VoidCallback? onPressed,
   }) {
     return VmServiceObjectLink(
-      object: data.value,
+      // TODO(srawlins): What type is `data` at runtime? If cast to `int`, no
+      // tests fail, but that can't be right...
+      // ignore: avoid-dynamic
+      object: (data as dynamic).value,
       onTap: onTap,
     );
   }
