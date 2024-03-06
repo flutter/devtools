2c9b0316eed94ab5bd8dabaa1b780d85637ca5d3
Kenzie Schmoll <kenzieschmoll@google.com>
2024-03-06 10:55:55 -0800

    Add a 'fatal-infos' argument to the `devtools_tool analyze` command

--- tool/lib/commands/analyze.dart
+++ tool/lib/commands/analyze.dart
@@ -11,7 +11,18 @@ import 'package:io/io.dart';
 import '../model.dart';
 import '../utils.dart';
 
+const _fatalInfosArg = 'fatal-infos';
+
 class AnalyzeCommand extends Command {
+  AnalyzeCommand() {
+    argParser.addFlag(
+      'fatal-infos',
+      help: 'Sets the "fatal-infos" flag for the dart analyze command',
+      defaultsTo: true,
+      negatable: true,
+    );
+  }
+
   @override
   String get name => 'analyze';
 
@@ -24,6 +35,8 @@ class AnalyzeCommand extends Command {
     final repo = DevToolsRepo.getInstance();
     final processManager = ProcessManager();
     final packages = repo.getPackages();
+    final fatalInfos = argResults![_fatalInfosArg] as bool;
+    final fatalInfosFlag = '--${fatalInfos ? '' : 'no-'}fatal-infos';
 
     log.stdout('Running flutter analyze...');
 
@@ -38,7 +51,7 @@ class AnalyzeCommand extends Command {
 
       final process = await processManager.runProcess(
         CliCommand.dart(
-          ['analyze', '--fatal-infos'],
+          ['analyze', fatalInfosFlag],
           // Run all so we can see the full set of results instead of stopping
           // on the first error.
           throwOnException: false,
