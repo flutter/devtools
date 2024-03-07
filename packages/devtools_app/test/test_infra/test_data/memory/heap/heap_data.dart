6884abb0cc58f4a7d44938ff40af3085743553fb
Polina Cherkasova <polina.c@live.com>
2024-03-07 13:21:47 -0800

    Update heap_data.dart

--- packages/devtools_app/test/test_infra/test_data/memory/heap/heap_data.dart
+++ packages/devtools_app/test/test_infra/test_data/memory/heap/heap_data.dart
@@ -2,51 +2,51 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
+// import 'dart:io';
 
 
-const _dataDir = 'test/test_infra/test_data/memory/heap/';
+// const _dataDir = 'test/test_infra/test_data/memory/heap/';
 
-typedef HeapLoader = Future<AdaptedHeapData> Function();
+// typedef HeapLoader = Future<AdaptedHeapData> Function();
 
-class GoldenHeapTest {
-  GoldenHeapTest({
-    required this.fileName,
-    required this.appClassName,
-  });
+// class GoldenHeapTest {
+//   GoldenHeapTest({
+//     required this.fileName,
+//     required this.appClassName,
+//   });
 
-  final String fileName;
-  final String appClassName;
+//   final String fileName;
+//   final String appClassName;
 
-  /// Loads the heap data from a file.
-  ///
-  /// Format is format used by [NativeRuntime.writeHeapSnapshotToFile].
-  Future<AdaptedHeapData> loadHeap() => heapFromFile('$_dataDir$fileName');
-}
+//   /// Loads the heap data from a file.
+//   ///
+//   /// Format is format used by [NativeRuntime.writeHeapSnapshotToFile].
+//   Future<AdaptedHeapData> loadHeap() => heapFromFile('$_dataDir$fileName');
+// }
 
-List<GoldenHeapTest> goldenHeapTests = <GoldenHeapTest>[
-  GoldenHeapTest(
-    fileName: 'counter_snapshot1',
-    appClassName: 'MyApp',
-  ),
-  GoldenHeapTest(
-    fileName: 'counter_snapshot2',
-    appClassName: 'MyApp',
-  ),
-  GoldenHeapTest(
-    fileName: 'counter_snapshot3',
-    appClassName: 'MyApp',
-  ),
-  GoldenHeapTest(
-    fileName: 'counter_snapshot4',
-    appClassName: 'MyApp',
-  ),
-];
+// List<GoldenHeapTest> goldenHeapTests = <GoldenHeapTest>[
+//   GoldenHeapTest(
+//     fileName: 'counter_snapshot1',
+//     appClassName: 'MyApp',
+//   ),
+//   GoldenHeapTest(
+//     fileName: 'counter_snapshot2',
+//     appClassName: 'MyApp',
+//   ),
+//   GoldenHeapTest(
+//     fileName: 'counter_snapshot3',
+//     appClassName: 'MyApp',
+//   ),
+//   GoldenHeapTest(
+//     fileName: 'counter_snapshot4',
+//     appClassName: 'MyApp',
+//   ),
+// ];
 
-Future<AdaptedHeapData> heapFromFile(
-  String fileName,
-) async {
-  final file = File(fileName);
-  final bytes = await file.readAsBytes();
-  return AdaptedHeapData.fromBytes(bytes);
-}
+// Future<AdaptedHeapData> heapFromFile(
+//   String fileName,
+// ) async {
+//   final file = File(fileName);
+//   final bytes = await file.readAsBytes();
+//   return AdaptedHeapData.fromBytes(bytes);
+// }
