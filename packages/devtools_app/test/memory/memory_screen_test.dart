e1752d8486858ed60192e0b242dfe96ea2cfa6b8
Polina Cherkasova <polina.c@live.com>
2024-03-12 13:11:35 -0700

    Update memory_screen_test.dart

--- packages/devtools_app/test/memory/memory_screen_test.dart
+++ packages/devtools_app/test/memory/memory_screen_test.dart
@@ -95,7 +95,10 @@ void main() {
         await pumpMemoryScreen(tester);
 
         // Should be collecting live feed.
-        expect(controller.isOffline, isFalse);
+        expect(
+          controller.initialization.value,
+          MemoryControllerInitialization.connected,
+        );
 
         // Verify Memory, Memory Source, and Memory Sources content.
         expect(find.byTooltip(ChartPaneTooltips.pauseTooltip), findsOneWidget);
