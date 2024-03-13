19a23ad288308b4b2a19c38a1af1985d486815e6
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2024-03-13 14:01:38 -0700

    Reuse logic to show disconnected screen body (#7358)

--- packages/devtools_app/test/shared/scaffold_profile_test.dart
+++ packages/devtools_app/test/shared/scaffold_profile_test.dart
@@ -108,7 +108,7 @@ class _TestScreen extends Screen {
   final Key key;
 
   @override
-  Widget build(BuildContext context) {
+  Widget buildScreenBody(BuildContext context) {
     return SizedBox(key: key);
   }
 }
