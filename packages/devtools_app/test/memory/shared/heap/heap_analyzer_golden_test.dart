6005df288874d0cc46e8ed0db5a1c2161471b411
Polina Cherkasova <polina.c@live.com>
2024-03-15 16:11:22 -0700

    Update heap_analyzer_golden_test.dart

--- packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
+++ packages/devtools_app/test/memory/shared/heap/heap_analyzer_golden_test.dart
@@ -1,43 +1,57 @@
-// // Copyright 2022 The Chromium Authors. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-// import 'package:devtools_app/src/screens/memory/shared/heap/spanning_tree.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:devtools_app/src/shared/memory/class_name.dart';
+import 'package:devtools_app/src/shared/memory/heap_data.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import '../../../test_infra/test_data/memory/heap/heap_data.dart';
+import '../../../test_infra/test_data/memory/heap/heap_data.dart';
 
-// void main() {
-//   for (var t in goldenHeapTests) {
-//     group(t.fileName, () {
-//       late AdaptedHeapData heap;
+void main() {
+  for (var t in goldenHeapTests) {
+    group(t.fileName, () {
+      late HeapData heap;
+      late int appClassId;
 
-//       setUp(() async {
-//         heap = await t.loadHeap();
-//       });
+      setUp(() async {
+        heap = await HeapData.calculate(await t.loadHeap(), DateTime.now());
+        appClassId = findClassId(heap, t.appClassName);
+      });
 
-//       test('has many objects and roots.', () {
-//         expect(heap.objects.length, greaterThan(1000));
-//         expect(
-//           heap.objects[heap.rootIndex].outRefs.length,
-//           greaterThan(1000),
-//           reason: t.fileName,
-//         );
-//       });
+      test('has many objects and roots.', () {
+        expect(heap.graph.objects.length, greaterThan(1000));
+        expect(
+          heap.graph.objects[heap.rootIndex].references.length,
+          greaterThan(1000),
+          reason: t.fileName,
+        );
+      });
 
-//       test('has exactly one object of type ${t.appClassName}.', () {
-//         final appObjects =
-//             heap.objects.where((o) => o.heapClass.className == t.appClassName);
-//         expect(appObjects, hasLength(1), reason: t.fileName);
-//       });
+      test('has exactly one object of type ${t.appClassName}.', () {
+        final appObjects =
+            heap.graph.objects.where((o) => o.classId == appClassId);
+        expect(appObjects, hasLength(1), reason: t.fileName);
+      });
 
-//       test('has path to the object of type ${t.appClassName}.', () async {
-//         await calculateHeap(heap);
-//         final appObject = heap.objects
-//             .where((o) => o.heapClass.className == t.appClassName)
-//             .first;
-//         expect(appObject.retainer, isNotNull, reason: t.fileName);
-//       });
-//     });
-//   }
-// }
+      test('has path to the object of type ${t.appClassName}.', () async {
+        final className =
+            HeapClassName.fromHeapSnapshotClass(heap.graph.classes[appClassId]);
+
+        final classData = heap.classes!.asMap()[className]!;
+        expect(classData.byPath, isNotEmpty, reason: t.fileName);
+      });
+    });
+  }
+}
+
+int findClassId(HeapData heap, String className) {
+  return heap.graph.classes
+      .firstWhere(
+        (c) => c.name == className,
+        orElse: () => throw StateError(
+          'No class found with name $className.',
+        ),
+      )
+      .classId;
+}
