d02406da9a25ce1bbdd65f2dbe2e42e89c8642cd
Polina Cherkasova <polina.c@live.com>
2024-03-24 12:06:41 -0700

    Update simple_items.dart

--- packages/devtools_app/lib/src/shared/memory/simple_items.dart
+++ packages/devtools_app/lib/src/shared/memory/simple_items.dart
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:typed_data';
+
+import 'package:file_selector/file_selector.dart';
+import 'package:vm_service/vm_service.dart';
+
 /// Direction of reference between objects in memory.
 enum RefDirection {
   inbound,
@@ -24,3 +29,19 @@ class MemoryFootprint {
 /// Value for rootIndex is taken from the doc:
 /// https://github.com/dart-lang/sdk/blob/main/runtime/vm/service/heap_snapshot.md#object-ids
 const int heapRootIndex = 1;
+
+extension HeapSnapshotGraphSerialization on HeapSnapshotGraph {
+  static Future<HeapSnapshotGraph> load(XFile file) async {
+    final bytes = await file.readAsBytes();
+    final data = bytes.buffer.asByteData();
+    return HeapSnapshotGraph.fromChunks([data]);
+  }
+
+  Uint8List toSavableData() {
+    final b = BytesBuilder();
+    for (var chunk in toChunks()) {
+      b.add(chunk.buffer.asUint8List());
+    }
+    return b.toBytes();
+  }
+}
