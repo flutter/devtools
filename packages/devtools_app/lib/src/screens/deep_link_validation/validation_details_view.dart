df52ae2fd55823debecdc9a5e98cdb9663b74fd3
hangyu <jhy03261997@gmail.com>
2024-03-25 16:42:07 -0700

    [deep link] some strings, colors, style updates (#7443)
    * 1
    
    * update
    
    * Update deep_links_model.dart
    
    * Update colors.dart

--- packages/devtools_app/lib/src/screens/deep_link_validation/validation_details_view.dart
+++ packages/devtools_app/lib/src/screens/deep_link_validation/validation_details_view.dart
@@ -196,8 +196,8 @@ class _AssetLinksJsonFileIssues extends StatelessWidget {
               _FailureDetails(
                 errors: errors,
                 oneFixGuideForAll:
-                    'To fix above issues, publish the recommended Digital Asset Links'
-                    ' JSON file below to all of the failed website domains at the following'
+                    'To fix above issues, copy the recommended Digital Asset Links'
+                    ' JSON file below and publish it to all of the failed website domains at the following'
                     ' location: https://${controller.selectedLink.value!.domain}/.well-known/assetlinks.json.',
               ),
               const SizedBox(height: denseSpacing),
@@ -338,7 +338,7 @@ class _LocalFingerprint extends StatelessWidget {
                     await showDialog(
                       context: context,
                       builder: (_) {
-                        return const AlertDialog(
+                        return const DevToolsDialog(
                           title: Text('This is not a valid fingerprint'),
                           content: Text(
                             'A valid fingerprint consists of 32 pairs of hexadecimal digits separated by colons.'
@@ -557,7 +557,7 @@ class _PathCheckTable extends StatelessWidget {
       children: [
         const SizedBox(height: intermediateSpacing),
         Text(
-          'Path check',
+          'App check',
           style: theme.textTheme.titleSmall,
         ),
         const SizedBox(height: intermediateSpacing),
