6e9d5a2e898347fcf09ba27a42ee6187519092ac
Polina Cherkasova <polina.c@live.com>
2024-03-25 12:39:44 -0700

    Update _export_desktop.dart

--- packages/devtools_app/lib/src/shared/config_specific/import_export/_export_desktop.dart
+++ packages/devtools_app/lib/src/shared/config_specific/import_export/_export_desktop.dart
@@ -24,6 +24,7 @@ class ExportControllerDesktop extends ExportController {
     if (content is String) {
       _fs.writeStringToFile(fileName, content);
     } else if (content is Uint8List) {
+      // TODO(polina-c): implement https://github.com/flutter/devtools/issues/7425
       throw UnimplementedError();
     } else {
       throw StateError('Unsupported content type: $T');
