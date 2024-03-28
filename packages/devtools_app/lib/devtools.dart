b0a58ad31c8045d44b4d2725382d0d56cb56c41c
DartDevtoolWorkflowBot <121203384+DartDevtoolWorkflowBot@users.noreply.github.com>
2024-03-28 17:55:37 -0400

    Updating from 2.34.1 to 2.35.0-dev.0 (#7492)

--- packages/devtools_app/lib/devtools.dart
+++ packages/devtools_app/lib/devtools.dart
@@ -9,4 +9,4 @@
 // the constant declaration `const String version =`.
 // If you change the declaration you must also modify the regex in
 // tools/update_version.dart.
-const String version = '2.34.1';
+const String version = '2.35.0-dev.0';
