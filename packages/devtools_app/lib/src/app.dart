959249f0c4850dddf98056edb12d1e26193430d6
Polina Cherkasova <polina.c@live.com>
2024-04-03 09:29:44 -0700

    Update app.dart

--- packages/devtools_app/lib/src/app.dart
+++ packages/devtools_app/lib/src/app.dart
@@ -444,11 +444,10 @@ class DevToolsScreen<C extends Object?> {
   /// If [createController] and [controller] are both null, [screen] will be
   /// responsible for creating and maintaining its own controller.
   ///
-  /// The controller should expect to be already connected to an application
-  /// at the time of creation.
-  /// But it should invoke unawaitable `serviceConnection.serviceManager.onServiceAvailable`
-  /// in the constructor to make sure the service is ready to be used.
-  ///
+  /// In the controller initialization, if logic requires a connected [VmService]
+  /// object (serviceConnection.serviceManager.service), then the controller should first await
+  /// the serviceConnection.serviceManager.onServiceAvailable future to ensure the service has
+  /// been initialized.
   /// The controller does not need to handle re-connection to the application. When reconnected,
   /// DevTools will create a new controller. However, the controller should make sure
   /// not to fail if the connection is lost.
