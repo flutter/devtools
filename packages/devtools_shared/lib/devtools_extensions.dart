aeb20e5b994b8fa132a9e6defbe86068b485a3bb
Kenzie Schmoll <kenzieschmoll@google.com>
2024-04-10 15:07:04 -0700

    Fix issue with io dependencies

--- packages/devtools_shared/lib/devtools_extensions.dart
+++ packages/devtools_shared/lib/devtools_extensions.dart
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/extensions/extension_manager.dart';
+export 'src/extensions/constants.dart';
 export 'src/extensions/extension_model.dart';
