520c850c330769655112cbea2c0aae7ab5be6762
Polina Cherkasova <polina.c@live.com>
2024-04-18 13:26:22 -0700

    Prepare cherry-pick release - DevTools 2.34.3

--- packages/devtools_app/lib/devtools.dart
+++ packages/devtools_app/lib/devtools.dart
@@ -9,4 +9,4 @@
 // the constant declaration `const String version =`.
 // If you change the declaration you must also modify the regex in
 // tools/update_version.dart.
-const String version = '2.34.2';
+const String version = '2.34.3';
