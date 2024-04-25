7af2302c599723748bd49163ecb0bfbaf54e71df
Polina Cherkasova <polina.c@live.com>
2024-04-25 16:28:51 -0700

    Update event_chart_controller.dart

--- packages/devtools_app/lib/src/screens/memory/panes/chart/controller/charts/event_chart_controller.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/chart/controller/charts/event_chart_controller.dart
@@ -12,18 +12,20 @@ import '../../../../../../shared/charts/chart_trace.dart'
     show ChartType, PaintCharacteristics, ChartSymbol;
 import '../../../../shared/primitives/memory_timeline.dart';
 
-/// VM's GCs are displayed in a smaller glyph and closer to the heap graph.
-const visibleVmEvent = 0.4;
+class _Sizes {
+  /// VM's GCs are displayed in a smaller glyph and closer to the heap graph.
+  static const visibleVm = 0.4;
 
-/// Flutter events and user custom events.
-const extensionEvent = 3.7;
+  /// Flutter events and user custom events.
+  static const extensions = 3.7;
 
-/// Event to display in the event pane (User initiated GC, snapshot,
-/// automatic snapshot, etc.)
-const visibleEvent = 2.4;
+  /// Event to display in the event pane (User initiated GC, snapshot,
+  /// automatic snapshot, etc.)
+  static const visible = 2.4;
 
-/// Monitor events Y axis.
-const visibleMonitorEvent = 1.4;
+  /// Monitor events Y axis.
+  static const visibleMonitor = 1.4;
+}
 
 /// Name of each trace being charted, index order is the trace index
 /// too (order of trace creation top-down order).
@@ -58,7 +60,7 @@ class EventChartController extends ChartController {
           name: 'Event Pane',
         ) {
     _setupTraces();
-    setFixedYRange(visibleVmEvent, extensionEvent);
+    setFixedYRange(_Sizes.visibleVm, _Sizes.extensions);
     setupData();
 
     addAutoDisposeListener(memoryTimeline.sampleAdded, () {
@@ -140,8 +142,8 @@ class EventChartController extends ChartController {
         symbol: ChartSymbol.filledTriangle,
         height: 20,
         width: 20,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.extensionEvents.toString(),
     );
@@ -157,8 +159,8 @@ class EventChartController extends ChartController {
         color: Colors.green,
         strokeWidth: 3,
         diameter: 6,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.snapshot.toString(),
     );
@@ -175,8 +177,8 @@ class EventChartController extends ChartController {
         color: Colors.red,
         strokeWidth: 3,
         diameter: 6,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.autoSnapshot.toString(),
     );
@@ -193,8 +195,8 @@ class EventChartController extends ChartController {
         color: Colors.blue,
         strokeWidth: 3,
         diameter: 6,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.manualGC.toString(),
     );
@@ -213,8 +215,8 @@ class EventChartController extends ChartController {
         color: mainMonitorColor,
         strokeWidth: 3,
         diameter: 6,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.monitor.toString(),
     );
@@ -230,8 +232,8 @@ class EventChartController extends ChartController {
         color: Colors.grey[600]!,
         strokeWidth: 4,
         diameter: 6,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
         concentricCenterColor: mainMonitorColor,
         concentricCenterDiameter: 4,
       ),
@@ -250,8 +252,8 @@ class EventChartController extends ChartController {
         color: Colors.blue,
         symbol: ChartSymbol.disc,
         diameter: 4,
-        fixedMinY: visibleVmEvent,
-        fixedMaxY: extensionEvent,
+        fixedMinY: _Sizes.visibleVm,
+        fixedMaxY: _Sizes.extensions,
       ),
       name: EventsTraceName.gc.toString(),
     );
@@ -273,14 +275,14 @@ class EventChartController extends ChartController {
       // Plot the VM GC on the VmEvent trace with a fixed Y coordinate.
       addDataToTrace(
         EventsTraceName.gc.index,
-        chart_trace.Data(sample.timestamp, visibleVmEvent),
+        chart_trace.Data(sample.timestamp, _Sizes.visibleVm),
       );
     }
     final events = sample.memoryEventInfo;
     if (events.hasExtensionEvents) {
       final data = chart_trace.DataAggregate(
         sample.timestamp,
-        extensionEvent,
+        _Sizes.extensions,
         (events.extensionEvents?.theEvents ?? []).length,
       );
       addDataToTrace(EventsTraceName.extensionEvents.index, data);
@@ -290,7 +292,7 @@ class EventChartController extends ChartController {
     // of the event pane (visible Events).
     final data = chart_trace.Data(
       sample.timestamp,
-      visibleEvent,
+      _Sizes.visible,
     );
 
     if (events.isEventGC) {
@@ -312,7 +314,7 @@ class EventChartController extends ChartController {
       final allocationEvent = events.allocationAccumulator!;
       final data = chart_trace.Data(
         sample.timestamp,
-        visibleMonitorEvent,
+        _Sizes.visibleMonitor,
       );
       if (allocationEvent.isReset) {
         addDataToTrace(EventsTraceName.monitorReset.index, data);
