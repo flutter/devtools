c28e97e595bacaa41fd2ae0518de6c9800987df7
Kenzie Schmoll <kenzieschmoll@google.com>
2024-05-06 13:50:25 -0700

    more testing

--- packages/devtools_app/test/shared/query_parameters_test.dart
+++ packages/devtools_app/test/shared/query_parameters_test.dart
@@ -9,16 +9,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('$DevToolsQueryParams', () {
-    test('creates empty params', () {
-      final params = DevToolsQueryParams.empty();
-      expect(params.vmServiceUri, isNull);
-      expect(params.embed, isFalse);
-      expect(params.hiddenScreens, isEmpty);
-      expect(params.offlineScreenId, isNull);
-      expect(params.legacyPage, isNull);
-      expect(params.ideThemeParams.params, isEmpty);
-    });
-
     test('successfully creates params', () {
       final params = DevToolsQueryParams({
         DevToolsQueryParams.vmServiceUriKey: 'some_uri',
@@ -44,6 +34,64 @@ void main() {
       expect(params.ideThemeParams.foregroundColor, const Color(0xFF112244));
       expect(params.ideThemeParams.fontSize, 8.0);
       expect(params.ideThemeParams.darkMode, true);
+    });
+
+    test('creates empty params', () {
+      final params = DevToolsQueryParams.empty();
+      expect(params.vmServiceUri, isNull);
+      expect(params.embed, isFalse);
+      expect(params.hiddenScreens, isEmpty);
+      expect(params.offlineScreenId, isNull);
+      expect(params.legacyPage, isNull);
+      expect(params.ideThemeParams.params, isEmpty);
+    });
+
+    test('creates params with updates', () {
+      var params = DevToolsQueryParams({
+        DevToolsQueryParams.vmServiceUriKey: 'some_uri',
+        DevToolsQueryParams.hideScreensKey: 'foo,bar,baz',
+        DevToolsQueryParams.offlineScreenIdKey: 'performance',
+        DevToolsQueryParams.legacyPageKey: 'memory',
+        // IdeThemeQueryParams values
+        'embed': 'true',
+        'backgroundColor': '#112233',
+        'foregroundColor': '#112244',
+        'fontSize': '8.0',
+        'theme': 'dark',
+      });
+
+      expect(params.vmServiceUri, 'some_uri');
+      expect(params.embed, true);
+      expect(params.hiddenScreens, {'foo', 'bar', 'baz'});
+      expect(params.offlineScreenId, 'performance');
+      expect(params.legacyPage, 'memory');
+      expect(params.ideThemeParams.params, isNotEmpty);
+      expect(params.ideThemeParams.embed, true);
+      expect(params.ideThemeParams.backgroundColor, const Color(0xFF112233));
+      expect(params.ideThemeParams.foregroundColor, const Color(0xFF112244));
+      expect(params.ideThemeParams.fontSize, 8.0);
+      expect(params.ideThemeParams.darkMode, true);
+
+      params = params.withUpdates({
+        DevToolsQueryParams.vmServiceUriKey: 'some_other_uri',
+        DevToolsQueryParams.hideScreensKey: 'foo',
+        // Update some IdeThemeQueryParams values
+        'embed': 'false',
+        'fontSize': '10.0',
+        'theme': 'light',
+      });
+
+      expect(params.vmServiceUri, 'some_other_uri');
+      expect(params.embed, false);
+      expect(params.hiddenScreens, {'foo'});
+      expect(params.offlineScreenId, 'performance');
+      expect(params.legacyPage, 'memory');
+      expect(params.ideThemeParams.params, isNotEmpty);
+      expect(params.ideThemeParams.embed, false);
+      expect(params.ideThemeParams.backgroundColor, const Color(0xFF112233));
+      expect(params.ideThemeParams.foregroundColor, const Color(0xFF112244));
+      expect(params.ideThemeParams.fontSize, 10.0);
+      expect(params.ideThemeParams.darkMode, false);
     });
   });
 }
