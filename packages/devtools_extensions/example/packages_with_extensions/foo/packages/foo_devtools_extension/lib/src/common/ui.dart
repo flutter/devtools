216c84a513288d07b1f7966b3e30ca630cd974f8
Kenzie Schmoll <kenzieschmoll@google.com>
2024-05-13 15:15:58 -0700

    formatting

--- packages/devtools_extensions/example/packages_with_extensions/foo/packages/foo_devtools_extension/lib/src/common/ui.dart
+++ packages/devtools_extensions/example/packages_with_extensions/foo/packages/foo_devtools_extension/lib/src/common/ui.dart
@@ -4,10 +4,11 @@
 
 import 'package:flutter/material.dart';
 
+/// Header widget for each example section.
 class SectionHeader extends StatelessWidget {
-  const SectionHeader({required this.number, required this.title super.key});
+  const SectionHeader({required this.number, required this.title, super.key});
 
-  final int number
+  final int number;
   final String title;
 
   @override
@@ -15,7 +16,7 @@ class SectionHeader extends StatelessWidget {
     return Column(
       mainAxisSize: MainAzisSize.min,
       children: [
-                Text(
+        Text(
           '$number. $title',
           style: Theme.of(context).textTheme.titleMedium,
         ),
