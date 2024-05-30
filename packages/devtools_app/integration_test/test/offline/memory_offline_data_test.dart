60fdf293ff5fc171215258e0b5fd5f1c18c52356
Polina Cherkasova <polina.c@live.com>
2024-05-30 15:45:26 -0700

    Update memory_offline_data_test.dart

--- packages/devtools_app/integration_test/test/offline/memory_offline_data_test.dart
+++ packages/devtools_app/integration_test/test/offline/memory_offline_data_test.dart
@@ -31,11 +31,6 @@ void main() {
       logStatus('5 - tapped Diff Snapshots');
       await tester.pumpAndSettle(longPumpDuration);
       logStatus('6 - pumped and settled');
-      expect(
-        find.text('Class type legend:', findRichText: true),
-        findsOneWidget,
-      );
-      logStatus('7 - found Class type legend');
 
       await tester.tap(find.text('Profile Memory'));
       logStatus('8 - tapped Profile Memory');
