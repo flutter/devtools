0dffb5650e6a2c71eda15f5ac8256a50d7862c36
Kenzie Schmoll <kenzieschmoll@google.com>
2024-06-07 09:08:39 -0700

    Fix a regression with accessing the Flutter store file.

--- packages/devtools_app/lib/src/shared/server/_analytics_api.dart
+++ packages/devtools_app/lib/src/shared/server/_analytics_api.dart
@@ -57,22 +57,17 @@ Future<void> setAnalyticsEnabled([bool value = true]) async {
 // '/api/devToolsEnabled' returns the value (identical VM service) and
 // '/api/devToolsEnabled?value=true' sets the value.
 
-/// Request Flutter tool stored property value enabled (GA enabled) stored in
-/// the file '~\.flutter'.
+/// Whether GA is enabled in the Flutter store file ~\.flutter.
 ///
-/// Return bool.
-/// Return value of false implies either GA is disabled or the Flutter Tool has
-/// never been run (null returned from the server).
+/// A return value of false implies either GA is disabled or the Flutter Tool
+/// has never been run.
 Future<bool> _isFlutterGAEnabled() async {
   bool enabled = false;
 
   if (isDevToolsServerAvailable) {
     final resp = await request(apiGetFlutterGAEnabled);
     if (resp?.statusOk ?? false) {
-      // A return value of 'null' implies Flutter tool has never been run so
-      // return false for Flutter GA enabled.
-      final responseValue = json.decode(resp!.body);
-      enabled = responseValue ?? false;
+      enabled = json.decode(resp!.body) as bool;
     } else {
       logWarning(resp, apiGetFlutterGAEnabled);
     }
@@ -81,12 +76,12 @@ Future<bool> _isFlutterGAEnabled() async {
   return enabled;
 }
 
-/// Request Flutter tool stored property value clientID (GA enabled) stored in
-/// the file '~\.flutter'.
+/// Requests the Flutter client id from the Flutter store file ~\.flutter.
 ///
-/// Return as a String, empty string implies Flutter Tool has never been run.
+/// If an empty String is returned, this means that Flutter Tool has never been
+/// run.
 Future<String> flutterGAClientID() async {
-  // Default empty string, Flutter tool never run.
+  // Default empty string, Flutter tool never ran.
   String clientId = '';
 
   if (isDevToolsServerAvailable) {
@@ -97,9 +92,6 @@ Future<String> flutterGAClientID() async {
       if (resp?.statusOk ?? false) {
         clientId = json.decode(resp!.body);
         if (clientId.isEmpty) {
-          // Requested value of 'null' (Flutter tool never ran). Server request
-          // apiGetFlutterGAClientId should not happen because the
-          // isFlutterGAEnabled test should have been false.
           _log.warning('$apiGetFlutterGAClientId is empty');
         }
       } else {
--- packages/devtools_shared/lib/src/server/file_system.dart
+++ packages/devtools_shared/lib/src/server/file_system.dart
@@ -7,7 +7,7 @@ import 'dart:io';
 
 import 'package:path/path.dart' as path;
 
-import 'usage.dart';
+import 'devtools_store.dart';
 
 // ignore: avoid_classes_with_only_static_members, requires refactor.
 class LocalFileSystem {
@@ -76,9 +76,95 @@ class LocalFileSystem {
     return jsonEncode(json);
   }
 
+  /// The location of the Flutter store file, ~/.flutter.
+  static String flutterStoreLocation() {
+    return path.join(_userHomeDir(), '.flutter');
+  }
+
   /// Whether the flutter store file exists.
   static bool flutterStoreExists() {
-    final flutterStore = File(path.join(_userHomeDir(), '.flutter'));
+    final flutterStore = File(flutterStoreLocation());
     return flutterStore.existsSync();
   }
 }
+
+class IOPersistentProperties extends PersistentProperties {
+  IOPersistentProperties(
+    String name, {
+    String? documentDirPath,
+  }) : super(name) {
+    final String fileName = name.replaceAll(' ', '_');
+    documentDirPath ??= LocalFileSystem._userHomeDir();
+    _file = File(path.join(documentDirPath, fileName));
+    if (!_file.existsSync()) {
+      _file.createSync(recursive: true);
+    }
+    syncSettings();
+  }
+
+  IOPersistentProperties.fromFile(File file) : super(path.basename(file.path)) {
+    _file = file;
+    if (!_file.existsSync()) {
+      _file.createSync(recursive: true);
+    }
+    syncSettings();
+  }
+
+  late File _file;
+
+  late Map<String, Object?> _map;
+
+  @override
+  // ignore: avoid-dynamic, necessary here.
+  dynamic operator [](String key) => _map[key];
+
+  @override
+  void operator []=(String key, Object? value) {
+    if (value == null && !_map.containsKey(key)) return;
+    if (_map[key] == value) return;
+
+    if (value == null) {
+      _map.remove(key);
+    } else {
+      _map[key] = value;
+    }
+
+    try {
+      _file.writeAsStringSync('${_jsonEncoder.convert(_map)}\n');
+    } catch (_) {}
+  }
+
+  @override
+  void syncSettings() {
+    try {
+      String contents = _file.readAsStringSync();
+      if (contents.isEmpty) contents = '{}';
+      _map = (jsonDecode(contents) as Map).cast<String, Object>();
+    } catch (_) {
+      _map = {};
+    }
+  }
+
+  void remove(String propertyName) {
+    _map.remove(propertyName);
+  }
+}
+
+abstract class PersistentProperties {
+  PersistentProperties(this.name);
+
+  final String name;
+
+  // ignore: avoid-dynamic, dynamic by design.
+  dynamic operator [](String key);
+
+  // ignore: avoid-dynamic, dynamic by design.
+  void operator []=(String key, dynamic value);
+
+  /// Re-read settings from the backing store.
+  ///
+  /// May be a no-op on some platforms.
+  void syncSettings();
+}
+
+const JsonEncoder _jsonEncoder = JsonEncoder.withIndent('  ');
