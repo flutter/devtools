40b9a2ccfe98bcbfa5751ce24dc68b9cdc004acf
Polina Cherkasova <polina.c@live.com>
2024-06-06 17:58:40 -0700

    Update diff_pane.dart

--- packages/devtools_app/lib/src/screens/memory/panes/diff/diff_pane.dart
+++ packages/devtools_app/lib/src/screens/memory/panes/diff/diff_pane.dart
@@ -142,7 +142,7 @@ class SnapshotInstanceItemPane extends StatelessWidget {
 String _snapshotDocumentation(bool isDark) {
   final filePostfix = isDark ? 'dark' : 'light';
 
-  /// See https://stackoverflow.com/questions/62292512/firebase-flutter-web-app-works-locally-but-when-deployed-images-are-not-shown
+  // TODO(polina-c): remove after fixing https://github.com/flutter/flutter/issues/149866
   const isWebProd = kIsWeb && !kDebugMode;
   const imagePath = isWebProd ? 'assets/' : '';
   final uploadImageUrl = '${imagePath}assets/img/doc/upload_$filePostfix.png';
