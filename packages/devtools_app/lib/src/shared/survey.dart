c15eee8b8641b48f02d84101373f1634570b9b8a
Kenzie Schmoll <kenzieschmoll@google.com>
2024-06-21 11:01:48 -0700

    formatting

--- packages/devtools_app/lib/src/shared/survey.dart
+++ packages/devtools_app/lib/src/shared/survey.dart
@@ -28,10 +28,10 @@ class SurveyService {
   static const _maxShowSurveyCount = 5;
 
   /// The URL that we will fetch the DevTools survey metadata from.
-  /// 
+  ///
   /// To run new surveys, update the content at
   /// https://github.com/flutter/uxr/blob/master/surveys/devtools-survey-metadata.json.
-  /// 
+  ///
   /// This content will be propagated to the storage.googleapis.com domain
   /// automatically.
   static final _metadataUrl = Uri.https(
