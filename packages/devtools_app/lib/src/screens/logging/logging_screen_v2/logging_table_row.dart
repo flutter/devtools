95b7c357999110bf49434277133296b3cad5c97c
Dan Chevalier <1386322+CoderDake@users.noreply.github.com>
2024-07-05 14:24:06 -0400

    Got Metadata chits working and overflowing nicely with predictive heights. Pushing up changes so I'll be ready to take this on from the outside.

--- packages/devtools_app/lib/src/screens/logging/logging_screen_v2/logging_table_row.dart
+++ packages/devtools_app/lib/src/screens/logging/logging_screen_v2/logging_table_row.dart
@@ -2,14 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:devtools_app_shared/ui.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../shared/globals.dart';
 import '../../../shared/ui/utils.dart';
 import 'logging_controller_v2.dart';
+
+final loggingTableTimeFormat = DateFormat('HH:mm:ss.SSS');
 
 class LoggingTableRow extends StatefulWidget {
   const LoggingTableRow({
@@ -31,6 +35,25 @@ class LoggingTableRow extends StatefulWidget {
   static TextStyle get detailsStyle =>
       Theme.of(navigatorKey.currentContext!).regularTextStyle;
 
+  static List<_MetadataChit> _metadataChits(LogDataV2 data, double maxWidth) {
+    //TODO: maxwidth - prevWidget on Line
+    final allChits = [
+      _WhenMetaDataChit(
+        data: data,
+        maxWidth: maxWidth,
+      ),
+      _KindMetaDataChit(
+        data: data,
+        maxWidth: maxWidth,
+      ),
+      _FrameElapsedMetaDataChit(
+        data: data,
+        maxWidth: maxWidth,
+      ),
+    ];
+    return allChits.where((chit) => chit.isPresent()).toList();
+  }
+
   @override
   State<LoggingTableRow> createState() => _LoggingTableRowState();
 
@@ -47,15 +70,42 @@ class LoggingTableRow extends StatefulWidget {
       TextSpan(text: text, style: detailsStyle),
       maxWidth: maxWidth,
     );
-
-    // TODO(danchevalier): Improve row2 height by manually flowing metadas into another row
-    // if theyoverflow.
-    final row2Height = calculateTextSpanHeight(
-      TextSpan(text: 'short text for now', style: metadataStyle),
-      maxWidth: maxWidth,
-    );
+    print('bef rowHeight');
+    final row2Height = _calculateMetaDataHeight(log, width);
+    print('row2height: ${row2Height}');
 
     return row1Height + row2Height + _padding * 2;
+  }
+
+  static double _calculateMetaDataHeight(LogDataV2 data, double maxWidth) {
+    double totalHeight = 0.0;
+    double rowHeight = 0.0;
+    double remainingWidth = maxWidth;
+    print('------------------------\nSTATS:${[
+      totalHeight,
+      rowHeight,
+      remainingWidth
+    ]}');
+    for (final presentChit in LoggingTableRow._metadataChits(data, maxWidth)) {
+      final chitSize = presentChit.getSize();
+      print('chitSize: ${chitSize}');
+      if (chitSize.width > remainingWidth) {
+        print('O1');
+        // The chit does not fit so add it to a new row
+        totalHeight += rowHeight;
+        rowHeight = 0.0;
+        remainingWidth = maxWidth;
+      }
+
+      print('O2');
+      // The Chit fits so it will stay in this row.
+      rowHeight = max(rowHeight, chitSize.height);
+      remainingWidth -= chitSize.width;
+      print('STATS:${[totalHeight, rowHeight, remainingWidth]}');
+    }
+    totalHeight += rowHeight;
+    print('STATS:${[totalHeight, rowHeight, remainingWidth]}');
+    return totalHeight;
   }
 }
 
@@ -87,22 +137,15 @@ class _LoggingTableRowState extends State<LoggingTableRow> {
                     style: LoggingTableRow.detailsStyle,
                   ),
                 ),
-                Row(
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        text: 'Some META data',
-                        style: LoggingTableRow.metadataStyle,
+                LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Wrap(
+                      children: LoggingTableRow._metadataChits(
+                        widget.data,
+                        constraints.maxWidth,
                       ),
-                    ),
-                    const SizedBox(width: defaultSpacing),
-                    RichText(
-                      text: TextSpan(
-                        text: 'Some OTHER META data',
-                        style: LoggingTableRow.metadataStyle,
-                      ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
               ],
             ),
@@ -110,5 +153,127 @@ class _LoggingTableRowState extends State<LoggingTableRow> {
         },
       ),
     );
+  }
+}
+
+abstract class _MetadataChit extends StatelessWidget {
+  const _MetadataChit({
+    required this.data,
+    required this.maxWidth,
+  });
+
+  final LogDataV2 data;
+  final double maxWidth;
+  static const padding = defaultSpacing;
+
+  String getValue();
+  bool isPresent();
+  IconData getIcon();
+
+  TextSpan textSpan() {
+    return TextSpan(
+      text: getValue(),
+      style: LoggingTableRow.metadataStyle,
+    );
+  }
+
+  Size getSize() {
+    final maxWidthInsidePadding = maxWidth - padding * 2;
+    final iconSize = Size.square(tooltipIconSize);
+    final textSize = calculateTextSpanSize(
+      textSpan(),
+      maxWidth: maxWidthInsidePadding,
+    );
+    return Size(
+      iconSize.width + defaultSpacing + textSize.width + padding * 2,
+      max(iconSize.height, textSize.height) + padding * 2,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('maxWidth: $maxWidth');
+    //TODO: tooltip ?
+    return Container(
+      constraints: BoxConstraints(maxWidth: maxWidth),
+      padding: const EdgeInsets.all(padding),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            getIcon(),
+            size: tooltipIconSize,
+          ),
+          const SizedBox(width: defaultSpacing),
+          RichText(
+            text: textSpan(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _WhenMetaDataChit extends _MetadataChit {
+  const _WhenMetaDataChit({required super.data, required super.maxWidth});
+
+  @override
+  IconData getIcon() => Icons.punch_clock;
+
+  @override
+  String getValue() => data.timestamp == null
+      ? ''
+      : loggingTableTimeFormat
+          .format(DateTime.fromMillisecondsSinceEpoch(data.timestamp!));
+
+  @override
+  bool isPresent() {
+    return data.timestamp != null;
+  }
+}
+
+class _KindMetaDataChit extends _MetadataChit {
+  const _KindMetaDataChit({required super.data, required super.maxWidth});
+
+  @override
+  IconData getIcon() => Icons.type_specimen;
+
+  @override
+  String getValue() => data.kind;
+
+  @override
+  bool isPresent() {
+    return true;
+  }
+}
+
+class _FrameElapsedMetaDataChit extends _MetadataChit {
+  const _FrameElapsedMetaDataChit({
+    required super.data,
+    required super.maxWidth,
+  });
+
+  @override
+  IconData getIcon() => Icons.timer;
+
+  String? _getValue() {
+    double? frameLength;
+    try {
+      final int micros = (jsonDecode(data.details!) as Map)['elapsed'];
+      frameLength = micros * 3.0 / 1000.0;
+    } catch (e) {
+      // ignore
+    }
+    return frameLength?.toString();
+  }
+
+  @override
+  String getValue() {
+    return _getValue() ?? '';
+  }
+
+  @override
+  bool isPresent() {
+    return _getValue() != null;
   }
 }
