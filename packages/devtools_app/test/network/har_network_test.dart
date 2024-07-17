6c73e2897a3d186b19cf1a62270bf5db3ddd2f4b
hrajwade96 <hrishikeshrajwade96@gmail.com>
2024-07-17 15:34:31 +0530

    removed ignore, added cast everywhere, changed dynamic to Object?

--- packages/devtools_app/test/network/har_network_test.dart
+++ packages/devtools_app/test/network/har_network_test.dart
@@ -9,12 +9,11 @@ import 'package:devtools_app/src/screens/network/har_network_data.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 
-// ignore_for_file: avoid_dynamic_calls
-
 void main() {
   final file = File('test/network/sample_requests.json');
   final fileContent = file.readAsStringSync();
   final jsonData = jsonDecode(fileContent);
+
   group('HarNetworkData', () {
     test('toJson serializes correctly', () {
       // Parse the HAR data
@@ -25,30 +24,31 @@ void main() {
 
       // Verify the serialization
       expect(json['log'], isNotNull);
-      final log = json['log'] as Map<String, dynamic>?;
+      final log = json['log'] as Map<String, Object?>?;
       expect(log?['version'], '1.2');
-      expect(log?['creator']?['name'], 'devtools');
+      expect((log?['creator'] as Map<String, Object?>)['name'], 'devtools');
 
-      final entries = log?['entries'] as List<dynamic>?;
+      final entries = log?['entries'] as List<Object?>?;
       expect(entries?.length, 2);
 
-      final entry = entries?[0] as Map<String, dynamic>?;
+      final entry = entries?[0] as Map<String, Object?>?;
       expect(entry?['startedDateTime'], '2024-07-11T13:19:35.156Z');
-      expect(entry?['request']?['method'], 'GET');
+      expect((entry?['request'] as Map<String, Object?>)['method'], 'GET');
       expect(
-        entry?['request']?['url'],
+        (entry?['request'] as Map<String, Object?>)['url'],
         'https://jsonplaceholder.typicode.com/albums/1',
       );
-      expect(entry?['request']?['httpVersion'], 'HTTP/1.1');
-      expect(entry?['request']?['cookies'], isEmpty);
+      expect((entry?['request'] as Map<String, Object?>)['httpVersion'],
+          'HTTP/1.1');
+      expect((entry?['request'] as Map<String, Object?>)['cookies'], isEmpty);
 
       expect(entry?['cache'], isEmpty);
-      expect(entry?['timings']?['blocked'], -1);
-      expect(entry?['timings']?['dns'], -1);
-      expect(entry?['timings']?['connect'], -1);
-      expect(entry?['timings']?['send'], 1);
-      expect(entry?['timings']?['receive'], 1);
-      expect(entry?['timings']?['ssl'], -1);
+      expect((entry?['timings'] as Map<String, Object?>)['blocked'], -1);
+      expect((entry?['timings'] as Map<String, Object?>)['dns'], -1);
+      expect((entry?['timings'] as Map<String, Object?>)['connect'], -1);
+      expect((entry?['timings'] as Map<String, Object?>)['send'], 1);
+      expect((entry?['timings'] as Map<String, Object?>)['receive'], 1);
+      expect((entry?['timings'] as Map<String, Object?>)['ssl'], -1);
       expect(entry?['comment'], '');
     });
   });
