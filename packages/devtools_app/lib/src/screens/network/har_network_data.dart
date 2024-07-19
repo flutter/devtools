df0b56fd7764a40bce90a881af285b35840a3a0b
hrajwade96 <hrishikeshrajwade96@gmail.com>
2024-07-19 22:49:21 +0530

    used values from constants, added dart docs

--- packages/devtools_app/lib/src/screens/network/har_network_data.dart
+++ packages/devtools_app/lib/src/screens/network/har_network_data.dart
@@ -28,14 +28,15 @@ class HarNetworkData with Serializable {
   /// final harData = HarNetworkData.fromJson(json);
   /// ```
   factory HarNetworkData.fromJson(Map<String, Object?> json) {
-    final entries =
-        ((json['log'] as Map<String, Object?>)['entries'] as List<Object?>)
-            .map(
-              (entryJson) =>
-                  HarDataEntry.fromJson(entryJson as Map<String, Object?>)
-                      .toDartIOHttpRequest(),
-            )
-            .toList();
+    final entries = ((json[NetworkEventKeys.log.name]
+                as Map<String, Object?>)[NetworkEventKeys.entries.name]
+            as List<Object?>)
+        .map(
+          (entryJson) =>
+              HarDataEntry.fromJson(entryJson as Map<String, Object?>)
+                  .toDartIOHttpRequest(),
+        )
+        .toList();
 
     return HarNetworkData(entries);
   }
@@ -57,6 +58,13 @@ class HarNetworkData with Serializable {
   }
 }
 
+/// A class representing a single entry in the HTTP Archive (HAR) format.
+///
+/// This class is used to parse and handle data from HAR entries, converting
+/// them into [DartIOHttpRequestData] objects which represent HTTP request data.
+///
+/// This class also provides functionality to convert its instances back to
+/// JSON format, maintaining the HAR entry structure.
 class HarDataEntry {
   HarDataEntry(this.request);
 
@@ -64,41 +72,45 @@ class HarDataEntry {
   ///
   /// This factory constructor expects the [json] parameter to be a Map
   /// representing a single HAR entry.
-  factory HarDataEntry.fromJson(Map<String, Object?> json) {
+  factory HarDataEntry.fromJson(Map<String, dynamic> json) {
     _convertHeaders(json);
 
-    final modifiedRequestData =
-        _remapCustomFieldKeys(json) as Map<String, dynamic>;
+    final modifiedRequestData = _remapCustomFieldKeys(json);
 
     // Retrieving url, method from requestData
-    modifiedRequestData['uri'] =
-        (modifiedRequestData['request'] as Map<String, Object?>)['url'];
-    modifiedRequestData['method'] =
-        (modifiedRequestData['request'] as Map<String, Object?>)['method'];
+    modifiedRequestData[NetworkEventKeys.uri.name] =
+        (modifiedRequestData[NetworkEventKeys.request.name]
+            as Map<String, dynamic>)[NetworkEventKeys.url.name];
+    modifiedRequestData[NetworkEventKeys.method.name] =
+        (modifiedRequestData[NetworkEventKeys.request.name]
+            as Map<String, dynamic>)[NetworkEventKeys.method.name];
 
     // Adding missing keys which are mandatory for parsing
-    (modifiedRequestData['response'] as Map<String, Object?>)['redirects'] = [];
-    Object? requestPostData;
-    Object? responseContent;
-    if (modifiedRequestData['response'] != null &&
-        (modifiedRequestData['response'] as Map<String, Object?>)['content'] !=
+    (modifiedRequestData[NetworkEventKeys.response.name]
+        as Map<String, dynamic>)[NetworkEventKeys.redirects.name] = [];
+    dynamic requestPostData;
+    dynamic responseContent;
+    if (modifiedRequestData[NetworkEventKeys.response.name] != null &&
+        (modifiedRequestData[NetworkEventKeys.response.name]
+                as Map<String, dynamic>)[NetworkEventKeys.content.name] !=
             null) {
-      responseContent =
-          (modifiedRequestData['response'] as Map<String, Object?>)['content'];
+      responseContent = (modifiedRequestData[NetworkEventKeys.response.name]
+          as Map<String, dynamic>)[NetworkEventKeys.content.name];
     }
 
-    if (modifiedRequestData['request'] != null &&
-        (modifiedRequestData['request'] as Map<String, Object?>)['postData'] !=
+    if (modifiedRequestData[NetworkEventKeys.request.name] != null &&
+        (modifiedRequestData[NetworkEventKeys.request.name]
+                as Map<String, dynamic>)[NetworkEventKeys.postData.name] !=
             null) {
-      requestPostData =
-          (modifiedRequestData['response'] as Map<String, Object?>)['content'];
+      requestPostData = (modifiedRequestData[NetworkEventKeys.response.name]
+          as Map<String, dynamic>)[NetworkEventKeys.content.name];
     }
 
     return HarDataEntry(
       DartIOHttpRequestData.fromJson(
         modifiedRequestData,
-        requestPostData as Map<String, Object?>,
-        responseContent as Map<String, Object?>,
+        requestPostData as Map<String, dynamic>,
+        responseContent as Map<String, dynamic>,
       ),
     );
   }
@@ -149,25 +161,34 @@ class HarDataEntry {
   // Convert list of headers to map
   static void _convertHeaders(Map<String, dynamic> requestData) {
     // Request Headers
-    if (requestData['request'] != null &&
-        (requestData['request'] as Map<String, Object?>)['headers'] != null) {
-      if ((requestData['request'] as Map<String, Object?>)['headers'] is List) {
-        (requestData['request'] as Map<String, Object?>)['headers'] =
+    if (requestData[NetworkEventKeys.request.name] != null &&
+        (requestData[NetworkEventKeys.request.name]
+                as Map<String, dynamic>)[NetworkEventKeys.headers.name] !=
+            null) {
+      if ((requestData[NetworkEventKeys.request.name]
+          as Map<String, dynamic>)[NetworkEventKeys.headers.name] is List) {
+        (requestData[NetworkEventKeys.request.name]
+                as Map<String, dynamic>)[NetworkEventKeys.headers.name] =
             _convertHeadersListToMap(
-          ((requestData['request'] as Map<String, Object?>)['headers'])
+          ((requestData[NetworkEventKeys.request.name]
+                  as Map<String, dynamic>)[NetworkEventKeys.headers.name])
               as List<dynamic>,
         );
       }
     }
 
     // Response Headers
-    if (requestData['response'] != null &&
-        (requestData['response'] as Map<String, Object?>)['headers'] != null) {
-      if ((requestData['response'] as Map<String, Object?>)['headers']
-          is List) {
-        (requestData['response'] as Map<String, Object?>)['headers'] =
+    if (requestData[NetworkEventKeys.response.name] != null &&
+        (requestData[NetworkEventKeys.response.name]
+                as Map<String, dynamic>)[NetworkEventKeys.headers.name] !=
+            null) {
+      if ((requestData[NetworkEventKeys.response.name]
+          as Map<String, dynamic>)[NetworkEventKeys.headers.name] is List) {
+        (requestData[NetworkEventKeys.response.name]
+                as Map<String, dynamic>)[NetworkEventKeys.headers.name] =
             _convertHeadersListToMap(
-          ((requestData['response'] as Map<String, Object?>)['headers'])
+          ((requestData[NetworkEventKeys.response.name]
+                  as Map<String, dynamic>)[NetworkEventKeys.headers.name])
               as List<dynamic>,
         );
       }
