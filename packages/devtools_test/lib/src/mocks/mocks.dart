e4f08663df69b3fc5e151cff36ab3f3226d92d90
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2024-07-23 16:09:12 -0700

    Fix tests

--- packages/devtools_test/lib/src/mocks/mocks.dart
+++ packages/devtools_test/lib/src/mocks/mocks.dart
@@ -96,6 +96,10 @@ class TestInspectorV2Controller extends Fake
   final _selectedNode = ValueNotifier<inspector_v2.InspectorTreeNode?>(null);
 
   @override
+  ValueListenable<List<RemoteDiagnosticsNode>> get selectedNodeProperties =>
+      ValueNotifier<List<RemoteDiagnosticsNode>>(<RemoteDiagnosticsNode>[]);
+
+  @override
   void setSelectedNode(inspector_v2.InspectorTreeNode? newSelection) {
     _selectedNode.value = newSelection;
   }
