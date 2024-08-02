aa4ac74b25687e3df20798f785d6bdbe5569f45f
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2024-08-02 14:41:18 -0700

    Fix analyzer error

--- packages/devtools_test/lib/src/integration_test/integration_test_utils.dart
+++ packages/devtools_test/lib/src/integration_test/integration_test_utils.dart
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:convert';
 import 'dart:ui' as ui;
 
-import 'package:collection/collection.dart';
 import 'package:devtools_app/devtools_app.dart';
 import 'package:devtools_app/main.dart' as app;
 import 'package:devtools_app_shared/ui.dart';
