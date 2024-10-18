bd8e5c4c4ed12d59425cb1debb29fde5246f40af
Ann Mossman <233583+mossmana@users.noreply.github.com>
2024-10-18 13:12:25 -0700

    renamed functions to better match functionality, added logic to return replacement header, and added more tests

--- packages/devtools_shared/lib/src/utils/license_utils.dart
+++ packages/devtools_shared/lib/src/utils/license_utils.dart
@@ -52,48 +52,84 @@ class LicenseConfig {
 
 class LicenseHeader {
 
-  /// If the [file] has the given [licenseText] within the first number of
-  /// [byteCount] bytes, return the stored name and value map which can be empty
-  /// when no stored value is requested. (For now, only up to one stored value 
-  /// is supported.) Otherwise, throw an exception.
-  Future<Map<String,String>> getStoredValue(File file, String licenseText, int byteCount) async {
+  /// If the [file] matches the given [existingLicenseText] within the first
+  /// number of [byteCount] bytes, return the 'existing_header' and
+  /// 'replacement_header' each with the stored value ('<value>'
+  /// if configured, defaults to [defaultStoreValue] or current year)
+  /// populated. For now, only up to one stored value is supported.
+  /// If the file can't be read or no match is found, throw an exception.
+  Future<Map<String,String>> getReplacementInfo(
+    File file,
+    String existingLicenseText,
+    String replacementLicenseText, 
+    int byteCount,
+    [defaultStoredValue,]) async {
     final stream = file
       .openRead(0, byteCount)
       .transform(utf8.decoder)
       .handleError((e) => throw Exception('License header expected, but error reading file - $e'));
     await for (final content in stream) {
-      // Return an empty map for the simple case with no stored value requested 
-      // (i.e. content matches licenseText verbatim)
-      if (content.contains(licenseText)) {
-        return {};
+      // Return just the license headers for the simple case with no stored
+      // value requested (i.e. content matches licenseText verbatim)
+      if (content.contains(existingLicenseText)) {
+        final String storedName = _parseStoredName(replacementLicenseText);
+        replacementLicenseText = 
+          replacementLicenseText.replaceAll(
+            '<$storedName>',
+            defaultStoredValue ?? DateTime.now().year.toString(),
+          );
+        return {
+          'existing_header':existingLicenseText,
+          'replacement_header':replacementLicenseText,
+        };
       }
       // Return a non-empty map for the case where there is a stored value 
       // requested (i.e. when there is a '<value>' defined in the license text)
-      final String storedName = _parseStoredName(licenseText);
-      if (storedName.isNotEmpty) {
-        final String storedValue = 
-          _parseStoredValue(storedName, licenseText, content);
-        return {storedName:storedValue};
+      final String storedName = _parseStoredName(existingLicenseText);
+      if (storedName.isNotEmpty) { 
+        return _processHeaders(
+            storedName,
+            existingLicenseText,
+            replacementLicenseText,
+            content,);
       }
     }
     throw Exception('License header expected, but not found!');
   }
 
-  String _parseStoredValue(String storedName, String licenseText, String content) {
-    String matchStr = RegExp.escape(licenseText);
-    String? storedValue = '';
+  static const String existingHeaderKey = 'existing_header';
+  static const String replacementHeaderKey = 'replacement_header';
+
+  Map<String,String> _processHeaders(
+    String storedName,
+    String existingLicenseText,
+    String replacementLicenseText,
+    String content,) {
+    final String matchStr = RegExp.escape(existingLicenseText);
     final int storedNameIndex = matchStr.indexOf('<$storedName>');
     if (storedNameIndex != -1) {
       final String beforeStoredName = matchStr.substring(0, storedNameIndex);
       final String afterStoredName = matchStr
         .substring(storedNameIndex + storedName.length + 2)
         .trimRight();
-      final storedMatch = RegExp(r'' + beforeStoredName + 
-        r'(?<' + storedName + r'>\S+)' + 
-        afterStoredName + r'.*',);
-      storedValue = storedMatch.firstMatch(content)?.namedGroup(storedName);
+      final RegExp storedMatcher = RegExp(r'' + beforeStoredName + 
+        r'((?<' + storedName + r'>\S+))' + 
+        afterStoredName,);
+      if (storedMatcher.hasMatch(content)) {
+        final RegExpMatch? match = storedMatcher.firstMatch(content);
+        final String? existingHeaderValue = match?.group(0);
+        final String? storedValue = match?.namedGroup(storedName);
+        final String replacementHeaderValue = 
+          replacementLicenseText.replaceAll(
+            '<$storedName>', 
+            storedValue ?? DateTime.now().year.toString(),
+          );
+        return {LicenseHeader.existingHeaderKey:existingHeaderValue ?? '',
+          LicenseHeader.replacementHeaderKey:replacementHeaderValue,
+        }; 
+      }
     }
-    return storedValue ?? '';
+    return {};
   }
 
   // TODO: [mossmana] Add support for multiple stored names?
