f7091d2c0d037f7daf91315ec3da00294d380a8d
Kenzie Davisson <43759233+kenzieschmoll@users.noreply.github.com>
2024-11-04 16:14:08 -0800

    Bump Devtools dev version and Flutter version (#8504)

--- packages/devtools_app/lib/devtools.dart
+++ packages/devtools_app/lib/devtools.dart
@@ -3,10 +3,11 @@
 // found in the LICENSE file.
 
 /// The DevTools application version
-// This version should only be updated by running tools/update_version.dart
-// that updates all versions for DevTools.
-// Note: a regexp in tools/update_version.dart matches
-// the constant declaration `const version =`.
-// If you change the declaration you must also modify the regex in
-// tools/update_version.dart.
-const version = '2.41.0-dev.1';
+///
+/// This version should only be updated by running the 'dt update-version'
+/// command that updates the version here and in 'devtools_app/pubspec.yaml'.
+///
+/// Note: a regexp in the `dt update-version' command logic matches the constant
+/// declaration `const version =`. If you change the declaration you must also
+/// modify the regex in the `dt update-version' command logic.
+const version = '2.41.0-dev.2';
