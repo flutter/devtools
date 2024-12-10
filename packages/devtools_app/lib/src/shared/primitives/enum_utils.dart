30453ab2f5206fda91216d7cbda4f9f66bc173a6
Parker Lougheed <parlough@gmail.com>
2024-12-10 13:22:13 +0530

    Replace `EnumUtils` with extension methods from SDK (#8614)

--- packages/devtools_app/lib/src/shared/primitives/enum_utils.dart
+++ packages/devtools_app/lib/src/shared/primitives/enum_utils.dart
@@ -2,42 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(kenz): see if this class can be removed now that advanced enums are
-// supported in Dart.
-
-/// Class that converts enum value names to enum entries and vice versa.
-///
-/// Example usage:
-///
-/// ```dart
-/// enum Color {
-///   red, green, blue
-/// }
-///
-/// void f() {
-///   EnumUtils<Color> colorUtils = EnumUtils(Color.values);
-///   colorUtils.getEnum('red'); // returns Color.red
-///   colorUtils.getName(Color.red); // returns 'red'
-/// }
-/// ```
-class EnumUtils<T extends Enum> {
-  EnumUtils(List<T> enumValues) {
-    for (final val in enumValues) {
-      final enumDescription = val.name;
-      _lookupTable[enumDescription] = val;
-      _reverseLookupTable[val] = enumDescription;
-    }
-  }
-
-  final _lookupTable = <String, T>{};
-  final _reverseLookupTable = <T, String>{};
-
-  T? enumEntry(String? enumName) =>
-      enumName != null ? _lookupTable[enumName] : null;
-
-  String? name(T enumEntry) => _reverseLookupTable[enumEntry];
-}
-
 mixin EnumIndexOrdering<T extends Enum> on Enum implements Comparable<T> {
   @override
   int compareTo(T other) => index.compareTo(other.index);
