82edb9b63e693517e4bd6a28f496e925ac2af928
Ann Mossman <233583+mossmana@users.noreply.github.com>
2025-01-09 11:39:13 -0800

    license update - batch 9

--- packages/devtools_app/benchmark/devtools_benchmarks_test.dart
+++ packages/devtools_app/benchmark/devtools_benchmarks_test.dart
@@ -1,6 +1,6 @@
-// Copyright 2023 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
 // Note: this test was modeled after the example test from Flutter Gallery:
 // https://github.com/flutter/gallery/blob/master/test_benchmarks/benchmarks_test.dart
