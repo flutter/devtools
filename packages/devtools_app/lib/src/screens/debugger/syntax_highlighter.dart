431c4e6a235c99f07f889dd10310f3d62c56584e
Ann Mossman <233583+mossmana@users.noreply.github.com>
2025-01-13 14:51:01 -0800

    corrected edge case

--- packages/devtools_app/lib/src/screens/debugger/syntax_highlighter.dart
+++ packages/devtools_app/lib/src/screens/debugger/syntax_highlighter.dart
@@ -1,9 +1,13 @@
+<<<<<<< Updated upstream
 // Copyright 2025 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 // Copyright 2021 The Chromium Authors. All rights reserved.
+=======
+// Copyright 2021 The Flutter Authors
+>>>>>>> Stashed changes
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
 import 'dart:collection';
 import 'dart:convert';
