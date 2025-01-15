679ac4a937dcf0769f7c1f362bf72b1ca7bb026c
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-01-14 17:17:18 -0800

    Remove unused import

--- packages/devtools_app/test/standalone_ui/ide_shared/property_editor_test.dart
+++ packages/devtools_app/test/standalone_ui/ide_shared/property_editor_test.dart
@@ -16,8 +16,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../test_infra/utils/test_utils.dart';
-
 typedef Location = ({TextDocument document, CursorPosition position});
 typedef LocationToArgsResult = Map<Location, EditableArgumentsResult>;
 
