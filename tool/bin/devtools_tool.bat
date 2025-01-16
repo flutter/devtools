17c40d3a3226778b96e85b0b48db174eb304b44a
Ann Marie Mossman <233583+mossmana@users.noreply.github.com>
2025-01-16 15:43:24 -0800

    license update - batch 65

--- tool/bin/devtools_tool.bat
+++ tool/bin/devtools_tool.bat
@@ -1,3 +1,6 @@
+REM Copyright 2025 The Flutter Authors
+REM Use of this source code is governed by a BSD-style license that can be
+REM found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 @echo off
 
 echo Warning: devtools_tool has been replaced by dt. Please use dt instead.
