00258c707d29e96f79d48ee4749144be62d5c1bf
Ann Marie Mossman <233583+mossmana@users.noreply.github.com>
2025-01-16 16:13:28 -0800

    license update - batch 64 (#8779)

--- packages/devtools_test/lib/src/mocks/fake_object_inspector_view_controller.dart
+++ packages/devtools_test/lib/src/mocks/fake_object_inspector_view_controller.dart
@@ -1,6 +1,6 @@
-// Copyright 2023 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
 import 'package:devtools_app/devtools_app.dart';
 import 'package:flutter_test/flutter_test.dart';
