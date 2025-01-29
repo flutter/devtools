13f3389143c7e925c2aa2911f0e6cec1dc00db44
Ann Marie Mossman <233583+mossmana@users.noreply.github.com>
2025-01-29 15:39:38 -0800

    corrected license header placement (#8823)

--- tool/flutter_customer_tests/test.sh
+++ tool/flutter_customer_tests/test.sh
@@ -1,6 +1,7 @@
 # Copyright 2025 The Flutter Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
+
 # Script to execute smoke tests for the flutter/tests registry
 # https://github.com/flutter/tests
 # This is executed as a pre-submit check for every PR in flutter/flutter
