347207692f148a68f93eba18aecf08afc149537a
Sam Rawlins <srawlins@google.com>
2025-02-20 08:56:26 -0800

    feedback

--- packages/devtools_app/lib/src/screens/vm_developer/object_inspector/vm_object_model.dart
+++ packages/devtools_app/lib/src/screens/vm_developer/object_inspector/vm_object_model.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
-/// @docImport '../../vm_developer/object_inspector/vm_simple_list_display.dart';
+/// @docImport 'vm_simple_list_display.dart';
 library;
 
 import 'package:collection/collection.dart';
