40b2a264ff957e723fde3e1a0438db9c3cf5d024
Sam Rawlins <srawlins@google.com>
2025-03-10 14:30:17 -0700

    Add an integration test for the Network screen (#9003)
    There's a lot to this change!
    
    I wanted precise control over when the "test app" makes requests, and what kind, etc. I've added a capability for a Dart test app to print out what port it's "control server" is listening to, very similar to how it prints out the URL for DevTools to connect to.
    
    The "control server" is a basic HTTP server, that can be controlled with very simple requests. In this case, when the server sees a request to '/get/', it calls a dart:io HttpClient to send an HTTP GET request. A request to '/post/' directs the HttpClient to send an HTTP POST request, etc.
    
    For the network screen tests to see complete requests, there is a second HTTP server that responds to the test requests.
    
    So some changes are made to TestDartCliApp to support the "control port". This int is then passed to the integration test in the 'test args' (see the changes to integration_test/test_infra/run/run_test.dart).
    
    OK, then the test itself: This test is simple for now. The following types of requests are validated:
    
    * dart:io HttpClient GET
    * dart:io HttpClient POST
    * dart:io HttpClient PUT
    * dart:io HttpClient DELETE
    * Dio GET
    * Dio POST
    
    There are some TODOs for other requests we want to validate.
    
    Work towards #7554.

---
+++ packages/devtools_app/integration_test/test/live_connection/network_screen_test.dart
@@ -0,0 +1,109 @@
+// Copyright 2025 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
+
+// Do not delete these arguments. They are parsed by test runner.
+// test-argument:appPath="test/test_infra/fixtures/networking_app/bin/main.dart"
+
+import 'package:devtools_app/devtools_app.dart';
+import 'package:devtools_app/src/shared/table/table.dart' show DevToolsTable;
+import 'package:devtools_test/helpers.dart';
+import 'package:devtools_test/integration_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:integration_test/integration_test.dart';
+
+// To run:
+// dart run integration_test/run_tests.dart --target=integration_test/test/live_connection/network_screen_test.dart --test-app-device=cli
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  late TestApp testApp;
+
+  setUpAll(() {
+    testApp = TestApp.fromEnvironment();
+    expect(testApp.vmServiceUri, isNotNull);
+  });
+
+  tearDown(() async {
+    await resetHistory();
+    await http.get(Uri.parse('http://localhost:${testApp.controlPort}/exit/'));
+  });
+
+  testWidgets('nnn', (tester) async {
+    await pumpAndConnectDevTools(tester, testApp);
+    await _prepareNetworkScreen(tester);
+
+    final helper = _NetworkScreenHelper(tester, testApp.controlPort!);
+
+    // Instruct the app to make a GET request via the dart:io HttpClient.
+    await helper.triggerRequest('get/');
+    _expectInRequestTable('GET');
+    await helper.clear();
+
+    // Instruct the app to make a POST request via the dart:io HttpClient.
+    await helper.triggerRequest('post/');
+    _expectInRequestTable('POST');
+    await helper.clear();
+
+    // Instruct the app to make a PUT request via the dart:io HttpClient.
+    await helper.triggerRequest('put/');
+    _expectInRequestTable('PUT');
+    await helper.clear();
+
+    // Instruct the app to make a DELETE request via the dart:io HttpClient.
+    await helper.triggerRequest('delete/');
+    _expectInRequestTable('DELETE');
+    await helper.clear();
+
+    // Instruct the app to make a GET request via Dio.
+    await helper.triggerRequest('dio/get/');
+    _expectInRequestTable('GET');
+    await helper.clear();
+
+    // Instruct the app to make a POST request via Dio.
+    await helper.triggerRequest('dio/post/');
+    _expectInRequestTable('POST');
+  });
+}
+
+final class _NetworkScreenHelper {
+  _NetworkScreenHelper(this._tester, this._controlPort);
+
+  final WidgetTester _tester;
+
+  final int _controlPort;
+
+  Future<void> clear() async {
+    // Press the 'Clear' button between tests.
+    await _tester.tap(find.text('Clear'));
+    await _tester.pump(safePumpDuration);
+  }
+
+  Future<void> triggerRequest(String path) async {
+    await http.get(Uri.parse('http://localhost:$_controlPort/$path'));
+    await Future.delayed(const Duration(milliseconds: 200));
+    await _tester.pump(safePumpDuration);
+  }
+}
+
+void _expectInRequestTable(String text) {
+  expect(
+    find.descendant(
+      of: find.byType(DevToolsTable<NetworkRequest>),
+      matching: find.text(text),
+    ),
+    findsOneWidget,
+  );
+}
+
+/// Prepares the UI of the network screen for an integration test.
+Future<void> _prepareNetworkScreen(WidgetTester tester) async {
+  await switchToScreen(
+    tester,
+    tabIcon: ScreenMetaData.network.icon,
+    tabIconAsset: ScreenMetaData.network.iconAsset,
+    screenId: ScreenMetaData.network.id,
+  );
+}
