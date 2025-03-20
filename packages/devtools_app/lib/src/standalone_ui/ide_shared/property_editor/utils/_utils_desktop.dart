678ad9cc75b0e0ab41ebefad94370eb11e23204f
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-03-20 09:59:01 -0700

    [Property Editor] Show reconnection overlay when disconnection detected (#9043)

--- packages/devtools_app/lib/src/standalone_ui/ide_shared/property_editor/utils/_utils_desktop.dart
+++ packages/devtools_app/lib/src/standalone_ui/ide_shared/property_editor/utils/_utils_desktop.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
+void reloadIframe() {
+  // No-op for desktop platforms.
+}
+
 void addBlurListener() {
   // No-op for desktop platforms.
 }
