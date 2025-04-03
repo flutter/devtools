fb78b3ce19b5e51f600ea612b01230c136881ccd
Kenzie Davisson <kenzieschmoll@google.com>
2025-04-03 11:38:23 -0700

    Delete the `case_study` directory.

--- case_study/code_size/optimized/code_size_package/android/app/src/main/kotlin/com/example/code_size_package/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.code_size_package
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- case_study/memory_leaks/memory_leak_app/lib/common.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2019 The Flutter Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
-
-const appName = 'Memory Leak';
-
-const logMenu = 'Log';
-const aboutMenu = 'About';
