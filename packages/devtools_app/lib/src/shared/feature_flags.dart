02e58572f524365d72c7a6686bc811ba17269b7b
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-05-30 12:13:23 -0700

    [Property Editor] Flag clean-up (#9215)

--- packages/devtools_app/lib/src/shared/feature_flags.dart
+++ packages/devtools_app/lib/src/shared/feature_flags.dart
@@ -90,14 +90,9 @@ extension FeatureFlags on Never {
   /// https://github.com/flutter/devtools/issues/7856
   static bool wasmOptInSetting = true;
 
-  /// Flag to enable the Flutter Property Editor sidebar.
-  ///
-  /// https://github.com/flutter/devtools/issues/7854
-  static bool propertyEditor = enableExperiments;
-
   /// Flag to enable refactors in the Flutter Property Editor sidebar.
   ///
-  /// https://github.com/flutter/devtools/issues/8652
+  /// https://github.com/flutter/devtools/issues/9214
   static bool propertyEditorRefactors = enableExperiments;
 
   /// Stores a map of all the feature flags for debugging purposes.
@@ -112,7 +107,7 @@ extension FeatureFlags on Never {
     'dapDebugging': dapDebugging,
     'inspectorV2': inspectorV2,
     'wasmOptInSetting': wasmOptInSetting,
-    'propertyEditor': propertyEditor,
+    'propertyEditorRefactors': propertyEditorRefactors,
   };
 
   /// A helper to print the status of all the feature flags.
