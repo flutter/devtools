b947387c608d3942be1c200505e5db5879e22955
Kenzie Davisson <kenzieschmoll@google.com>
2025-06-03 08:12:56 -0700

    copyright

--- packages/devtools_app/lib/src/screens/dtd/events.dart
+++ packages/devtools_app/lib/src/screens/dtd/events.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
+
 import 'package:devtools_app_shared/ui.dart';
 import 'package:devtools_app_shared/utils.dart';
 import 'package:dtd/dtd.dart' show DartToolingDaemon, DTDEvent;
