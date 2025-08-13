187488d730ec909e04e49079f11ca68a0933602a
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-08-13 10:21:35 -0700

    Do not fast fail in bots.sh either

--- tool/ci/bots.sh
+++ tool/ci/bots.sh
@@ -4,8 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
-# Fast fail the script on failures.
-set -ex
+set -x
 
 source ./tool/ci/setup.sh
 
