e0bfd4e286b88e39aec7f48e024230b275d4c4fa
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-08-15 13:53:43 -0700

    Formatting

--- packages/devtools_shared/test/server/general_api_test.dart
+++ packages/devtools_shared/test/server/general_api_test.dart
@@ -165,7 +165,7 @@ void main() {
 
         setUp(() async {
           app = TestDartApp();
-            vmServiceUriString = await app!.start();
+          vmServiceUriString = await app!.start();
           // Await a short delay to give the VM a chance to initialize.
           await delay(duration: const Duration(milliseconds: 2500));
           expect(vmServiceUriString, isNotEmpty);
