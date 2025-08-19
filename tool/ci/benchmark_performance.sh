9831659ddec3421b4f1c97bda9dec681dddfacb0
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-08-19 15:31:02 -0700

    Change where we set fast-fail option

--- tool/ci/benchmark_performance.sh
+++ tool/ci/benchmark_performance.sh
@@ -4,10 +4,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
+source ./tool/ci/setup.sh
+
 # Fast fail the script on failures.
 set -ex
-
-source ./tool/ci/setup.sh
 
 pushd $DEVTOOLS_DIR/packages/devtools_app
 flutter test -j, --concurrency=1 benchmark/devtools_benchmarks_test.dart
