5e1792245005088a0a0dfe28f207bd22045ba783
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-08-22 12:32:06 -0700

    Revert PR #9372: Un-delete `packages/devtools_shared/lib/src/test/chrome.dart` (#9409)

--- packages/devtools_shared/lib/devtools_test_utils.dart
+++ packages/devtools_shared/lib/devtools_test_utils.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
+export 'src/test/chrome.dart';
 export 'src/test/chrome_driver.dart';
 export 'src/test/cli_test_driver.dart';
 export 'src/test/integration_test_runner.dart';
