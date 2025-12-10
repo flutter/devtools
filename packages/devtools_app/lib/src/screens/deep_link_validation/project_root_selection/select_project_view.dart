76a8ae0b5f7afe1637976060ed2bfbe2cfae03af
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-12-10 11:53:05 -0800

    Capitalize Flutter

--- packages/devtools_app/lib/src/screens/deep_link_validation/project_root_selection/select_project_view.dart
+++ packages/devtools_app/lib/src/screens/deep_link_validation/project_root_selection/select_project_view.dart
@@ -272,7 +272,7 @@ class _SelectProjectViewState extends State<SelectProjectView> {
         Padding(
           padding: const EdgeInsets.all(extraLargeSpacing),
           child: Text(
-            'Select a local flutter project to check the status of all deep links.',
+            'Select a local Flutter project to check the status of all deep links.',
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.titleMedium,
           ),
