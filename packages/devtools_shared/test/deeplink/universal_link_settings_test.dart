daa402c2ea0b9b24aebdf528b6b218ad4fd2c0b8
Chun-Heng Tai <chtai@google.com>
2025-12-11 15:51:26 -0800

    analyze

--- packages/devtools_shared/test/deeplink/universal_link_settings_test.dart
+++ packages/devtools_shared/test/deeplink/universal_link_settings_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
-import 'dart:convert';
-
 import 'package:devtools_shared/src/deeplink/universal_link_settings.dart';
 import 'package:test/test.dart';
 
