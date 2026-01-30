d24a06c288c6d2d48b52b7c7d1e19ea6b3f8c88b
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-01-30 15:07:31 -0800

    Remove unused import

--- packages/devtools_shared/lib/src/test/test_utils.dart
+++ packages/devtools_shared/lib/src/test/test_utils.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
 
-import 'dart:async';
-
 Future<void> waitFor(
   Future<bool> Function() condition, {
   Duration timeout = const Duration(seconds: 10),
