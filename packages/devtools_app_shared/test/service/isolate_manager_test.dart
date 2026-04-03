f6199b9a654aac977bd11f8ce97dace069eec851
rishika0212 <rishrish529@gmail.com>
2026-04-03 17:43:26 +0530

    Auto-select test suite isolate for paused dart tests

--- packages/devtools_app_shared/test/service/isolate_manager_test.dart
+++ packages/devtools_app_shared/test/service/isolate_manager_test.dart
@@ -13,10 +13,12 @@ class _FakeVmService extends Fake implements VmService {
   /// Map of isolate id -> Isolate to return from getIsolate().
   final Map<String, Isolate> isolates;
 
+  final _isolateEventController = StreamController<Event>.broadcast();
+
   _FakeVmService(this.isolates);
 
   @override
-  Stream<Event> get onIsolateEvent => const Stream.empty();
+  Stream<Event> get onIsolateEvent => _isolateEventController.stream;
 
   @override
   Stream<Event> get onDebugEvent => const Stream.empty();
@@ -34,6 +36,26 @@ class _FakeVmService extends Fake implements VmService {
   @override
   Future<Success> resume(String isolateId, {String? step, int? frameIndex}) =>
       Future.value(Success());
+
+  Future<void> emitIsolateStart(IsolateRef isolateRef) async {
+    _isolateEventController.add(
+      Event.parse({
+        'type': 'Event',
+        'kind': EventKind.kIsolateStart,
+        'isolate': {
+          'type': '@Isolate',
+          'id': isolateRef.id,
+          'name': isolateRef.name,
+          'isSystemIsolate': isolateRef.isSystemIsolate,
+        },
+      })!,
+    );
+    await Future<void>.delayed(Duration.zero);
+  }
+
+  Future<void> dispose() async {
+    await _isolateEventController.close();
+  }
 }
 
 /// Creates a minimal runnable [Isolate] for a given [IsolateRef].
@@ -63,6 +85,7 @@ IsolateRef _makeRef(String name, String id) {
 void main() {
   group('IsolateManager._computeMainIsolate', () {
     late IsolateManager manager;
+    final fakeServices = <_FakeVmService>[];
 
     setUp(() {
       manager = IsolateManager();
@@ -70,6 +93,10 @@ void main() {
 
     tearDown(() {
       manager.handleVmServiceClosed();
+      for (final fakeService in fakeServices) {
+        fakeService.dispose();
+      }
+      fakeServices.clear();
     });
 
     test(
@@ -91,6 +118,7 @@ void main() {
           'isolates/2': _makeIsolate(testSuiteRef),
           'isolates/3': _makeIsolate(vmServiceRef),
         });
+        fakeServices.add(fakeService);
 
         manager.vmServiceOpened(fakeService);
         await manager.init([testRunnerRef, testSuiteRef, vmServiceRef]);
@@ -118,6 +146,7 @@ void main() {
         'isolates/1': _makeIsolate(mainRef),
         'isolates/2': _makeIsolate(vmServiceRef),
       });
+      fakeServices.add(fakeService);
 
       manager.vmServiceOpened(fakeService);
       await manager.init([mainRef, vmServiceRef]);
@@ -135,6 +164,7 @@ void main() {
       final fakeService = _FakeVmService({
         'isolates/1': _makeIsolate(scriptRef),
       });
+      fakeServices.add(fakeService);
 
       manager.vmServiceOpened(fakeService);
       await manager.init([scriptRef]);
@@ -166,6 +196,7 @@ void main() {
             rootLibraryUri: 'dart:developer',
           ),
         });
+        fakeServices.add(fakeService);
 
         manager.vmServiceOpened(fakeService);
         await manager.init([testRunnerRef, userTestRef, vmServiceRef]);
@@ -179,6 +210,41 @@ void main() {
         expect(
           manager.mainIsolate.value?.name,
           equals('isolate-2'),
+        );
+      },
+    );
+
+    test(
+      'promotes main isolate from test runner to test suite on isolate start',
+      () async {
+        final testRunnerRef = _makeRef('main', 'isolates/1');
+        final testSuiteRef = _makeRef(
+          'test_suite:file:///tmp/dart_test.kernel.dill',
+          'isolates/2',
+        );
+
+        final fakeService = _FakeVmService({
+          'isolates/1': _makeIsolate(testRunnerRef),
+          'isolates/2': _makeIsolate(testSuiteRef),
+        });
+        fakeServices.add(fakeService);
+
+        manager.vmServiceOpened(fakeService);
+        await manager.init(const []);
+
+        await fakeService.emitIsolateStart(testRunnerRef);
+        expect(manager.selectedIsolate.value?.name, equals('main'));
+        expect(manager.mainIsolate.value?.name, equals('main'));
+
+        await fakeService.emitIsolateStart(testSuiteRef);
+        expect(
+          manager.selectedIsolate.value?.name,
+          equals('test_suite:file:///tmp/dart_test.kernel.dill'),
+          reason: 'Should switch selection to test_suite isolate once it starts',
+        );
+        expect(
+          manager.mainIsolate.value?.name,
+          equals('test_suite:file:///tmp/dart_test.kernel.dill'),
         );
       },
     );
