eb5547bda7c17d83904f8354df26c5bab5154585
Reid Baker <reidbaker@google.com>
2026-04-14 17:52:59 -0400

    Copyright added to new files and issue linked in pubspec

--- tool/test/validate_skills_test.dart
+++ tool/test/validate_skills_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2026 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file or at https://developers.google.com/open-source/licenses/bsd.
+
 import 'dart:async';
 import 'package:dart_skills_lint/dart_skills_lint.dart';
 import 'package:logging/logging.dart';
