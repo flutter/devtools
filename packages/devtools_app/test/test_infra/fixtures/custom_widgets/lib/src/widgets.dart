53802b77baecd6a123d8ebaeb90531313b934b78
Kenzie Davisson <kenzieschmoll@google.com>
2026-07-28 15:32:15 -0700

    fixes and update flutter

--- packages/devtools_app/test/test_infra/fixtures/custom_widgets/lib/src/widgets.dart
+++ packages/devtools_app/test/test_infra/fixtures/custom_widgets/lib/src/widgets.dart
@@ -65,26 +65,9 @@ class CustomContainer extends StatelessWidget {
   }
 }
 
-class CustomCenter extends StatelessWidget {
-  const CustomCenter({
-    super.key,
-    this.widthFactor,
-    this.heightFactor,
-    this.child,
-  });
-
-  final double? widthFactor;
-  final double? heightFactor;
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      widthFactor: widthFactor,
-      heightFactor: heightFactor,
-      child: child,
-    );
-  }
+class CustomCenter extends Align {
+  const CustomCenter(
+      {super.key, super.widthFactor, super.heightFactor, super.child});
 }
 
 class CustomText extends StatelessWidget {
