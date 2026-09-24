8f3c5585a216f208a73ee06b0eb95df932b9e245
Danny Tuppeny <danny@tuppeny.com>
2026-09-24 11:23:23 +0100

    Add code blocks with more than 3 backticks

--- packages/devtools_app/test/test_infra/test_data/syntax_highlighting/comments.dart
+++ packages/devtools_app/test/test_infra/test_data/syntax_highlighting/comments.dart
@@ -46,3 +46,27 @@ var g;
 /// Dartdoc with reference to [a].
 /// And a link to [example.org](http://example.org/).
 var h;
+
+/// Multiline dartdoc comment with more than three backticks.
+///
+/// ``````
+/// code in six backticks
+/// ```
+/// three backticks is nested code
+/// ```
+/// more code
+/// ``````
+///
+/// Text
+var moreBackticks;
+
+/// Multiline dartdoc comment with more closing backticks.
+///
+/// ``````
+/// code in six backticks
+/// ```
+/// still code
+/// ``````````````````
+///
+/// Text
+var moreClosingBackticks;
